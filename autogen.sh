b904a0bb2c87a73d1f4d517b09953ac7f22fd02c
rbdpngn <rbdpngn>
2005-01-22 06:56:56 +0000

    Use glibtoolize on OS X.
    SVN revision: 13052

--- autogen.sh
+++ autogen.sh
@@ -6,7 +6,7 @@ rm -f aclocal.m4
 echo "Running aclocal..."; aclocal $ACLOCAL_FLAGS -I m4 \
 && echo "Running autoheader..."; autoheader \
 && echo "Running autoconf..."; autoconf \
-&& echo "Running libtoolize..."; libtoolize --automake \
+&& echo "Running libtoolize..."; ( libtoolize --automake || glibtoolize --automake ) \
 && echo "Running automake..."; automake --add-missing --copy --gnu
 
 ###  If you want this, uncomment it.
