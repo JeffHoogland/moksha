2c3901e2176705ea18e0d2762168518f8409f941
sebastid <sebastid>
2005-02-09 11:39:05 +0000

    Formatting
    SVN revision: 13255

--- src/bin/e_resist.h
+++ src/bin/e_resist.h
@@ -8,6 +8,6 @@
 
 EAPI int e_resist_container_border_position(E_Container *con, Evas_List *skiplist, int px, int py, int pw, int ph, int x, int y, int w, int h, int *rx, int *ry, int *rw, int *rh);
 EAPI int e_resist_container_gadman_position(E_Container *con, Evas_List *skiplist, int px, int py, int pw, int ph, int x, int y, int w, int h, int *rx, int *ry);
-    
+
 #endif
 #endif
