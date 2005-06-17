ad3adb3286682fecc8a552c335e317061f658ac1
Carsten Haitzler <raster@rasterman.com>
2005-06-17 06:14:12 +0000

    follow x focus model explicitly :)
    SVN revision: 15375

--- src/bin/e_border.c
+++ src/bin/e_border.c
@@ -855,7 +855,8 @@ e_border_focus_set(E_Border *bd, int focus, int set)
  * 
  * now the focus is on Y where it should be on X
  */
-		  ecore_x_window_focus(bd->client.win);
+// technially this is wrong to set the focus explicitly (passive focus model)
+//		  ecore_x_window_focus(bd->client.win);
 		  ecore_x_icccm_take_focus_send(bd->client.win, ecore_x_current_time_get());
 //		  e_hints_active_window_set(bd->zone->container->manager, bd);
 	       }
