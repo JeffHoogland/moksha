648e0406fd005c5ff639d9ab74cd79836145edb0
Mike Frysinger <vapier@gentoo.org>
2005-08-08 21:46:03 +0000

    add error checking
    SVN revision: 16074

--- autogen.sh
+++ autogen.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
 rm -rf autom4te.cache
-rm -f aclocal.m4
+rm -f aclocal.m4 ltmain.sh
 
-echo "Running aclocal..."; aclocal $ACLOCAL_FLAGS -I m4 \
-&& echo "Running autoheader..."; autoheader \
-&& echo "Running autoconf..."; autoconf \
-&& echo "Running libtoolize..."; ( libtoolize --automake || glibtoolize --automake ) \
-&& echo "Running automake..."; automake --add-missing --copy --gnu \
-&& echo "Generating gettext enlightenment.pot template"; \
+touch README
+
+echo "Running aclocal..." ; aclocal $ACLOCAL_FLAGS -I m4 || exit 1
+echo "Running autoheader..." ; autoheader || exit 1
+echo "Running autoconf..." ; autoconf || exit 1
+echo "Running libtoolize..." ; (libtoolize --copy --automake || glibtoolize --automake) || exit 1
+echo "Running automake..." ; automake --add-missing --copy --gnu || exit 1
+echo "Generating gettext enlightenment.pot template"; \
 xgettext \
 -n \
 -C \
@@ -20,7 +22,7 @@ xgettext \
 -k -k_ -kd_ \
 --from-code=UTF-8 \
 -o enlightenment.pot \
-`find . -name "*.[ch]" -print` 
+`find . -name "*.[ch]" -print` || exit 1
 
 if [ -z "$NOCONFIGURE" ]; then
 	./configure "$@"
