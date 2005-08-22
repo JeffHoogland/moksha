14d3764b391f5e329e3528f2f9b09c5bdd9c139e
sebastid <sebastid>
2005-08-22 14:23:02 +0000

    Load ibox in new config.
    SVN revision: 16282

--- src/bin/e_config.c
+++ src/bin/e_config.c
@@ -422,6 +422,10 @@ e_config_init(void)
 	     em->enabled = 1;
 	     e_config->modules = evas_list_append(e_config->modules, em);
 	     em = E_NEW(E_Config_Module, 1);
+	     em->name = strdup("ibox");
+	     em->enabled = 0;
+	     e_config->modules = evas_list_append(e_config->modules, em);
+	     em = E_NEW(E_Config_Module, 1);
 	     em->name = strdup("dropshadow");
 	     em->enabled = 1;
 	     e_config->modules = evas_list_append(e_config->modules, em);
