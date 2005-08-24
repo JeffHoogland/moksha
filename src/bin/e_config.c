ba1d75580adb08986e03fefa69701abcfd835946
sebastid <sebastid>
2005-08-24 22:29:30 +0000

    Enable randr by default.
    SVN revision: 16323

--- src/bin/e_config.c
+++ src/bin/e_config.c
@@ -455,7 +455,7 @@ e_config_init(void)
 	     e_config->modules = evas_list_append(e_config->modules, em);
 	     em = E_NEW(E_Config_Module, 1);
 	     em->name = strdup("randr");
-	     em->enabled = 0;
+	     em->enabled = 1;
 	     e_config->modules = evas_list_append(e_config->modules, em);
 	  }
 	  {
