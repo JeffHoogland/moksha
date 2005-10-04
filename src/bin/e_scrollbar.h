2b7cbc4b4404450a79ff67819e3e075018e81fbe
sebastid <sebastid>
2005-10-04 22:53:27 +0000

    Typo.
    SVN revision: 17178

--- src/bin/e_scrollbar.h
+++ src/bin/e_scrollbar.h
@@ -3,7 +3,7 @@
  */
 #ifdef E_TYPEDEFS
 
-typedef enum _E_Scrollbar_Direction 
+typedef enum _E_Scrollbar_Direction
 {
    E_SCROLLBAR_HORIZONTAL,
    E_SCROLLBAR_VERTICAL
@@ -14,9 +14,9 @@ typedef enum _E_Scrollbar_Direction
 #define E_SCROLLBAR_H
 
 EAPI Evas_Object          *e_scrollbar_add(Evas *evas);
-EAPI void                  e_scrollbar_direction_set_(Evas_Object *object,
-						      E_Scrollbar_Direction dir);
+EAPI void                  e_scrollbar_direction_set(Evas_Object *object,
+						     E_Scrollbar_Direction dir);
 EAPI E_Scrollbar_Direction e_scrollbar_direction_get(Evas_Object *object);
-   
+
 #endif
 #endif
