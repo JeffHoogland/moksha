75a413776d4b3f902eb0969f3107f9efcf4d1655
sebastid <sebastid>
2005-11-04 21:01:37 +0000

    Correct define for this file. Fix function declarations.
    SVN revision: 18291

--- src/bin/e_widget_iconsel.h
+++ src/bin/e_widget_iconsel.h
@@ -3,11 +3,11 @@
  */
 #ifdef E_TYPEDEFS
 #else
-#ifndef E_WIDGET_BUTTON_H
-#define E_WIDGET_BUTTON_H
+#ifndef E_WIDGET_ICONSEL_H
+#define E_WIDGET_ICONSEL_H
 
-EAPI Evas_Object *e_widget_iconsel_add(Evas *evas, Evas_Object *icon, Evas_Coord minw, Evas_Coord minh, void (*func) (void *data, void *data2), void *data, void *data2);
-EAPI Evas_Object *e_widget_iconsel_add_from_file(Evas *evas, char *icon, Evas_Coord minw, Evas_Coord minh, void (*func) (void *data, void *data2), void *data, void *data2);
+EAPI Evas_Object *e_widget_iconsel_add(Evas *evas, Evas_Object *icon, Evas_Coord minw, Evas_Coord minh, char **file);
+EAPI Evas_Object *e_widget_iconsel_add_from_file(Evas *evas, char *icon, Evas_Coord minw, Evas_Coord minh, char **file);
 EAPI void         e_widget_iconsel_select_callback_add(Evas_Object *obj, void (*func)(Evas_Object *obj, char *file, void *data), void *data);
 
 #endif
