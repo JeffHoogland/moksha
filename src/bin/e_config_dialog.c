d9b9bc6f90dc39ff7cebf1933817d0add184310c
Carsten Haitzler <raster@rasterman.com>
2005-12-21 02:45:24 +0000

    cancel renamed to close.
    SVN revision: 19194

--- src/bin/e_config_dialog.c
+++ src/bin/e_config_dialog.c
@@ -112,15 +112,14 @@ _e_config_dialog_go(E_Config_Dialog *cfd, E_Config_Dialog_CFData_Type type)
    e_widget_on_change_hook_set(o, _e_config_dialog_cb_changed, cfd);
    e_dialog_content_set(cfd->dia, o, mw, mh);
    
-   if(!cfd->hide_buttons)
+   if (!cfd->hide_buttons)
      {
-   e_dialog_button_add(cfd->dia, _("OK"), NULL, _e_config_dialog_cb_ok, cfd);
-   e_dialog_button_add(cfd->dia, _("Apply"), NULL, _e_config_dialog_cb_apply, cfd);
-   //e_dialog_button_add(cfd->dia, _("Cancel"), NULL, NULL, NULL);
-   e_dialog_button_disable_num_set(cfd->dia, 0, 1);
-   e_dialog_button_disable_num_set(cfd->dia, 1, 1);
+	e_dialog_button_add(cfd->dia, _("OK"), NULL, _e_config_dialog_cb_ok, cfd);
+	e_dialog_button_add(cfd->dia, _("Apply"), NULL, _e_config_dialog_cb_apply, cfd);
+	e_dialog_button_disable_num_set(cfd->dia, 0, 1);
+	e_dialog_button_disable_num_set(cfd->dia, 1, 1);
      }
-   e_dialog_button_add(cfd->dia, _("Cancel"), NULL, NULL, NULL);
+   e_dialog_button_add(cfd->dia, _("Close"), NULL, NULL, NULL);
    e_win_centered_set(cfd->dia->win, 1);
    e_dialog_show(cfd->dia);
    cfd->view_type = type;
