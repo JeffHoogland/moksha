c1da03d53e7effcadc5ba8f8f6e9fefc9d333ad2
Christopher Michael <cpmichael1@comcast.net>
2005-12-24 19:47:42 +0000

    Remove unused proto
    SVN revision: 19286

--- src/modules/clock/e_mod_config.c
+++ src/modules/clock/e_mod_config.c
@@ -17,7 +17,6 @@ struct _Cfg_File_Data
 };
 
 /* Protos */
-static Evas_Object *_create_widgets(E_Config_Dialog *cfd, Evas *evas, Config *cfdata);
 static void *_create_data(E_Config_Dialog *cfd);
 static void _free_data(E_Config_Dialog *cfd, CFData *cfdata);
 static Evas_Object *_basic_create_widgets(E_Config_Dialog *cfd, Evas *evas, CFData *cfdata);
