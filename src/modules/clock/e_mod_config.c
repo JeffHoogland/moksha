03f97999f3a05aa31548011721fde0f9ebf1cfbd
Christopher Michael <cpmichael1@comcast.net>
2006-03-18 16:42:44 +0000

    Removed unused Cfg_File struct
    SVN revision: 21376

--- src/modules/clock/e_mod_config.c
+++ src/modules/clock/e_mod_config.c
@@ -3,17 +3,9 @@
 #include "e_mod_config.h"
 #include "config.h"
 
-typedef struct _Cfg_File_Data Cfg_File_Data;
-
 struct _E_Config_Dialog_Data
 {   
    int digital_style;
-};
-
-struct _Cfg_File_Data 
-{
-   E_Config_Dialog *cfd;
-   char *file;
 };
 
 /* Protos */
