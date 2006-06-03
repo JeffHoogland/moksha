284085fd7068dd8fcfa1fff6b4f5522add6bced8
Christopher Michael <cpmichael1@comcast.net>
2006-06-03 02:27:13 +0000

    Rework Shelf Config dialog, giving it a basic and advanced mode. Advanced mode contains everything, basic mode just has size and style.
    SVN revision: 23106

--- src/bin/e_int_shelf_config.c
+++ src/bin/e_int_shelf_config.c
@@ -8,6 +8,8 @@ static void *_create_data(E_Config_Dialog *cfd);
 static void _free_data(E_Config_Dialog *cfd, E_Config_Dialog_Data *cfdata);
 static int _basic_apply_data(E_Config_Dialog *cfd, E_Config_Dialog_Data *cfdata);
 static Evas_Object *_basic_create_widgets(E_Config_Dialog *cfd, Evas *evas, E_Config_Dialog_Data *cfdata);
+static int _advanced_apply_data(E_Config_Dialog *cfd, E_Config_Dialog_Data *cfdata);
+static Evas_Object *_advanced_create_widgets(E_Config_Dialog *cfd, Evas *evas, E_Config_Dialog_Data *cfdata);
 
 /* Actual config data we will be playing with whil the dialog is active */
 struct _E_Config_Dialog_Data
@@ -37,6 +39,9 @@ e_int_shelf_config(E_Shelf *es)
 	v->free_cfdata             = _free_data;
 	v->basic.apply_cfdata      = _basic_apply_data;
 	v->basic.create_widgets    = _basic_create_widgets;
+	v->advanced.apply_cfdata   = _advanced_apply_data;
+	v->advanced.create_widgets = _advanced_create_widgets;
+	
 	v->override_auto_apply = 1;
 	
 	/* create config diaolg for bd object/data */
@@ -102,6 +107,39 @@ _basic_apply_data(E_Config_Dialog *cfd, E_Config_Dialog_Data *cfdata)
 {
    E_Zone *zone;
    int id;
+
+   cfdata->escfg->fit_along = cfdata->fit_along;
+   cfdata->escfg->fit_size = cfdata->fit_size;
+   cfdata->escfg->size = cfdata->size;
+   
+   if (cfdata->escfg->style) evas_stringshare_del(cfdata->escfg->style);
+   cfdata->escfg->style = evas_stringshare_add(cfdata->style);
+
+   zone = cfdata->es->zone;
+   id = cfdata->es->id;
+   cfdata->es->config_dialog = NULL;
+   e_object_del(E_OBJECT(cfdata->es));
+   cfdata->es = e_shelf_zone_new(zone, cfdata->escfg->name, 
+				 cfdata->escfg->style,
+				 cfdata->escfg->popup,
+				 cfdata->escfg->layer, id);
+   cfdata->es->cfg = cfdata->escfg;
+   cfdata->es->fit_along = cfdata->escfg->fit_along;
+   cfdata->es->fit_size = cfdata->escfg->fit_size;
+   e_shelf_orient(cfdata->es, cfdata->escfg->orient);
+   e_shelf_position_calc(cfdata->es);
+   e_shelf_populate(cfdata->es);
+   e_shelf_show(cfdata->es);
+   e_config_save_queue();
+   cfdata->es->config_dialog = cfd;
+   return 1; /* Apply was OK */
+}
+
+static int
+_advanced_apply_data(E_Config_Dialog *cfd, E_Config_Dialog_Data *cfdata) 
+{
+   E_Zone *zone;
+   int id;
    
    cfdata->escfg->orient = cfdata->orient;
    cfdata->escfg->fit_along = cfdata->fit_along;
@@ -142,7 +180,7 @@ _basic_apply_data(E_Config_Dialog *cfd, E_Config_Dialog_Data *cfdata)
    e_shelf_show(cfdata->es);
    e_config_save_queue();
    cfdata->es->config_dialog = cfd;
-   return 1; /* Apply was OK */
+   return 1; /* Apply was OK */   
 }
 
 static void
@@ -158,6 +196,65 @@ _cb_configure(void *data, void *data2)
 /**--GUI--**/
 static Evas_Object *
 _basic_create_widgets(E_Config_Dialog *cfd, Evas *evas, E_Config_Dialog_Data *cfdata)
+{
+   /* generate the core widget layout for a basic dialog */
+   Evas_Object *o, *of, *ob, *oi, *oj;
+   E_Radio_Group *rg;
+   Evas_Coord wmw, wmh;
+   Evas_List *styles, *l;
+   int sel, n;
+
+   o = e_widget_list_add(evas, 0, 0);
+   
+   of = e_widget_framelist_add(evas, _("Size"), 0);
+   ob = e_widget_check_add(evas, _("Shrink to Content Size"), &(cfdata->fit_along));
+   e_widget_framelist_object_append(of, ob);
+//   ob = e_widget_check_add(evas, _("Expand width to fit contents"), &(cfdata->fit_size));
+//   e_widget_framelist_object_append(of, ob);
+   ob = e_widget_label_add(evas, _("Shelf Size"));
+   e_widget_framelist_object_append(of, ob);
+   ob = e_widget_slider_add(evas, 1, 0, _("%3.0f pixels"), 4, 120, 4, 0, NULL, &(cfdata->size), 100);
+   e_widget_framelist_object_append(of, ob);
+   e_widget_list_object_append(o, of, 1, 1, 0.5);
+
+   of = e_widget_framelist_add(evas, _("Styles"), 0);   
+   oi = e_widget_ilist_add(evas, 128, 20, &(cfdata->style));
+   
+   sel = 0;
+   styles = e_theme_shelf_list();
+
+   for (n = 0, l = styles; l; l = l->next, n++)
+     {
+	char buf[4096];
+	
+	ob = e_livethumb_add(evas);
+	e_livethumb_vsize_set(ob, 256, 40);
+	oj = edje_object_add(e_livethumb_evas_get(ob));
+	snprintf(buf, sizeof(buf), "shelf/%s/base", (char *)l->data);
+	e_theme_edje_object_set(oj, "base/theme/shelf", buf);
+	e_livethumb_thumb_set(ob, oj);
+	e_widget_ilist_append(oi, ob, (char *)l->data, NULL, NULL, l->data);
+	if (!strcmp(cfdata->es->style, (char *)l->data))
+	  sel = n;
+     }
+   e_widget_min_size_get(oi, &wmw, &wmh);
+   e_widget_min_size_set(oi, wmw, 120);
+   
+   e_widget_ilist_go(oi);
+   e_widget_ilist_selected_set(oi, sel);
+   
+   e_widget_framelist_object_append(of, oi);
+   
+   e_widget_list_object_append(o, of, 0, 0, 0.5);
+   
+   ob = e_widget_button_add(evas, _("Configure Contents..."), "widget/config", _cb_configure, cfdata, NULL);
+   e_widget_list_object_append(o, ob, 0, 0, 0.5);
+
+   return o;
+}
+
+static Evas_Object *
+_advanced_create_widgets(E_Config_Dialog *cfd, Evas *evas, E_Config_Dialog_Data *cfdata) 
 {
    /* generate the core widget layout for a basic dialog */
    Evas_Object *o, *o2, *of, *ob, *oi, *oj;
@@ -260,5 +357,5 @@ _basic_create_widgets(E_Config_Dialog *cfd, Evas *evas, E_Config_Dialog_Data *cf
    
    e_widget_list_object_append(o, o2, 0, 0, 0.0);
    
-   return o;
+   return o;   
 }
