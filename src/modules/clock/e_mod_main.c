6e6240a23acb36d9d4396171fad5664ddc29c992
Christopher Michael <cpmichael1@comcast.net>
2006-07-25 04:11:41 +0000

    Cleanup: Fix return value of modapi_init.
    SVN revision: 24186

--- src/modules/clock/e_mod_main.c
+++ src/modules/clock/e_mod_main.c
@@ -125,7 +125,7 @@ e_modapi_init(E_Module *m)
    clock_module = m;
    
    e_gadcon_provider_register(&_gadcon_class);
-   return 1;
+   return m;
 }
 
 EAPI int
--- src/modules/cpufreq/e_mod_main.c
+++ src/modules/cpufreq/e_mod_main.c
@@ -884,7 +884,7 @@ e_modapi_init(E_Module *m)
    cpufreq_config->module = m;
    
    e_gadcon_provider_register(&_gadcon_class);
-   return 1;
+   return m;
 }
 
 EAPI int
