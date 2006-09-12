5e44424c6b7fabb729ae3de825608c3be763b3d4
David Walter Seikel <onefang@gmail.com>
2006-09-12 06:48:30 +0000

    TODO--
    SVN revision: 25758

--- xnest.sh
+++ xnest.sh
@@ -8,7 +8,9 @@ Xnest :1 -ac &
 sleep 2   # Someone reported that it starts E before X has started properly.
 
 # Comment out all but one of these.
-DISPLAY=:1 ; enlightenment &   # Just run it.
+#DISPLAY=:1 ; enlightenment   # Just run it.
 #DISPLAY=:1 ; xterm -e gdb -x gdb.txt enlightenment   # Run it with the text based debugger.
+DISPLAY=:1 ; gdb -x gdb.txt enlightenment   # Run it with the text based debugger.
 #DISPLAY=:1 ; ddd -geometry 550x450+5+25 enlightenment &   # Run it with the GUI debugger.
-#DISPLAY=:1 ; valgrind --tool=memcheck --log-file=valgrind_log enlightenment &   # Run it with valgrind.
+#DISPLAY=:1 ; valgrind --tool=memcheck --log-file=valgrind_log enlightenment    # Run it with valgrind.
+#DISPLAY=:1 ; valgrind --tool=memcheck --leak-check=yes --show-reachable=yes --log-file=valgrind_log enlightenment    # Run it with valgrind and leak checking.
