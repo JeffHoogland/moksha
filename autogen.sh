f0aa9bbbac78cad4d344237a50a5356fb9552c83
Kim Woelders <kim@woelders.dk>
2006-10-31 20:51:32 +0000

    Trying to sort out the gettext stuff. Had to disable pl and ko due to problems in make dist.
    SVN revision: 26887

--- autogen.sh
+++ autogen.sh
@@ -5,24 +5,12 @@ rm -f aclocal.m4 ltmain.sh
 
 touch README
 
+echo "Running autopoint..." ; autopoint -f || exit 1
 echo "Running aclocal..." ; aclocal $ACLOCAL_FLAGS -I m4 || exit 1
-echo "Running autoheader..." ; autoheader || exit 1
 echo "Running autoconf..." ; autoconf || exit 1
+echo "Running autoheader..." ; autoheader || exit 1
 echo "Running libtoolize..." ; (libtoolize --copy --automake || glibtoolize --automake) || exit 1
 echo "Running automake..." ; automake --add-missing --copy --gnu || exit 1
-echo "Generating gettext enlightenment.pot template"; \
-xgettext \
--n \
--C \
--d enlightenment \
--p po \
---copyright-holder="Enlightenment development team" \
---foreign-user \
---msgid-bugs-address="enlightenment-devel@lists.sourceforge.net" \
--k -k_ -kd_ -kN_ \
---from-code=UTF-8 \
--o enlightenment.pot \
-`find . -name "*.[ch]" -print` || exit 1
 
 if [ -z "$NOCONFIGURE" ]; then
 	./configure "$@"
