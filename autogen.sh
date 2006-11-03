e758e8b95af2db61903c2cbaee2d60b39e85bbd6
Kim Woelders <kim@woelders.dk>
2006-11-03 19:01:57 +0000

    If autopoint fails there is no reason to continue.
    SVN revision: 26930

--- autogen.sh
+++ autogen.sh
@@ -6,17 +6,11 @@ rm -f aclocal.m4 ltmain.sh
 touch README
 touch ABOUT-NLS
 
-# no more autopoint. cuases too many hassles. too bad gentoo users - go back
-# to automake 1.9 :(
-#echo "Running autopoint..." ; autopoint -f || exit 1
+echo "Running autopoint..." ; autopoint -f || exit 1
 echo "Running aclocal..." ; aclocal $ACLOCAL_FLAGS -I m4 || exit 1
 echo "Running autoconf..." ; autoconf || exit 1
 echo "Running autoheader..." ; autoheader || exit 1
 echo "Running libtoolize..." ; (libtoolize --copy --automake || glibtoolize --automake) || exit 1
-#echo "Running gettextize..." ; gettextize -f --no-changelog&
-# hack - gettextize is interactive and demands input from a user. "screw it".
-#sleep 20
-#kill %1
 echo "Running automake..." ; automake --add-missing --copy --gnu || exit 1
 
 if [ -z "$NOCONFIGURE" ]; then
