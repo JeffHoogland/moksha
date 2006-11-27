bc5935bf3fac4d9eba6e18bc9196939feba5133a
Christopher Michael <cpmichael1@comcast.net>
2006-11-27 12:58:46 +0000

    Remove commented-out old code.
    SVN revision: 27233

--- src/bin/e_widget_desk_preview.c
+++ src/bin/e_widget_desk_preview.c
@@ -262,8 +262,6 @@ _e_wid_desk_cb_config(void *data, Evas *e, Evas_Object *obj, void *event_info)
 	
 	con = e_container_current_get(e_manager_current_get());
 	e_int_config_desk(con, dd->container, dd->zone, dd->x, dd->y);
-	
-//	e_int_config_wallpaper_desk(dd->container, dd->zone, dd->x, dd->y);
      }
 }
 
