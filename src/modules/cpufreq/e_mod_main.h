aac2d328f72106a0872ea47800bcbc44e6f3b028
Sebastian Dransfeld <sd@tango.flipp.net>
2006-12-17 16:09:13 +0000

    No config function.
    SVN revision: 27496

--- src/modules/cpufreq/e_mod_main.h
+++ src/modules/cpufreq/e_mod_main.h
@@ -38,6 +38,5 @@ EAPI void *e_modapi_init     (E_Module *m);
 EAPI int   e_modapi_shutdown (E_Module *m);
 EAPI int   e_modapi_save     (E_Module *m);
 EAPI int   e_modapi_about    (E_Module *m);
-EAPI int   e_modapi_config   (E_Module *m);
 
 #endif
