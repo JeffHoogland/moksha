fb50d074bf6cd2595d055b83588a753fb7941625
Sebastian Dransfeld <sd@tango.flipp.net>
2007-03-25 17:36:21 +0000

    efreet
    SVN revision: 29134

--- src/bin/e_int_config_imc.c
+++ src/bin/e_int_config_imc.c
@@ -348,10 +348,10 @@ _basic_create_widgets(E_Config_Dialog *cfd, Evas *evas, E_Config_Dialog_Data *cf
 		  icon = NULL;
 		  if (imc->e_im_setup_exec) 
 		    {
-		       E_App *eap;
-		       eap = e_app_exe_find(imc->e_im_setup_exec);
-		       if (eap)
-			 icon = e_app_icon_add(eap, evas);
+		       Efreet_Desktop *desktop;
+		       desktop = efreet_util_desktop_exec_find(imc->e_im_setup_exec);
+		       if (desktop)
+			 icon = e_util_desktop_icon_add(desktop, "48x48", evas);
 		    }
 		  
 		  e_widget_ilist_append(cfdata->gui.imc_basic_list, icon, imc->e_im_name, NULL, NULL, imc_path);
