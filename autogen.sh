1a2f29834bcee1c35b10ad877f092e57e5dbc82f
Kim Woelders <kim@woelders.dk>
2007-03-25 19:11:49 +0000

    Seach local m4 directory first.
    SVN revision: 29145

--- autogen.sh
+++ autogen.sh
@@ -7,7 +7,7 @@ touch README
 touch ABOUT-NLS
 
 echo "Running autopoint..." ; autopoint -f || :
-echo "Running aclocal..." ; aclocal $ACLOCAL_FLAGS -I m4 || exit 1
+echo "Running aclocal..." ; aclocal -I m4 $ACLOCAL_FLAGS || exit 1
 echo "Running autoconf..." ; autoconf || exit 1
 echo "Running autoheader..." ; autoheader || exit 1
 echo "Running libtoolize..." ; (libtoolize --copy --automake || glibtoolize --automake) || exit 1
