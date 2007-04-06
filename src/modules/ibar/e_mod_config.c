9be8115006f0a1e83a8341ad890f72106de4007f
Sebastian Dransfeld <sd@tango.flipp.net>
2007-04-06 09:28:26 +0000

    Make configuration work.
    SVN revision: 29408

--- src/modules/ibar/e_mod_config.c
+++ src/modules/ibar/e_mod_config.c
@@ -186,10 +186,9 @@ _cb_config(void *data, void *data2)
    E_Config_Dialog_Data *cfdata;
    
    cfdata = data;
-   snprintf(path, sizeof(path), "%s/.e/e/applications/bar/%s", 
+   snprintf(path, sizeof(path), "%s/.e/e/applications/bar/%s/.order", 
 	    e_user_homedir_get(), cfdata->dir);
-//   e_int_config_apps_once(e_container_current_get(e_manager_current_get()), 
-//			  _("IBar"), NULL, path, NULL, NULL);
+   e_int_config_apps_ibar_other(e_container_current_get(e_manager_current_get()), path);
 }
 
 static void
