17a05b4caee26ce0513cc2d8cf0eb2b042c4d0d6
Christopher Michael <cpmichael1@comcast.net>
2007-04-17 09:23:21 +0000

    Make startup config dialog resizable.
    SVN revision: 29569

--- src/bin/e_int_config_startup.c
+++ src/bin/e_int_config_startup.c
@@ -336,13 +336,14 @@ _basic_create_widgets(E_Config_Dialog *cfd, Evas *evas, E_Config_Dialog_Data *cf
    if (cfdata->splash)
      f = cfdata->splash;
    e_widget_preview_edje_set(o, f, "e/init/splash");
-   e_widget_list_object_append(of, o, 1, 0, 0.5);
+   e_widget_list_object_append(of, o, 1, 1, 0.5);
    
-   e_widget_table_object_append(ot, of, 1, 0, 1, 1, 0, 1, 0, 1);
+   e_widget_table_object_append(ot, of, 1, 0, 1, 1, 1, 1, 1, 1);
    
    o = e_widget_check_add(evas, _("Show Splash Screen on Login"), 
 			  &(cfdata->show_splash));
    e_widget_table_object_append(ot, o, 1, 3, 1, 1, 1, 0, 0, 0);
 
+   e_dialog_resizable_set(cfd->dia, 1);
    return ot;
 }
