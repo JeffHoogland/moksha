fdbf463deb590bc10db30938d6393a157cb9ef44
Carsten Haitzler <raster@rasterman.com>
2007-06-03 03:51:33 +0000

    undef __USE_FILE_OFFSET64 - let features.h do it
    SVN revision: 30205

--- src/bin/e.h
+++ src/bin/e.h
@@ -15,10 +15,8 @@
 #ifndef _FILE_OFFSET_BITS
 #define _FILE_OFFSET_BITS  64
 #endif
-#ifndef __USE_FILE_OFFSET64 /* for large file support */
-#define __USE_FILE_OFFSET64
-#endif
 
+#include <features.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
