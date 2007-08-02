9a179d464d31dde856c50d5a4910b047354089b6
Miculcy Brian <morlenxus@gmx.net>
2007-08-02 23:40:58 +0000

    Added a configuration menu entry for the profiles dialog.
    SVN revision: 31124

--- src/modules/conf_profiles/e_mod_main.c
+++ src/modules/conf_profiles/e_mod_main.c
@@ -8,7 +8,11 @@
 /**/
 /* actual module specifics */
 
+static void  _e_mod_run_cb(void *data, E_Menu *m, E_Menu_Item *mi);
+static void  _e_mod_menu_add(void *data, E_Menu *m);
+
 static E_Module *conf_module = NULL;
+static E_Int_Menu_Augmentation *maug = NULL;
 
 /**/
 /***************************************************************************/
@@ -33,6 +37,8 @@ e_modapi_init(E_Module *m)
 {
    e_configure_registry_category_add("advanced", 80, _("Advanced"), NULL, "enlightenment/advanced");
    e_configure_registry_item_add("advanced/profiles", 50, _("Profiles"), NULL, "enlightenment/profiles", e_int_config_profiles);
+   maug = e_int_menus_menu_augmentation_add("config/1", _e_mod_menu_add, NULL, NULL, NULL);
+
    conf_module = m;
    e_module_delayed_set(m, 1);
    return m;
@@ -62,4 +68,23 @@ e_modapi_about(E_Module *m)
 			_("Enlightenment Configuration Module - Profiles"),
 			_("Configuration dialog for config profiles."));
    return 1;
+}
+
+/* menu item callback(s) */
+static void 
+_e_mod_run_cb(void *data, E_Menu *m, E_Menu_Item *mi)
+{
+   e_configure_registry_call("advanced/profiles", m->zone->container, NULL);
+}
+
+/* menu item add hook */
+static void
+_e_mod_menu_add(void *data, E_Menu *m)
+{
+   E_Menu_Item *mi;
+   
+   mi = e_menu_item_new(m);
+   e_menu_item_label_set(mi, _("Profiles"));
+   e_util_menu_item_edje_icon_set(mi, "enlightenment/profiles");
+   e_menu_item_callback_set(mi, _e_mod_run_cb, NULL);
 }
