eb16f16b48e1fb4f21f5777ecad49c37216fa6b4
Christopher Michael <cpmichael1@comcast.net>
2007-08-13 00:08:59 +0000

    Remove Profiles from the Config Submenu, doesn't need to be there. Fix some formatting.
    SVN revision: 31282

--- src/modules/conf_profiles/e_mod_main.c
+++ src/modules/conf_profiles/e_mod_main.c
@@ -4,27 +4,12 @@
 #include "e.h"
 #include "e_mod_main.h"
 
-/***************************************************************************/
-/**/
 /* actual module specifics */
-
 static void  _e_mod_run_cb(void *data, E_Menu *m, E_Menu_Item *mi);
 static void  _e_mod_menu_add(void *data, E_Menu *m);
 
 static E_Module *conf_module = NULL;
-static E_Int_Menu_Augmentation *maug = NULL;
 
-/**/
-/***************************************************************************/
-
-/***************************************************************************/
-/**/
-
-/**/
-/***************************************************************************/
-
-/***************************************************************************/
-/**/
 /* module setup */
 EAPI E_Module_Api e_modapi =
 {
@@ -35,10 +20,11 @@ EAPI E_Module_Api e_modapi =
 EAPI void *
 e_modapi_init(E_Module *m)
 {
-   e_configure_registry_category_add("advanced", 80, _("Advanced"), NULL, "enlightenment/advanced");
-   e_configure_registry_item_add("advanced/profiles", 50, _("Profiles"), NULL, "enlightenment/profiles", e_int_config_profiles);
-   maug = e_int_menus_menu_augmentation_add("config/1", _e_mod_menu_add, NULL, NULL, NULL);
-
+   e_configure_registry_category_add("advanced", 80, _("Advanced"), 
+				     NULL, "enlightenment/advanced");
+   e_configure_registry_item_add("advanced/profiles", 50, _("Profiles"), 
+				 NULL, "enlightenment/profiles", 
+				 e_int_config_profiles);
    conf_module = m;
    e_module_delayed_set(m, 1);
    return m;
@@ -48,13 +34,9 @@ EAPI int
 e_modapi_shutdown(E_Module *m)
 {
    E_Config_Dialog *cfd;
-   /* remove module-supplied menu additions */
-   if (maug)
-     {
-	e_int_menus_menu_augmentation_del("config/1", maug);
-	maug = NULL;
-     }
-   while ((cfd = e_config_dialog_get("E", "_config_profiles_dialog"))) e_object_del(E_OBJECT(cfd));
+
+   while ((cfd = e_config_dialog_get("E", "_config_profiles_dialog"))) 
+     e_object_del(E_OBJECT(cfd));
    e_configure_registry_item_del("advanced/profiles");
    e_configure_registry_category_del("advanced");
    conf_module = NULL;
