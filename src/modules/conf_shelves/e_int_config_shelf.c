252965d8e182d84c792fb0f98e453a934ea213e0
Christopher Michael <cpmichael1@comcast.net>
2007-12-18 01:49:45 +0000

    Formatting fixes & whitespace removal.
    SVN revision: 33176

--- src/modules/conf_shelves/e_int_config_shelf.c
+++ src/modules/conf_shelves/e_int_config_shelf.c
@@ -2,7 +2,7 @@
 
 static void *_create_data(E_Config_Dialog *cfd);
 static void  _free_data(E_Config_Dialog *cfd, E_Config_Dialog_Data *cfdata);
-static Evas_Object *_basic_create_widgets(E_Config_Dialog *cfd, Evas *evas, E_Config_Dialog_Data *cfdata);
+static Evas_Object *_basic_create(E_Config_Dialog *cfd, Evas *evas, E_Config_Dialog_Data *cfdata);
 static void _ilist_fill(E_Config_Dialog_Data *cfdata);
 static void _ilist_cb_selected(void *data);
 static void _cb_add(void *data, void *data2);
@@ -16,7 +16,7 @@ struct _E_Config_Dialog_Data
    Evas_Object *o_list;
    Evas_Object *o_delete;
    Evas_Object *o_config;
-   
+
    char *cur_shelf;
 };
 
@@ -26,24 +26,22 @@ struct _Shelf_Del_Confirm_Data
     E_Config_Dialog_Data *cfdata;
     E_Shelf *es;
 };
-    
 
 EAPI E_Config_Dialog *
 e_int_config_shelf(E_Container *con, const char *params __UNUSED__) 
 {
    E_Config_Dialog *cfd;
    E_Config_Dialog_View *v;
-   
+
    if (e_config_dialog_find("E", "_config_shelf_dialog")) return NULL;
    v = E_NEW(E_Config_Dialog_View, 1);
    if (!v) return NULL; 
    v->create_cfdata = _create_data;
    v->free_cfdata = _free_data;
-   v->basic.create_widgets = _basic_create_widgets;
+   v->basic.create_widgets = _basic_create;
 
-   cfd = e_config_dialog_new(con,
-			     _("Shelf Settings"),
-			    "E", "_config_shelf_dialog",
+   cfd = e_config_dialog_new(con, _("Shelf Settings"), "E", 
+			     "_config_shelf_dialog",
 			     "enlightenment/shelf", 0, v, NULL);
    return cfd;
 }
@@ -52,7 +50,7 @@ static void *
 _create_data(E_Config_Dialog *cfd) 
 {
    E_Config_Dialog_Data *cfdata;
-   
+
    cfdata = E_NEW(E_Config_Dialog_Data, 1);
    return cfdata;
 }
@@ -64,34 +62,36 @@ _free_data(E_Config_Dialog *cfd, E_Config_Dialog_Data *cfdata)
 }
 
 static Evas_Object *
-_basic_create_widgets(E_Config_Dialog *cfd, Evas *evas, E_Config_Dialog_Data *cfdata) 
+_basic_create(E_Config_Dialog *cfd, Evas *evas, E_Config_Dialog_Data *cfdata) 
 {
    Evas_Object *o, *of, *ot, *ob;
-   
+
    o = e_widget_list_add(evas, 0, 1);
-   
+
    of = e_widget_framelist_add(evas, _("Configured Shelves"), 0);
    cfdata->o_list = e_widget_ilist_add(evas, 24, 24, &(cfdata->cur_shelf));
    e_widget_ilist_selector_set(cfdata->o_list, 1);
    e_widget_min_size_set(cfdata->o_list, 155, 250);
    e_widget_framelist_object_append(of, cfdata->o_list);   
    e_widget_list_object_append(o, of, 1, 1, 0.5);
-   
+
    ot = e_widget_table_add(evas, 0);
    ob = e_widget_button_add(evas, _("Add"), "widget/add", _cb_add, cfdata, NULL);
    e_widget_table_object_append(ot, ob, 0, 0, 1, 1, 1, 1, 0, 0);
-   cfdata->o_delete = e_widget_button_add(evas, _("Delete"), "widget/del", _cb_delete, cfdata, NULL);
+   cfdata->o_delete = e_widget_button_add(evas, _("Delete"), "widget/del", 
+					  _cb_delete, cfdata, NULL);
    e_widget_table_object_append(ot, cfdata->o_delete, 0, 1, 1, 1, 1, 1, 0, 0);
-   cfdata->o_config = e_widget_button_add(evas, _("Configure"), "widget/config", _cb_config, cfdata, NULL);
+   cfdata->o_config = e_widget_button_add(evas, _("Configure"), "widget/config", 
+					  _cb_config, cfdata, NULL);
    e_widget_table_object_append(ot, cfdata->o_config, 0, 2, 1, 1, 1, 1, 0, 0);
 
    e_widget_disabled_set(cfdata->o_delete, 1);
    e_widget_disabled_set(cfdata->o_config, 1);
-   
+
    e_widget_list_object_append(o, ot, 1, 0, 0.0);
-   
+
    _ilist_fill(cfdata);
-   
+
    e_dialog_resizable_set(cfd->dia, 1);
    return o;
 }
@@ -104,35 +104,34 @@ _ilist_fill(E_Config_Dialog_Data *cfdata)
    Evas_List *l;
    int n = -1;
    char buf[256];
-   
+
    if (!cfdata) return;
    if (!cfdata->o_list) return;
-   
+
    evas = evas_object_evas_get(cfdata->o_list);
-   
+
    if (e_widget_ilist_count(cfdata->o_list) > 0) 
      n = e_widget_ilist_selected_get(cfdata->o_list);
 
    evas_event_freeze(evas);
    edje_freeze();
    e_widget_ilist_freeze(cfdata->o_list);
-   
    e_widget_ilist_clear(cfdata->o_list);
    e_widget_ilist_go(cfdata->o_list);
-   
+
    for (l = e_shelf_list(); l; l = l->next) 
      {
 	E_Shelf *es;
 	Evas_Object *ob;
 	const char *label;
-	
+
 	es = l->data;
 	if (!es) continue;
-	
+
 	label = es->name;
 	if (!label) label = _("Shelf #");
 	snprintf(buf, sizeof(buf), "%s %i", label, es->id);
-	
+
 	ob = edje_object_add(evas);
         switch (es->cfg->orient)
           {
@@ -176,16 +175,16 @@ _ilist_fill(E_Config_Dialog_Data *cfdata)
              e_util_edje_icon_set(ob, "enlightenment/e");
              break;
           }
-	e_widget_ilist_append(cfdata->o_list, ob, buf, _ilist_cb_selected, cfdata, buf);
+	e_widget_ilist_append(cfdata->o_list, ob, buf, 
+			      _ilist_cb_selected, cfdata, buf);
      }
-   
+
    e_widget_min_size_set(cfdata->o_list, 155, 250);
    e_widget_ilist_go(cfdata->o_list);
-
    e_widget_ilist_thaw(cfdata->o_list);
    edje_thaw();
    evas_event_thaw(evas);
-   
+
    if (n > -1) 
      {
 	e_widget_disabled_set(cfdata->o_delete, 0);
@@ -203,10 +202,10 @@ static void
 _ilist_cb_selected(void *data) 
 {
    E_Config_Dialog_Data *cfdata;
-   
+
    cfdata = data;
    if (!cfdata) return;
-   
+
    e_widget_disabled_set(cfdata->o_delete, 0);
    e_widget_disabled_set(cfdata->o_config, 0);
 }
@@ -218,13 +217,13 @@ _cb_add(void *data, void *data2)
    E_Config_Shelf *cfg;
    E_Container *con;
    E_Zone *zone;
-   
+
    cfdata = data;
    if (!cfdata) return;
-   
+
    con = e_container_current_get(e_manager_current_get());
    zone = e_zone_current_get(con);
-   
+
    cfg = E_NEW(E_Config_Shelf, 1);
    cfg->name = evas_stringshare_add("shelf");
    cfg->container = con->num;
@@ -242,7 +241,7 @@ _cb_add(void *data, void *data2)
    e_config_save_queue();
 
    e_shelf_config_init();
-   
+
    _ilist_fill(cfdata);
 }
 
@@ -251,23 +250,24 @@ _cb_delete(void *data, void *data2)
 {
    Shelf_Del_Confirm_Data *d;
    char buf[4096];
-   
+
    d = E_NEW(Shelf_Del_Confirm_Data, 1);
    if (!d) return;
    d->cfdata = data;
    if (!d->cfdata) return;
    if (!d->cfdata->cur_shelf) return;
-   d->es = evas_list_nth(e_shelf_list(), e_widget_ilist_selected_get(d->cfdata->o_list));
+   d->es = evas_list_nth(e_shelf_list(), 
+			 e_widget_ilist_selected_get(d->cfdata->o_list));
    if (!d->es) return;
    e_object_ref(E_OBJECT(d->es));
-   
+
    snprintf(buf, sizeof(buf), _("You requested to delete \"%s\".<br><br>"
 				"Are you sure you want to delete this shelf?"),
-                                d->cfdata->cur_shelf);
-   
+	    d->cfdata->cur_shelf);
+
    e_confirm_dialog_show(_("Are you sure you want to delete this shelf?"), 
-			   "enlightenment/exit", buf, NULL, NULL, _cb_dialog_yes, NULL, d, NULL, 
-                           _cb_dialog_destroy, d);
+			 "enlightenment/exit", buf, NULL, NULL, 
+			 _cb_dialog_yes, NULL, d, NULL, _cb_dialog_destroy, d);
 }
 
 static void 
@@ -300,11 +300,12 @@ _cb_config(void *data, void *data2)
 {
    E_Config_Dialog_Data *cfdata;
    E_Shelf *es;
-   
+
    cfdata = data;
    if (!cfdata) return;
-   
-   es = evas_list_nth(e_shelf_list(), e_widget_ilist_selected_get(cfdata->o_list));
+
+   es = evas_list_nth(e_shelf_list(), 
+		      e_widget_ilist_selected_get(cfdata->o_list));
    if (!es) return;
    if (!es->config_dialog) e_int_shelf_config(es);
 }
