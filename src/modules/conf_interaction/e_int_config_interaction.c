a25642b6027e1732baf8546c4f10c654982fb3bb
Chidambar Zinnoury <illogict@online.fr>
2008-10-07 05:57:37 +0000

    Some work on interaction settings: - group on a thumbscroll framelist; - threshhold → threshold; - add check changed.
    SVN revision: 36490

--- src/modules/conf_interaction/e_int_config_interaction.c
+++ src/modules/conf_interaction/e_int_config_interaction.c
@@ -2,6 +2,7 @@
 
 static void        *_create_data(E_Config_Dialog *cfd);
 static void        _free_data(E_Config_Dialog *cfd, E_Config_Dialog_Data *cfdata);
+static int         _basic_check_changed(E_Config_Dialog *cfd, E_Config_Dialog_Data *cfdata);
 static int         _basic_apply_data(E_Config_Dialog *cfd, E_Config_Dialog_Data *cfdata);
 static Evas_Object *_basic_create_widgets(E_Config_Dialog *cfd, Evas *evas, E_Config_Dialog_Data *cfdata);
 
@@ -29,6 +30,7 @@ e_int_config_interaction(E_Container *con, const char *params __UNUSED__)
    v->free_cfdata = _free_data;
    v->basic.apply_cfdata = _basic_apply_data;
    v->basic.create_widgets = _basic_create_widgets;
+   v->basic.check_changed = _basic_check_changed;
    v->override_auto_apply = 1;
    
    cfd = e_config_dialog_new(con,
@@ -64,6 +66,15 @@ _free_data(E_Config_Dialog *cfd, E_Config_Dialog_Data *cfdata)
 }
 
 static int
+_basic_check_changed(E_Config_Dialog *cfd, E_Config_Dialog_Data *cfdata) 
+{
+   return ((cfdata->thumbscroll_enable != e_config->thumbscroll_enable) ||
+	   (cfdata->thumbscroll_threshhold != e_config->thumbscroll_threshhold) ||
+	   (cfdata->thumbscroll_momentum_threshhold != e_config->thumbscroll_momentum_threshhold) ||
+	   (cfdata->thumbscroll_friction != e_config->thumbscroll_friction));
+}
+
+static int
 _basic_apply_data(E_Config_Dialog *cfd, E_Config_Dialog_Data *cfdata) 
 {
    e_config->thumbscroll_enable = cfdata->thumbscroll_enable;
@@ -77,35 +88,36 @@ _basic_apply_data(E_Config_Dialog *cfd, E_Config_Dialog_Data *cfdata)
 static Evas_Object *
 _basic_create_widgets(E_Config_Dialog *cfd, Evas *evas, E_Config_Dialog_Data *cfdata) 
 {
-   Evas_Object *o, *ob;
+   Evas_Object *o, *of, *ob;
  
    o = e_widget_list_add(evas, 0, 0);
+   of = e_widget_framelist_add(evas, _("Thumbscroll"), 0);
 
    ob = e_widget_check_add(evas, _("Enable Thumbscroll"), &(cfdata->thumbscroll_enable));
-   e_widget_list_object_append(o, ob, 1, 1, 0.5);
+   e_widget_framelist_object_append(of, ob);
    evas_object_smart_callback_add(ob, "changed", _enabled_cb, cfdata);
  
-   ob = e_widget_label_add(evas, _("Threshhold for a thumb drag"));
-   e_widget_list_object_append(o, ob, 1, 1, 0.5);
+   ob = e_widget_label_add(evas, _("Threshold for a thumb drag"));
+   e_widget_framelist_object_append(of, ob);
    cfdata->l1 = ob;
    ob = e_widget_slider_add(evas, 1, 0, _("%1.0f pixels"), 0, 64, 4, 0, NULL, &(cfdata->thumbscroll_threshhold), 200);
-   e_widget_list_object_append(o, ob, 1, 1, 0.5);
+   e_widget_framelist_object_append(of, ob);
    cfdata->sl1 = ob;
    
-   ob = e_widget_label_add(evas, _("Threshhold for for applying drag momentum"));
-   e_widget_list_object_append(o, ob, 1, 1, 0.5);
+   ob = e_widget_label_add(evas, _("Threshold for for applying drag momentum"));
+   e_widget_framelist_object_append(of, ob);
    cfdata->l2 = ob;
    ob = e_widget_slider_add(evas, 1, 0, _("%1.0f pixels/sec"), 0, 2000, 20, 0, &(cfdata->thumbscroll_momentum_threshhold), NULL, 200);
-   e_widget_list_object_append(o, ob, 1, 1, 0.5);
+   e_widget_framelist_object_append(of, ob);
    cfdata->sl2 = ob;
    
    ob = e_widget_label_add(evas, _("Friction slowdown"));
-   e_widget_list_object_append(o, ob, 1, 1, 0.5);
+   e_widget_framelist_object_append(of, ob);
    cfdata->l3 = ob;
    ob = e_widget_slider_add(evas, 1, 0, _("%1.2f sec"), 0.00, 5.0, 0.1, 0, &(cfdata->thumbscroll_friction), NULL, 200);
-   e_widget_list_object_append(o, ob, 1, 1, 0.5);
+   e_widget_framelist_object_append(of, ob);
    cfdata->sl3 = ob;
-   
+
    if (!e_config->thumbscroll_enable)
      {
 	e_widget_disabled_set(cfdata->l1, 1);
@@ -115,6 +127,9 @@ _basic_create_widgets(E_Config_Dialog *cfd, Evas *evas, E_Config_Dialog_Data *cf
 	e_widget_disabled_set(cfdata->l3, 1);
 	e_widget_disabled_set(cfdata->sl3, 1);
      }
+
+   e_widget_list_object_append(o, of, 1, 1, 0.5);
+
    return o;
 }
 
@@ -124,22 +139,11 @@ _enabled_cb(void *data, Evas_Object *obj, void *event_info)
    E_Config_Dialog_Data *cfdata;
 
    cfdata = data;
-   if (!cfdata->thumbscroll_enable)
-     {
-	e_widget_disabled_set(cfdata->l1, 1);
-	e_widget_disabled_set(cfdata->sl1, 1);
-	e_widget_disabled_set(cfdata->l2, 1);
-	e_widget_disabled_set(cfdata->sl2, 1);
-	e_widget_disabled_set(cfdata->l3, 1);
-	e_widget_disabled_set(cfdata->sl3, 1);
-     }
-   else
-     {
-	e_widget_disabled_set(cfdata->l1, 0);
-	e_widget_disabled_set(cfdata->sl1, 0);
-	e_widget_disabled_set(cfdata->l2, 0);
-	e_widget_disabled_set(cfdata->sl2, 0);
-	e_widget_disabled_set(cfdata->l3, 0);
-	e_widget_disabled_set(cfdata->sl3, 0);
-     }
+
+   e_widget_disabled_set(cfdata->l1, !cfdata->thumbscroll_enable);
+   e_widget_disabled_set(cfdata->sl1, !cfdata->thumbscroll_enable);
+   e_widget_disabled_set(cfdata->l2, !cfdata->thumbscroll_enable);
+   e_widget_disabled_set(cfdata->sl2, !cfdata->thumbscroll_enable);
+   e_widget_disabled_set(cfdata->l3, !cfdata->thumbscroll_enable);
+   e_widget_disabled_set(cfdata->sl3, !cfdata->thumbscroll_enable);
 }
