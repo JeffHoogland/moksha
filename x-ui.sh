be2fe2e6899a3e4a9c3041f34435bc6a4fe02e8f
Carsten Haitzler <raster@rasterman.com>
2008-11-25 08:09:19 +0000

    and fix x-ui.sh to be default again and... fix themr to have right icon max sizes.
    SVN revision: 37798

--- x-ui.sh
+++ x-ui.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-#Xephyr :1 -noreset -ac -br -dpi 284 -screen 480x640x16 &
+Xephyr :1 -noreset -ac -br -dpi 284 -screen 480x640x16 &
 #Xephyr :1 -noreset -ac -br -dpi 284 -screen 480x640 &
 #Xephyr :1 -noreset -ac -br -dpi 284 -screen 640x480x16 &
 #Xephyr :1 -noreset -ac -br -dpi 284 -screen 640x480 &
-Xephyr :1 -noreset -ac -br -dpi 181 -screen 320x320x16 &
+#Xephyr :1 -noreset -ac -br -dpi 181 -screen 320x320x16 &
 #Xephyr :1 -noreset -ac -br -dpi 181 -screen 320x320 &
 #Xephyr :1 -noreset -ac -br -dpi 186 -screen 272x480x16 &
 #Xephyr :1 -noreset -ac -br -dpi 186 -screen 272x480 &
@@ -14,5 +14,5 @@ Xephyr :1 -noreset -ac -br -dpi 181 -screen 320x320x16 &
 sleep 1
 export DISPLAY=:1
 unset E_RESTART E_START E_IPC_SOCKET E_START_TIME
-E_CONF_PROFILE=default enlightenment_start
-#enlightenment_start -profile illume
+#E_CONF_PROFILE=default enlightenment_start
+enlightenment_start -profile illume
