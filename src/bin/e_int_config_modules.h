c15693083ec77cc9730988e797790488df226f4d
Cedric BAIL <cedric.bail@free.fr>
2008-12-17 16:17:08 +0000

    Remove __UNUSED__ as it doesn't make sense here.
    SVN revision: 38188

--- src/bin/e_int_config_modules.h
+++ src/bin/e_int_config_modules.h
@@ -6,7 +6,7 @@
 #ifndef E_INT_CONFIG_MODULES_H
 #define E_INT_CONFIG_MODULES_H
 
-EAPI E_Config_Dialog *e_int_config_modules(E_Container *con, const char *params __UNUSED__);
+EAPI E_Config_Dialog *e_int_config_modules(E_Container *con, const char *params);
 
 #endif
 #endif
