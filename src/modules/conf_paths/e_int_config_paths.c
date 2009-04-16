df07dc580712c09dc9fb9c31f0e079b1136581c4
Andreas Volz <linux@brachttal.net>
2009-04-16 20:03:32 +0000

    the ilist was to small
    SVN revision: 40116

--- src/modules/conf_paths/e_int_config_paths.c
+++ src/modules/conf_paths/e_int_config_paths.c
@@ -168,7 +168,7 @@ _basic_create_widgets(E_Config_Dialog *cfd, Evas *evas, E_Config_Dialog_Data *cf
    of = e_widget_framelist_add(evas, _("E Paths"), 0);
    ob = e_widget_ilist_add(evas, 0, 0, NULL);
    cfdata->gui.path_list = ob;
-   e_widget_min_size_set(ob, 100, 100);
+   e_widget_min_size_set(ob, 170, 100);
 
    evas_event_freeze(evas_object_evas_get(cfdata->gui.path_list));
    edje_freeze();
