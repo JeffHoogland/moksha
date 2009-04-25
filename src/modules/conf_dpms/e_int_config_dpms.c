4d0e5d64df10ae2932de8fddc336975d78639c2f
Gustavo Sverzut Barbieri <barbieri@gmail.com>
2009-04-25 16:10:34 +0000

    whitespace-cleanup
    SVN revision: 40376

--- src/modules/conf_dpms/e_int_config_dpms.c
+++ src/modules/conf_dpms/e_int_config_dpms.c
@@ -18,11 +18,11 @@ static void _cb_disable_check_list(void *data, Evas_Object *obj);
 struct _E_Config_Dialog_Data
 {
    E_Config_Dialog *cfd;
-   
+
    Evas_Object *standby_slider;
    Evas_Object *suspend_slider;
    Evas_Object *off_slider;
-   
+
    int enable_dpms;
    int enable_standby;
    int enable_suspend;
@@ -31,8 +31,8 @@ struct _E_Config_Dialog_Data
    /*
     * The following timeouts are represented as minutes
     * while the underlying e_config variables are in seconds
-    */ 
-   double standby_timeout;  
+    */
+   double standby_timeout;
    double suspend_timeout;
    double off_timeout;
 };
@@ -51,9 +51,9 @@ static int
 _e_int_config_dpms_capable(void)
 {
    if (ecore_x_dpms_capable_get()) return 1;
-   
+
    if (dpms_dialog) e_object_del(E_OBJECT(dpms_dialog));
-   dpms_dialog = e_dialog_new(e_container_current_get(e_manager_current_get()), 
+   dpms_dialog = e_dialog_new(e_container_current_get(e_manager_current_get()),
 			      "E", "_dpms_capable_dialog");
    if (!dpms_dialog) return 0;
 
@@ -72,9 +72,9 @@ static int
 _e_int_config_dpms_available(void)
 {
    if (ecore_x_dpms_query()) return 1;
-   
+
    if (dpms_dialog) e_object_del(E_OBJECT(dpms_dialog));
-   dpms_dialog = e_dialog_new(e_container_current_get(e_manager_current_get()), 
+   dpms_dialog = e_dialog_new(e_container_current_get(e_manager_current_get()),
 			      "E", "_dpms_available_dialog");
    if (!dpms_dialog) return 0;
 
@@ -94,23 +94,23 @@ e_int_config_dpms(E_Container *con, const char *params __UNUSED__)
 {
    E_Config_Dialog *cfd;
    E_Config_Dialog_View *v;
-   
+
    if ((e_config_dialog_find("E", "_config_dpms_dialog")) ||
-       (!_e_int_config_dpms_available()) || 		   
+       (!_e_int_config_dpms_available()) ||
        (!_e_int_config_dpms_capable()))
      return NULL;
 
    v = E_NEW(E_Config_Dialog_View, 1);
-   
+
    v->create_cfdata = _create_data;
    v->free_cfdata = _free_data;
    v->basic.apply_cfdata = _advanced_apply_data;
    v->basic.create_widgets = _advanced_create_widgets;
    v->basic.check_changed = _advanced_check_changed;
    v->override_auto_apply = 1;
-   
-   cfd = e_config_dialog_new(con, _("Display Power Management Settings"), "E", 
-			     "_config_dpms_dialog", "preferences-system-power-management", 
+
+   cfd = e_config_dialog_new(con, _("Display Power Management Settings"), "E",
+			     "_config_dpms_dialog", "preferences-system-power-management",
 			     0, v, NULL);
    return cfd;
 }
@@ -134,11 +134,11 @@ _create_data(E_Config_Dialog *cfd)
 
    cfdata = E_NEW(E_Config_Dialog_Data, 1);
    cfdata->cfd = cfd;
-   
+
    cfdata->standby_slider = NULL;
    cfdata->suspend_slider = NULL;
    cfdata->off_slider = NULL;
-   
+
    _fill_data(cfdata);
    return cfdata;
 }
@@ -157,7 +157,7 @@ _apply_data(E_Config_Dialog *cfd, E_Config_Dialog_Data *cfdata)
    e_config->dpms_enable = cfdata->enable_dpms;
    e_config->dpms_standby_enable = cfdata->enable_standby;
    e_config->dpms_suspend_enable = cfdata->enable_suspend;
-   e_config->dpms_off_enable = cfdata->enable_off;   
+   e_config->dpms_off_enable = cfdata->enable_off;
 
    e_config->dpms_standby_timeout = cfdata->standby_timeout * 60;
    e_config->dpms_suspend_timeout = cfdata->suspend_timeout * 60;
@@ -196,29 +196,29 @@ _advanced_create_widgets(E_Config_Dialog *cfd, Evas *evas, E_Config_Dialog_Data 
 
    o = e_widget_list_add(evas, 0, 0);
 
-   dpms_check = e_widget_check_add(evas, _("Enable Display Power Management"), 
+   dpms_check = e_widget_check_add(evas, _("Enable Display Power Management"),
 			   &(cfdata->enable_dpms));
-   e_widget_list_object_append(o, dpms_check, 1, 1, 0);   
-   
+   e_widget_list_object_append(o, dpms_check, 1, 1, 0);
+
    of = e_widget_framelist_add(evas, _("Timers"), 0);
 
    ob = e_widget_check_add(evas, _("Standby time"), &(cfdata->enable_standby));
    e_widget_framelist_object_append(of, ob);
    dpms_list = eina_list_append (dpms_list, ob);
    e_widget_disabled_set(ob, !cfdata->enable_standby); // set state from saved config
-   ob = e_widget_slider_add(evas, 1, 0, _("%1.0f minutes"), 1.0, 90.0, 1.0, 0, 
+   ob = e_widget_slider_add(evas, 1, 0, _("%1.0f minutes"), 1.0, 90.0, 1.0, 0,
 			    &(cfdata->standby_timeout), NULL, 200);
-   e_widget_on_change_hook_set(ob, _cb_standby_slider_change, cfdata);   
+   e_widget_on_change_hook_set(ob, _cb_standby_slider_change, cfdata);
    cfdata->standby_slider = ob;
    dpms_list = eina_list_append (dpms_list, ob);
    e_widget_disabled_set(ob, !cfdata->enable_standby); // set state from saved config
    e_widget_framelist_object_append(of, ob);
-   
+
    ob = e_widget_check_add(evas, _("Suspend time"), &(cfdata->enable_suspend));
    e_widget_framelist_object_append(of, ob);
    dpms_list = eina_list_append (dpms_list, ob);
    e_widget_disabled_set(ob, !cfdata->enable_standby); // set state from saved config
-   ob = e_widget_slider_add(evas, 1, 0, _("%1.0f minutes"), 1.0, 90.0, 1.0, 0, 
+   ob = e_widget_slider_add(evas, 1, 0, _("%1.0f minutes"), 1.0, 90.0, 1.0, 0,
 			    &(cfdata->suspend_timeout), NULL, 200);
    e_widget_on_change_hook_set(ob, _cb_suspend_slider_change, cfdata);
    cfdata->suspend_slider = ob;
@@ -230,7 +230,7 @@ _advanced_create_widgets(E_Config_Dialog *cfd, Evas *evas, E_Config_Dialog_Data 
    e_widget_framelist_object_append(of, ob);
    dpms_list = eina_list_append (dpms_list, ob);
    e_widget_disabled_set(ob, !cfdata->enable_standby); // set state from saved config
-   ob = e_widget_slider_add(evas, 1, 0, _("%1.0f minutes"), 1.0, 90.0, 1.0, 0, 
+   ob = e_widget_slider_add(evas, 1, 0, _("%1.0f minutes"), 1.0, 90.0, 1.0, 0,
 			    &(cfdata->off_timeout), NULL, 200);
    e_widget_on_change_hook_set(ob, _cb_off_slider_change, cfdata);
    cfdata->off_slider = ob;
@@ -242,7 +242,7 @@ _advanced_create_widgets(E_Config_Dialog *cfd, Evas *evas, E_Config_Dialog_Data 
    e_widget_on_change_hook_set(dpms_check, _cb_disable_check_list, dpms_list);
 
    e_widget_list_object_append(o, of, 1, 1, 0.5);
-   return o;   
+   return o;
 }
 
 /* general functionality/callbacks */
@@ -256,14 +256,14 @@ _cb_standby_slider_change(void *data, Evas_Object *obj)
      {
 	cfdata->suspend_timeout = cfdata->standby_timeout;
 	if (cfdata->suspend_slider)
-	  e_widget_slider_value_double_set(cfdata->suspend_slider, 
+	  e_widget_slider_value_double_set(cfdata->suspend_slider,
 					   cfdata->suspend_timeout);
-	
+
 	if (cfdata->suspend_timeout > cfdata->off_timeout)
 	  {
 	     cfdata->off_timeout = cfdata->suspend_timeout;
 	     if (cfdata->off_slider)
-	       e_widget_slider_value_double_set(cfdata->off_slider, 
+	       e_widget_slider_value_double_set(cfdata->off_slider,
 						cfdata->off_timeout);
 	  }
      }
@@ -279,14 +279,14 @@ _cb_suspend_slider_change(void *data, Evas_Object *obj)
      {
 	cfdata->off_timeout = cfdata->suspend_timeout;
 	if (cfdata->off_slider)
-	  e_widget_slider_value_double_set(cfdata->off_slider, 
+	  e_widget_slider_value_double_set(cfdata->off_slider,
 					   cfdata->off_timeout);
      }
    if (cfdata->suspend_timeout < cfdata->standby_timeout)
-     {   
+     {
 	cfdata->standby_timeout = cfdata->suspend_timeout;
 	if (cfdata->standby_slider)
-	  e_widget_slider_value_double_set(cfdata->standby_slider, 
+	  e_widget_slider_value_double_set(cfdata->standby_slider,
 					   cfdata->standby_timeout);
      }
 }
@@ -301,14 +301,14 @@ _cb_off_slider_change(void *data, Evas_Object *obj)
      {
 	cfdata->suspend_timeout = cfdata->off_timeout;
 	if (cfdata->suspend_slider)
-	  e_widget_slider_value_double_set(cfdata->suspend_slider, 
+	  e_widget_slider_value_double_set(cfdata->suspend_slider,
 					   cfdata->suspend_timeout);
-	
+
 	if (cfdata->suspend_timeout < cfdata->standby_timeout)
-	  {   
+	  {
 	     cfdata->standby_timeout = cfdata->suspend_timeout;
 	     if (cfdata->standby_slider)
-	       e_widget_slider_value_double_set(cfdata->standby_slider, 
+	       e_widget_slider_value_double_set(cfdata->standby_slider,
 						cfdata->standby_timeout);
 	  }
      }
