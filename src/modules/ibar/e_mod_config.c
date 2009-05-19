575c1f43a0829f5ba8cc8495693a4d3127ba6100
Christopher Michael <cpmichael1@comcast.net>
2009-05-19 22:53:46 +0000

    Formatting & Whitespace Removal
    SVN revision: 40753

--- src/modules/ibar/e_mod_config.c
+++ src/modules/ibar/e_mod_config.c
@@ -8,9 +8,8 @@
 struct _E_Config_Dialog_Data
 {
    const char *dir;
-   int   show_label;
-   int   eap_label;
-   
+   int show_label, eap_label;
+
    Evas_Object *tlist;
    Evas_Object *radio_name;
    Evas_Object *radio_comment;
@@ -38,7 +37,7 @@ _config_ibar_module(Config_Item *ci)
    E_Config_Dialog *cfd;
    E_Config_Dialog_View *v;
    char buf[4096];
-   
+
    v = E_NEW(E_Config_Dialog_View, 1);
 
    /* Dialog Methods */
@@ -49,7 +48,9 @@ _config_ibar_module(Config_Item *ci)
    v->advanced.apply_cfdata = NULL;
    v->advanced.create_widgets = NULL;
 
-   snprintf(buf, sizeof(buf), "%s/e-module-ibar.edj", e_module_dir_get(ibar_config->module));
+   snprintf(buf, sizeof(buf), "%s/e-module-ibar.edj", 
+            e_module_dir_get(ibar_config->module));
+
    /* Create The Dialog */
    cfd = e_config_dialog_new(e_container_current_get(e_manager_current_get()),
 			     _("IBar Settings"),
@@ -74,7 +75,7 @@ _create_data(E_Config_Dialog *cfd)
 {
    E_Config_Dialog_Data *cfdata;
    Config_Item *ci;
-   
+
    ci = cfd->data;
    cfdata = E_NEW(E_Config_Dialog_Data, 1);
    _fill_data(ci, cfdata);
@@ -95,16 +96,16 @@ _basic_create_widgets(E_Config_Dialog *cfd, Evas *evas, E_Config_Dialog_Data *cf
 {
    Evas_Object *o, *of, *ol, *ob, *ot;
    E_Radio_Group *rg;
-   
+
    o = e_widget_list_add(evas, 0, 0);
-   
+
    of = e_widget_frametable_add(evas, _("Selected Bar Source"), 0);
    ol = e_widget_ilist_add(evas, 32, 32, &(cfdata->dir));
    cfdata->tlist = ol;
    _load_tlist(cfdata);
    e_widget_min_size_set(ol, 140, 140);
    e_widget_frametable_object_append(of, ol, 0, 0, 1, 2, 1, 1, 1, 0);
-   
+
    ot = e_widget_table_add(evas, 0);
    ob = e_widget_button_add(evas, _("Add"), "list-add", _cb_add, cfdata, NULL);
    e_widget_table_object_append(ot, ob, 0, 0, 1, 1, 1, 1, 1, 0);
@@ -115,15 +116,15 @@ _basic_create_widgets(E_Config_Dialog *cfd, Evas *evas, E_Config_Dialog_Data *cf
 
    if (!e_configure_registry_exists("applications/ibar_applications")) 
      e_widget_disabled_set(ob, 1);
-   
+
    e_widget_frametable_object_append(of, ot, 1, 0, 1, 1, 1, 1, 1, 0);
    e_widget_list_object_append(o, of, 1, 1, 0.5);
-   
+
    of = e_widget_framelist_add(evas, _("Icon Labels"), 0);
    ob = e_widget_check_add(evas, _("Show Icon Label"), &(cfdata->show_label));
    e_widget_on_change_hook_set(ob, _show_label_cb_change, cfdata);
    e_widget_framelist_object_append(of, ob);  
-   
+
    rg = e_widget_radio_group_new(&(cfdata->eap_label));
 
    cfdata->radio_name = e_widget_radio_add(evas, _("Display App Name"), 0, rg);
@@ -133,11 +134,11 @@ _basic_create_widgets(E_Config_Dialog *cfd, Evas *evas, E_Config_Dialog_Data *cf
    cfdata->radio_comment = e_widget_radio_add(evas, _("Display App Comment"), 1, rg);
    e_widget_framelist_object_append(of, cfdata->radio_comment);
    if (!cfdata->show_label) e_widget_disabled_set(cfdata->radio_comment, 1);
-   
+
    cfdata->radio_generic = e_widget_radio_add(evas, _("Display App Generic"), 2, rg);
    e_widget_framelist_object_append(of, cfdata->radio_generic);
    if (!cfdata->show_label) e_widget_disabled_set(cfdata->radio_generic, 1);
-   
+
    e_widget_list_object_append(o, of, 1, 1, 0.5);
    return o;
 }
@@ -146,7 +147,7 @@ static int
 _basic_apply_data(E_Config_Dialog *cfd, E_Config_Dialog_Data *cfdata)
 {
    Config_Item *ci;
-   
+
    ci = cfd->data;
    if (ci->dir) eina_stringshare_del(ci->dir);
    ci->dir = NULL;
@@ -162,7 +163,7 @@ static void
 _cb_add(void *data, void *data2) 
 {
    E_Config_Dialog_Data *cfdata;
-   
+
    cfdata = data;
    e_entry_dialog_show(_("Create new IBar source"), "enlightenment",
 		       _("Enter a name for this new source:"), "", NULL, NULL,
@@ -177,16 +178,16 @@ _cb_del(void *data, void *data2)
    E_Confirm_Dialog *dialog;
 
    cfdata = data;   
-   if (cfdata->dialog_delete)
-     return;
+   if (cfdata->dialog_delete) return;
 
    snprintf(buf, sizeof(buf), _("You requested to delete \"%s\".<br><br>"
 				"Are you sure you want to delete this bar source?"),
 	    cfdata->dir);
-   
+
    dialog = e_confirm_dialog_show(_("Are you sure you want to delete this bar source?"),
                                   "application-exit", buf, NULL, NULL, 
-                                  _cb_confirm_dialog_yes, NULL, cfdata, NULL, _cb_confirm_dialog_destroy, cfdata);
+                                  _cb_confirm_dialog_yes, NULL, cfdata, NULL, 
+                                  _cb_confirm_dialog_destroy, cfdata);
    cfdata->dialog_delete = dialog;
 }
 
@@ -195,9 +196,10 @@ _cb_config(void *data, void *data2)
 {
    char path[PATH_MAX];
    E_Config_Dialog_Data *cfdata;
-   
+
    cfdata = data;
-   e_user_dir_snprintf(path, sizeof(path), "applications/bar/%s/.order", cfdata->dir);
+   e_user_dir_snprintf(path, sizeof(path), "applications/bar/%s/.order", 
+                       cfdata->dir);
    e_configure_registry_call("internal/ibar_other",
 			     e_container_current_get(e_manager_current_get()),
 			     path);
@@ -228,7 +230,7 @@ _cb_entry_ok(char *text, void *data)
 	     fclose(f);
 	  }
      }
-   
+
    _load_tlist(data);
 }
 
@@ -251,7 +253,7 @@ static void
 _cb_confirm_dialog_destroy(void *data) 
 {
    E_Config_Dialog_Data *cfdata;
-   
+
    cfdata = data;
    cfdata->dialog_delete = NULL;
 }
@@ -291,18 +293,16 @@ _load_tlist(E_Config_Dialog_Data *cfdata)
      }
 
    e_widget_ilist_go(cfdata->tlist);
-   if (selnum >= 0)
-     e_widget_ilist_selected_set(cfdata->tlist, selnum);   
+   if (selnum >= 0) e_widget_ilist_selected_set(cfdata->tlist, selnum);   
 }
 
 static void 
 _show_label_cb_change(void *data, Evas_Object *obj) 
 {
    E_Config_Dialog_Data *cfdata;
-   
+
    cfdata = data;
    if (!cfdata) return;
-   
    e_widget_disabled_set(cfdata->radio_name, !cfdata->show_label);
    e_widget_disabled_set(cfdata->radio_comment, !cfdata->show_label);
    e_widget_disabled_set(cfdata->radio_generic, !cfdata->show_label);   
