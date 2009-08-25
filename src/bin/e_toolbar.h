a616287b1a44c43415e09ed4076b81b5c5b6f3a8
Christopher Michael <cpmichael1@comcast.net>
2009-08-25 11:49:46 +0000

    Formatting.
    SVN revision: 41973

--- src/bin/e_toolbar.h
+++ src/bin/e_toolbar.h
@@ -26,21 +26,21 @@ struct _E_Toolbar
    Evas_Object *o_base, *o_event;
 };
 
-EAPI int          e_toolbar_init          (void);
-EAPI int          e_toolbar_shutdown      (void);
-EAPI E_Toolbar   *e_toolbar_new           (Evas *evas, const char *name, E_Win *fwin, Evas_Object *fm2);
-EAPI void         e_toolbar_fwin_set      (E_Toolbar *tbar, E_Win *fwin);
-EAPI E_Win       *e_toolbar_fwin_get      (E_Toolbar *tbar);
-EAPI void         e_toolbar_fm2_set       (E_Toolbar *tbar, Evas_Object *fm2);
-EAPI Evas_Object *e_toolbar_fm2_get       (E_Toolbar *tbar);
-EAPI void         e_toolbar_show          (E_Toolbar *tbar);
-EAPI void         e_toolbar_hide          (E_Toolbar *tbar);
-EAPI void         e_toolbar_move          (E_Toolbar *tbar, int x, int y);
-EAPI void         e_toolbar_resize        (E_Toolbar *tbar, int w, int h);
-EAPI void         e_toolbar_move_resize   (E_Toolbar *tbar, int x, int y, int w, int h);
-EAPI void         e_toolbar_orient        (E_Toolbar *tbar, E_Gadcon_Orient orient);
-EAPI void         e_toolbar_position_calc (E_Toolbar *tbar);
-EAPI void         e_toolbar_populate      (E_Toolbar *tbar);
+EAPI int e_toolbar_init(void);
+EAPI int e_toolbar_shutdown(void);
+EAPI E_Toolbar *e_toolbar_new(Evas *evas, const char *name, E_Win *fwin, Evas_Object *fm2);
+EAPI void e_toolbar_fwin_set(E_Toolbar *tbar, E_Win *fwin);
+EAPI E_Win *e_toolbar_fwin_get(E_Toolbar *tbar);
+EAPI void e_toolbar_fm2_set(E_Toolbar *tbar, Evas_Object *fm2);
+EAPI Evas_Object *e_toolbar_fm2_get(E_Toolbar *tbar);
+EAPI void e_toolbar_show(E_Toolbar *tbar);
+EAPI void e_toolbar_hide(E_Toolbar *tbar);
+EAPI void e_toolbar_move(E_Toolbar *tbar, int x, int y);
+EAPI void e_toolbar_resize(E_Toolbar *tbar, int w, int h);
+EAPI void e_toolbar_move_resize(E_Toolbar *tbar, int x, int y, int w, int h);
+EAPI void e_toolbar_orient(E_Toolbar *tbar, E_Gadcon_Orient orient);
+EAPI void e_toolbar_position_calc(E_Toolbar *tbar);
+EAPI void e_toolbar_populate(E_Toolbar *tbar);
 
 #endif
 #endif
