1454cbbd04b64558da79755c21637c8e524bbdea
Christopher Michael <cpmichael1@comcast.net>
2009-11-21 17:42:57 +0000

    Port the old illume keyboard to new illume-keyboard module.
    SVN revision: 43866

--- src/modules/illume-keyboard/e_kbd_dbus.h
+++
@@ -1,7 +0,0 @@
-#ifndef E_KBD_DBUS_H
-#define E_KBD_DBUS_H
-
-EAPI void e_kbd_dbus_real_kbd_init(void);
-EAPI void e_kbd_dbus_real_kbd_shutdown(void);
-
-#endif
--- src/modules/illume-keyboard/e_mod_main.c
+++ src/modules/illume-keyboard/e_mod_main.c
@@ -1,23 +1,30 @@
 #include "e.h"
 #include "e_mod_main.h"
-#include "e_kbd.h"
+#include "e_kbd_int.h"
+
+/* local variables */
+const char *mod_dir = NULL;
+static E_Kbd_Int *ki = NULL;
 
 EAPI E_Module_Api e_modapi = { E_MODULE_API_VERSION, "Illume Keyboard" };
-const char *mod_dir = NULL;
 
 EAPI void *
 e_modapi_init(E_Module *m) 
 {
-   if (!e_kbd_init(m)) return NULL;
    mod_dir = eina_stringshare_add(m->dir);
+   ki = e_kbd_int_new(mod_dir, mod_dir, mod_dir);
    return m;
 }
 
 EAPI int 
 e_modapi_shutdown(E_Module *m) 
 {
+   if (ki) 
+     {
+        e_kbd_int_free(ki);
+        ki = NULL;
+     }
    if (mod_dir) eina_stringshare_del(mod_dir);
-   e_kbd_shutdown();
    return 1;
 }
 
