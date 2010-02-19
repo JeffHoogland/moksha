448f5524d63d20e53f4f7ad9aaf70e7c1fad8cfc
Christopher Michael <cpmichael1@comcast.net>
2010-02-19 14:45:10 +0000

    Don't need to trigger layout update twice. Set focus to the newly shown border.
    SVN revision: 46311

--- src/modules/illume2/policies/illume/policy.c
+++ src/modules/illume2/policies/illume/policy.c
@@ -202,20 +202,16 @@ _policy_border_show_below(E_Border *bd)
 
              if ((bd->fullscreen) || (bd->need_fullscreen)) 
                {
-                  if (!b->visible) 
-                    e_border_show(b);
-                  b->changes.visible = 1;
-                  b->changed = 1;
+                  if (!b->visible) e_border_show(b);
+                  _policy_border_set_focus(b);
                }
              else 
                {
                   /* need to check x/y position */
                   if ((b->x == bd->x) && (b->y == bd->y)) 
                     {
-                       if (!b->visible) 
-                         e_border_show(b);
-                       b->changes.visible = 1;
-                       b->changed = 1;
+                       if (!b->visible) e_border_show(b);
+                       _policy_border_set_focus(b);
                     }
                }
           }
