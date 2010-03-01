dabc09958aaafe78d0837d69285168aeba4b40e0
Christopher Michael <cpmichael1@comcast.net>
2010-03-01 17:35:40 +0000

    Don't expand lists. Fix formatting.
    SVN revision: 46719

--- src/modules/conf_window_manipulation/e_int_config_window_geometry.c
+++ src/modules/conf_window_manipulation/e_int_config_window_geometry.c
@@ -19,17 +19,18 @@ struct _E_Config_Dialog_Data
    int desk_resist;
    int window_resist;
    int gadget_resist;
-   struct {
-	double timeout;
-	struct {
-	     int dx;
-	     int dy;
-	} move;
-	struct {
-	     int dx;
-	     int dy;
-	} resize;
-   } border_keyboard;
+   struct 
+     {
+        double timeout;
+        struct 
+          {
+             int dx, dy;
+          } move;
+        struct 
+          {
+             int dx, dy;
+          } resize;
+     } border_keyboard;
 };
 
 Eina_List *resistance_list = NULL;
@@ -51,8 +52,7 @@ e_int_config_window_geometry(E_Container *con, const char *params __UNUSED__)
    v->basic.create_widgets = _basic_create_widgets;
 
    /* create config diaolg for NULL object/data */
-   cfd = e_config_dialog_new(con,
-			     _("Window Geometry"),
+   cfd = e_config_dialog_new(con, _("Window Geometry"),
 			     "E", "windows/window_geometry",
 			     "preferences-window-manipulation", 0, v, NULL);
    return cfd;
@@ -128,13 +128,15 @@ _basic_create_widgets(E_Config_Dialog *cfd, Evas *evas, E_Config_Dialog_Data *cf
    o = e_widget_list_add(evas, 0, 0);
 
    of = e_widget_framelist_add(evas, _("Resistance"), 0);
-   resistance_check = e_widget_check_add(evas, _("Resist moving or resizing a window over an obstacle"), &(cfdata->use_resist));
+   resistance_check = e_widget_check_add(evas, _("Resist moving or resizing a window over an obstacle"), 
+                                         &(cfdata->use_resist));
    e_widget_framelist_object_append(of, resistance_check);
    ob = e_widget_label_add(evas, _("Resistance between windows:"));
    resistance_list = eina_list_append (resistance_list, ob);
    e_widget_disabled_set(ob, !cfdata->use_resist); // set state from saved config
    e_widget_framelist_object_append(of, ob);
-   ob = e_widget_slider_add(evas, 1, 0, _("%2.0f pixels"), 0, 64.0, 1.0, 0, NULL, &(cfdata->window_resist), 200);
+   ob = e_widget_slider_add(evas, 1, 0, _("%2.0f pixels"), 0, 64.0, 1.0, 0, 
+                            NULL, &(cfdata->window_resist), 200);
    resistance_list = eina_list_append (resistance_list, ob);
    e_widget_disabled_set(ob, !cfdata->use_resist); // set state from saved config
    e_widget_framelist_object_append(of, ob);
@@ -142,7 +144,8 @@ _basic_create_widgets(E_Config_Dialog *cfd, Evas *evas, E_Config_Dialog_Data *cf
    resistance_list = eina_list_append (resistance_list, ob);
    e_widget_disabled_set(ob, !cfdata->use_resist); // set state from saved config
    e_widget_framelist_object_append(of, ob);
-   ob = e_widget_slider_add(evas, 1, 0, _("%2.0f pixels"), 0, 64.0, 1.0, 0, NULL, &(cfdata->desk_resist), 200);
+   ob = e_widget_slider_add(evas, 1, 0, _("%2.0f pixels"), 0, 64.0, 1.0, 0, 
+                            NULL, &(cfdata->desk_resist), 200);
    resistance_list = eina_list_append (resistance_list, ob);
    e_widget_disabled_set(ob, !cfdata->use_resist); // set state from saved config
    e_widget_framelist_object_append(of, ob);
@@ -150,28 +153,34 @@ _basic_create_widgets(E_Config_Dialog *cfd, Evas *evas, E_Config_Dialog_Data *cf
    resistance_list = eina_list_append (resistance_list, ob);
    e_widget_disabled_set(ob, !cfdata->use_resist); // set state from saved config
    e_widget_framelist_object_append(of, ob);
-   ob = e_widget_slider_add(evas, 1, 0, _("%2.0f pixels"), 0, 64.0, 1.0, 0, NULL, &(cfdata->gadget_resist), 200);
+   ob = e_widget_slider_add(evas, 1, 0, _("%2.0f pixels"), 0, 64.0, 1.0, 0, 
+                            NULL, &(cfdata->gadget_resist), 200);
    resistance_list = eina_list_append (resistance_list, ob);
    e_widget_disabled_set(ob, !cfdata->use_resist); // set state from saved config
    e_widget_framelist_object_append(of, ob);
-   e_widget_list_object_append(o, of, 1, 1, 0.5);
+   e_widget_list_object_append(o, of, 1, 0, 0.5);
+
    // handler for enable/disable widget array
-   e_widget_on_change_hook_set(resistance_check, _cb_disable_check_list, resistance_list);
+   e_widget_on_change_hook_set(resistance_check, _cb_disable_check_list, 
+                               resistance_list);
 
    of = e_widget_framelist_add(evas, _("Keyboard move and resize"), 0);
    ob = e_widget_label_add(evas, _("Automatically accept changes after:"));
    e_widget_framelist_object_append(of, ob);
-   ob = e_widget_slider_add(evas, 1, 0, _("%1.1f sec"), 0.0, 9.9, 0.1, 0, &(cfdata->border_keyboard.timeout), NULL, 100);
+   ob = e_widget_slider_add(evas, 1, 0, _("%1.1f sec"), 0.0, 9.9, 0.1, 0, 
+                            &(cfdata->border_keyboard.timeout), NULL, 100);
    e_widget_framelist_object_append(of, ob);
    ob = e_widget_label_add(evas, _("Move by:"));
    e_widget_framelist_object_append(of, ob);
-   ob = e_widget_slider_add(evas, 1, 0, _("%2.0f pixels"), 1, 255, 1, 0, NULL, &(cfdata->border_keyboard.move.dx), 200);
+   ob = e_widget_slider_add(evas, 1, 0, _("%2.0f pixels"), 1, 255, 1, 0, NULL, 
+                            &(cfdata->border_keyboard.move.dx), 200);
    e_widget_framelist_object_append(of, ob);
    ob = e_widget_label_add(evas, _("Resize by:"));
    e_widget_framelist_object_append(of, ob);
-   ob = e_widget_slider_add(evas, 1, 0, _("%2.0f pixels"), 1, 255, 1, 0, NULL, &(cfdata->border_keyboard.resize.dx), 200);
+   ob = e_widget_slider_add(evas, 1, 0, _("%2.0f pixels"), 1, 255, 1, 0, NULL, 
+                            &(cfdata->border_keyboard.resize.dx), 200);
    e_widget_framelist_object_append(of, ob);
-   e_widget_list_object_append(o, of, 1, 1, 0.5);
+   e_widget_list_object_append(o, of, 1, 0, 0.5);
 
    return o;
 }
@@ -190,5 +199,5 @@ _cb_disable_check_list(void *data, Evas_Object *obj)
    Evas_Object *o;
 
    EINA_LIST_FOREACH(list, l, o)
-      e_widget_disabled_set(o, !e_widget_check_checked_get(obj));
+     e_widget_disabled_set(o, !e_widget_check_checked_get(obj));
 }
