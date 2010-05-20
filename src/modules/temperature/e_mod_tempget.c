276fbbb6744ab47ab62cab2c3f053b07953d0510
Carsten Haitzler <raster@rasterman.com>
2010-05-20 11:47:07 +0000

    stick in dummy file so it compile... bad disco! bad!
    SVN revision: 49064

---
+++ src/modules/temperature/e_mod_tempget.c
@@ -0,0 +1,14 @@
+#include "e.h"
+#include "e_mod_main.h"
+
+int
+_temperature_cb_exe_data(void *data, int type, void *event)
+{
+   return 1;
+}
+
+int
+_temperature_cb_exe_del(void *data, int type, void *event)
+{
+   return 1;
+}
