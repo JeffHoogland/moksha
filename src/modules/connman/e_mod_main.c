916000bccce4353b9fcec97ab8510d65b67561df
Lucas De Marchi <lucas.demarchi@profusion.mobi>
2010-06-29 16:30:10 +0000

    connman: show configuration instead of status in config dialog IPv4.* are the status properties and are valid only when a service is connected. In a configuration dialog, the right properties to use are the IPv4.Configuration.* ones.
    SVN revision: 49940

--- src/modules/connman/e_mod_main.c
+++ src/modules/connman/e_mod_main.c
@@ -337,9 +337,9 @@ _connman_service_changed(void *data, const E_Connman_Element *element)
    GSTR(state, e_connman_service_state_get);
    GSTR(error, e_connman_service_error_get);
    GSTR(security, e_connman_service_security_get);
-   GSTR(ipv4_method, e_connman_service_ipv4_method_get);
-   GSTR(ipv4_address, e_connman_service_ipv4_address_get);
-   GSTR(ipv4_netmask, e_connman_service_ipv4_netmask_get);
+   GSTR(ipv4_method, e_connman_service_ipv4_configuration_method_get);
+   GSTR(ipv4_address, e_connman_service_ipv4_configuration_address_get);
+   GSTR(ipv4_netmask, e_connman_service_ipv4_configuration_netmask_get);
 #undef GSTR
 
    if ((service->state != e_str_failure) && (service->error))
