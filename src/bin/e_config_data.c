6f30fefd521b2018765d497600bd1a30f283d672
Cedric BAIL <cedric.bail@free.fr>
2010-08-13 16:50:55 +0000

    * e: fix eet breakage.
    SVN revision: 51089

--- src/bin/e_config_data.c
+++ src/bin/e_config_data.c
@@ -5,7 +5,7 @@ e_config_descriptor_new(const char *name, int size)
 {
    Eet_Data_Descriptor_Class eddc;
 
-   if (!eet_eina_stream_data_descriptor_class_set(&eddc, name, size))
+   if (!eet_eina_stream_data_descriptor_class_set(&eddc, sizeof (eddc), name, size))
      return NULL;
 
    /* FIXME: We can directly map string inside an Eet_File and reuse it.
