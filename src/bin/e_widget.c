ead21f18544e0aa33a338733dae2194f38dccf91
Christopher Michael <cpmichael1@comcast.net>
2010-08-18 19:41:30 +0000

    Add missing function placeholders for Evas_Smart_Class.
    SVN revision: 51326

--- src/bin/e_widget.c
+++ src/bin/e_widget.c
@@ -581,7 +581,10 @@ _e_smart_init(void)
 	       NULL,
 	       NULL,
 	       NULL,
-	       NULL
+	       NULL, 
+               NULL, 
+               NULL, 
+               NULL
 	  };
         _e_smart = evas_smart_class_new(&sc);
      }
