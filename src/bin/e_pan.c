35dd0dce96fb27fd1ad52077faee01f6b4f94589
Christopher Michael <cpmichael1@comcast.net>
2010-08-18 19:34:04 +0000

    Add unused.
    SVN revision: 51323

--- src/bin/e_pan.c
+++ src/bin/e_pan.c
@@ -131,7 +131,7 @@ e_pan_child_size_get(Evas_Object *obj, Evas_Coord *w, Evas_Coord *h)
 
 /* local subsystem functions */
 static void
-_e_smart_child_del_hook(void *data, Evas *e, Evas_Object *obj, void *event_info)
+_e_smart_child_del_hook(void *data, Evas *e __UNUSED__, Evas_Object *obj __UNUSED__, void *event_info __UNUSED__)
 {
    E_Smart_Data *sd;
 
@@ -141,7 +141,7 @@ _e_smart_child_del_hook(void *data, Evas *e, Evas_Object *obj, void *event_info)
 }
 
 static void
-_e_smart_child_resize_hook(void *data, Evas *e, Evas_Object *obj, void *event_info)
+_e_smart_child_resize_hook(void *data, Evas *e __UNUSED__, Evas_Object *obj __UNUSED__, void *event_info __UNUSED__)
 {
    E_Smart_Data *sd;
    Evas_Coord w, h;
