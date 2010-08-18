f8b4a010367998acbceff7553509f703339c3825
Christopher Michael <cpmichael1@comcast.net>
2010-08-18 20:09:01 +0000

    Add unused.
    SVN revision: 51337

--- src/bin/e_widget_textblock.c
+++ src/bin/e_widget_textblock.c
@@ -158,7 +158,7 @@ _e_wid_focus_hook(Evas_Object *obj)
 }
 
 static void
-_e_wid_cb_scrollframe_resize(void *data, Evas *e, Evas_Object *obj, void *event_info)
+_e_wid_cb_scrollframe_resize(void *data, Evas *e __UNUSED__, Evas_Object *obj __UNUSED__, void *event_info __UNUSED__)
 {
    E_Widget_Data *wd;
    Evas_Coord mw, mh, vw, vh;
@@ -172,7 +172,7 @@ _e_wid_cb_scrollframe_resize(void *data, Evas *e, Evas_Object *obj, void *event_
 }
    
 static void
-_e_wid_focus_steal(void *data, Evas *e, Evas_Object *obj, void *event_info)
+_e_wid_focus_steal(void *data, Evas *e __UNUSED__, Evas_Object *obj __UNUSED__, void *event_info __UNUSED__)
 {
    e_widget_focus_steal(data);
 }
