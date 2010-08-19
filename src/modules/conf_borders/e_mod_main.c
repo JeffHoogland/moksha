e49850d0e5787d65d0838aeab516a95b62862d62
Christopher Michael <cpmichael1@comcast.net>
2010-08-19 17:16:49 +0000

    Add UNUSED where needed.
    SVN revision: 51420

--- src/modules/conf_borders/e_mod_main.c
+++ src/modules/conf_borders/e_mod_main.c
@@ -24,7 +24,7 @@ e_modapi_init(E_Module *m)
 }
 
 EAPI int
-e_modapi_shutdown(E_Module *m)
+e_modapi_shutdown(E_Module *m __UNUSED__)
 {
    E_Config_Dialog *cfd;
 
@@ -41,7 +41,7 @@ e_modapi_shutdown(E_Module *m)
 }
 
 EAPI int
-e_modapi_save(E_Module *m)
+e_modapi_save(E_Module *m __UNUSED__)
 {
    return 1;
 }
