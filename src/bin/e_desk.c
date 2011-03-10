1925cec0c618281f96853951d03592b883170ca4
Hannes Janetzek <hannes.janetzek@gmail.com>
2011-03-10 00:04:31 +0000

    e17: fix the rare case that the last focused border had lock_focus_out. try next instead of break
    SVN revision: 57644

--- src/bin/e_desk.c
+++ src/bin/e_desk.c
@@ -380,8 +380,10 @@ e_desk_last_focused_focus(E_Desk *desk)
 	  {
 	     /* this was the window last focused in this desktop */
              if (!bd->lock_focus_out)
-	       e_border_focus_set(bd, 1, 1);
-	     break;
+	       {
+		  e_border_focus_set(bd, 1, 1);
+		  break;
+	       }
 	  }
      }
 }
