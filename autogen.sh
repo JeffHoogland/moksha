72c84ef2f2e3517b4c8a2a65dd389bc350a6ee5d
Cedric BAIL <cedric.bail@free.fr>
2011-04-14 19:26:15 +0000

    autogen: add forgotten autopoint.
    SVN revision: 58671

--- autogen.sh
+++ autogen.sh
@@ -3,6 +3,7 @@
 touch README
 touch ABOUT-NLS
 
+echo "Running autopoint..." ; autopoint -f || :
 echo "Running aclocal..." ; aclocal $ACLOCAL_FLAGS -I m4 || exit 1
 echo "Running autoheader..." ; autoheader || exit 1
 echo "Running autoconf..." ; autoconf || exit 1
