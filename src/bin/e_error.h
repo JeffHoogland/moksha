e32856d08454650f0aec60409125448a58ed6159
Mike McCormack <mj.mccormack@samsung.com>
2011-07-27 02:35:03 +0000

    e: Fix macro so it can be used as a statement
    Should fix devilhorn's compile error.
    
    Signed-off-by: Mike McCormack <mj.mccormack@samsung.com>
    
    SVN revision: 61783

--- src/bin/e_error.h
+++ src/bin/e_error.h
@@ -1,12 +1,12 @@
 #ifdef E_TYPEDEFS
 
-#define e_error_message_show(args...) \
+#define e_error_message_show(args...) do \
 { \
    char __tmpbuf[PATH_MAX]; \
  \
    snprintf(__tmpbuf, sizeof(__tmpbuf), ##args); \
    e_error_message_show_internal(__tmpbuf); \
-}
+} while (0)
 
 #else
 #ifndef E_ERROR_H
