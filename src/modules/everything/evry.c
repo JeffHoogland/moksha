aea6b1e70ba3cd1aa62de35e74941224021cd595
Hannes Janetzek <hannes.janetzek@gmail.com>
2011-08-11 09:47:16 +0000

    e17/evry: ok this one fixes it
    SVN revision: 62338

--- src/modules/everything/evry.c
+++ src/modules/everything/evry.c
@@ -2919,17 +2919,16 @@ _evry_plugin_select(Evry_State *s, Evry_Plugin *p)
    if (p)
      {
 	s->plugin_auto_selected = EINA_FALSE;
-	s->plugin = p;
      }
    else
      {
 	p = eina_list_data_get(s->cur_plugins);
 	s->plugin_auto_selected = EINA_TRUE;
-	s->plugin = NULL;
-	return;
      }
 
-   if ((s->view && s->plugin->view) &&
+   s->plugin = p;
+
+   if ((s->view && s->plugin && s->plugin->view) &&
        (s->view->name != s->plugin->view->name))
      {
 	s->view->destroy(s->view);
