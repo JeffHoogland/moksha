d6f5442a511b3b02623f4e6a6ace4f9fdbdbeffe
Carsten Haitzler <raster@rasterman.com>
2011-08-30 00:30:48 +0000

    seed fsel with filename (shot.jpg)
    SVN revision: 62979

--- src/modules/shot/e_mod_main.c
+++ src/modules/shot/e_mod_main.c
@@ -201,6 +201,7 @@ _file_select_ok_cb(void *data __UNUSED__, E_Dialog *dia)
    const char *file;
 
    file = e_widget_fsel_selection_path_get(o_fsel);
+   printf("SAVE: %s\n", file);
    if (file) _save_to(file);
    if (dia) e_util_defer_object_del(E_OBJECT(dia));
    if (win)
@@ -227,12 +228,13 @@ _win_save_cb(void *data __UNUSED__, void *data2 __UNUSED__)
    E_Dialog *dia;
    Evas_Object *o;
    Evas_Coord mw, mh;
-  
+   
    dia = e_dialog_new(scon, "E", "_e_shot_fsel");
    e_dialog_title_set(dia, _("Select screenshot save location"));
-   o = e_widget_fsel_add(dia->win->evas, "~/", "/", NULL, NULL,
+   o = e_widget_fsel_add(dia->win->evas, "~/", "/", "shot.jpg", NULL,
                          NULL, NULL,
                          NULL, NULL, 1);
+   e_widget_fsel_window_object_set(o, E_OBJECT(dia->win));
    o_fsel = o;
    evas_object_show(o);
    e_widget_size_min_get(o, &mw, &mh);
