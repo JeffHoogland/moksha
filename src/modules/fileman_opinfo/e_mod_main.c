0645e7012d8826353e6756907a8e53a1c5fd96c0
Boris Faure <billiob@gmail.com>
2011-11-14 22:23:36 +0000

    e: some more const to _gc_label()
    SVN revision: 65209

--- src/modules/fileman_opinfo/e_mod_main.c
+++ src/modules/fileman_opinfo/e_mod_main.c
@@ -16,7 +16,7 @@ typedef struct _Instance
 static E_Gadcon_Client *_gc_init    (E_Gadcon *gc, const char *name, const char *id, const char *style);
 static void             _gc_shutdown(E_Gadcon_Client *gcc);
 static void             _gc_orient  (E_Gadcon_Client *gcc, E_Gadcon_Orient orient);
-static char            *_gc_label   (E_Gadcon_Client_Class *client_class);
+static const char      *_gc_label   (E_Gadcon_Client_Class *client_class);
 static Evas_Object     *_gc_icon    (E_Gadcon_Client_Class *client_class, Evas *evas);
 static const char      *_gc_id_new  (E_Gadcon_Client_Class *client_class);
 
@@ -403,7 +403,7 @@ _gc_orient(E_Gadcon_Client *gcc, E_Gadcon_Orient orient __UNUSED__)
    e_gadcon_client_min_size_set(gcc, mw, mh);
 }
 
-static char *
+static const char *
 _gc_label(E_Gadcon_Client_Class *client_class __UNUSED__)
 {
    return _("EFM Operation Info");
--- src/modules/notification/e_mod_main.c
+++ src/modules/notification/e_mod_main.c
@@ -6,7 +6,7 @@ static E_Gadcon_Client *_gc_init(E_Gadcon   *gc,
                                  const char *id,
                                  const char *style);
 static void         _gc_shutdown(E_Gadcon_Client *gcc);
-static char        *_gc_label(E_Gadcon_Client_Class *client_class);
+static const char  *_gc_label(E_Gadcon_Client_Class *client_class);
 static Evas_Object *_gc_icon(E_Gadcon_Client_Class *client_class,
                              Evas                  *evas);
 static const char  *_gc_id_new(E_Gadcon_Client_Class *client_class);
@@ -122,7 +122,7 @@ _gc_orient(E_Gadcon_Client *gcc,
    e_gadcon_client_min_size_set(gcc, 16, 16);
 }
 
-static char *
+static const char *
 _gc_label(E_Gadcon_Client_Class *client_class __UNUSED__)
 {
    return _("Notification Box");
