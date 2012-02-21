b8f3d41c08aadd75743e3056d1e513336bfb2792
Leif Middelschulte <leif.middelschulte@gmail.com>
2012-02-21 23:12:48 +0000

    Fix another possible SEGV
    SVN revision: 68237

--- src/bin/e_randr_12_output.c
+++ src/bin/e_randr_12_output.c
@@ -200,6 +200,8 @@ Eina_List
      {
         EINA_LIST_FOREACH(outputs, output_iter, output_info)
           {
+             if (!output_info->monitor)
+               continue;
              if (!eina_list_data_find(output_info->monitor->modes, mode_info))
                common_modes = eina_list_remove(common_modes, mode_info);
           }
