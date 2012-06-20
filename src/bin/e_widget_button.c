9ad5316aa3d1708b92534dfcf5e23bf0b5622a2d
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2012-06-20 13:51:57 +0000

    formatting
    SVN revision: 72544

--- src/bin/e_widget_button.c
+++ src/bin/e_widget_button.c
@@ -10,11 +10,11 @@ enum _E_Widget_Button_Type
 typedef struct _E_Widget_Data E_Widget_Data;
 struct _E_Widget_Data
 {
-   Evas_Object *o_button;
-   Evas_Object *o_icon;
-   void (*func) (void *data, void *data2);
-   void *data;
-   void *data2;   
+   Evas_Object         *o_button;
+   Evas_Object         *o_icon;
+   void                 (*func)(void *data, void *data2);
+   void                *data;
+   void                *data2;
    E_Widget_Button_Type type;
 };
 
@@ -29,7 +29,7 @@ static void _e_wid_button_state_send(E_Widget_Data *wd);
 
 /* externally accessible functions */
 EAPI Evas_Object *
-e_widget_button_add(Evas *evas, const char *label, const char *icon, void (*func) (void *data, void *data2), void *data, void *data2)
+e_widget_button_add(Evas *evas, const char *label, const char *icon, void (*func)(void *data, void *data2), void *data, void *data2)
 {
    Evas_Object *obj, *o;
    E_Widget_Data *wd;
@@ -50,30 +50,30 @@ e_widget_button_add(Evas *evas, const char *label, const char *icon, void (*func
    o = edje_object_add(evas);
    wd->o_button = o;
    e_theme_edje_object_set(o, "base/theme/widgets",
-			   "e/widgets/button");
-   edje_object_signal_callback_add(o, "e,action,click", "", 
-				   _e_wid_signal_cb1, obj);
+                           "e/widgets/button");
+   edje_object_signal_callback_add(o, "e,action,click", "",
+                                   _e_wid_signal_cb1, obj);
    if ((label) && (label[0] != 0))
      {
-	edje_object_part_text_set(o, "e.text.label", label);
-	wd->type |= E_WIDGET_BUTTON_TEXT;
+        edje_object_part_text_set(o, "e.text.label", label);
+        wd->type |= E_WIDGET_BUTTON_TEXT;
      }
 
    e_widget_sub_object_add(obj, o);
-   evas_object_event_callback_add(o, EVAS_CALLBACK_MOUSE_DOWN, 
-				  _e_wid_focus_steal, obj);
+   evas_object_event_callback_add(o, EVAS_CALLBACK_MOUSE_DOWN,
+                                  _e_wid_focus_steal, obj);
    e_widget_resize_object_set(obj, o);
    evas_object_show(o);
 
    if (icon)
      {
-	o = e_icon_add(evas);
-	wd->o_icon = o;
-	e_util_icon_theme_set(o, icon);
-	edje_object_part_swallow(wd->o_button, "e.swallow.icon", o);
-	e_widget_sub_object_add(obj, o);
-	evas_object_show(o);
-	wd->type |= E_WIDGET_BUTTON_ICON;
+        o = e_icon_add(evas);
+        wd->o_icon = o;
+        e_util_icon_theme_set(o, icon);
+        edje_object_part_swallow(wd->o_button, "e.swallow.icon", o);
+        e_widget_sub_object_add(obj, o);
+        evas_object_show(o);
+        wd->type |= E_WIDGET_BUTTON_ICON;
      }
 
    _e_wid_button_state_send(wd);
@@ -109,20 +109,20 @@ e_widget_button_icon_set(Evas_Object *obj, Evas_Object *icon)
    wd = e_widget_data_get(obj);
    if (wd->o_icon)
      {
-	e_widget_sub_object_del(obj, wd->o_icon);
-	evas_object_hide(wd->o_icon);
-	edje_object_part_unswallow(wd->o_button, wd->o_icon);
-	evas_object_del(wd->o_icon);
-	wd->o_icon = NULL;
+        e_widget_sub_object_del(obj, wd->o_icon);
+        evas_object_hide(wd->o_icon);
+        edje_object_part_unswallow(wd->o_button, wd->o_icon);
+        evas_object_del(wd->o_icon);
+        wd->o_icon = NULL;
      }
    if (icon)
      {
-	wd->o_icon = icon;
-	edje_object_part_swallow(wd->o_button, "e.swallow.icon", icon);
-	evas_object_pass_events_set(icon, 1);
-	evas_object_show(icon);
-	e_widget_sub_object_add(obj, icon);
-	wd->type |= E_WIDGET_BUTTON_ICON;
+        wd->o_icon = icon;
+        edje_object_part_swallow(wd->o_button, "e.swallow.icon", icon);
+        evas_object_pass_events_set(icon, 1);
+        evas_object_show(icon);
+        e_widget_sub_object_add(obj, icon);
+        wd->type |= E_WIDGET_BUTTON_ICON;
      }
    else
      wd->type = ~(wd->type & E_WIDGET_BUTTON_ICON);
@@ -148,13 +148,13 @@ _e_wid_focus_hook(Evas_Object *obj)
    wd = e_widget_data_get(obj);
    if (e_widget_focus_get(obj))
      {
-	edje_object_signal_emit(wd->o_button, "e,state,focused", "e");
-	evas_object_focus_set(wd->o_button, 1);
+        edje_object_signal_emit(wd->o_button, "e,state,focused", "e");
+        evas_object_focus_set(wd->o_button, 1);
      }
    else
      {
-	edje_object_signal_emit(wd->o_button, "e,state,unfocused", "e");
-	evas_object_focus_set(wd->o_button, 0);
+        edje_object_signal_emit(wd->o_button, "e,state,unfocused", "e");
+        evas_object_focus_set(wd->o_button, 0);
      }
 }
 
@@ -203,12 +203,13 @@ _e_wid_button_state_send(E_Widget_Data *wd)
    if ((!wd) || (!wd->o_button)) return;
    if (wd->type & E_WIDGET_BUTTON_TEXT)
      {
-	if (wd->type & E_WIDGET_BUTTON_ICON)
-	  edje_object_signal_emit(wd->o_button, "e,state,combo", "e");
-	else
-	  edje_object_signal_emit(wd->o_button, "e,state,text", "e");
+        if (wd->type & E_WIDGET_BUTTON_ICON)
+          edje_object_signal_emit(wd->o_button, "e,state,combo", "e");
+        else
+          edje_object_signal_emit(wd->o_button, "e,state,text", "e");
      }
    else if (wd->type & E_WIDGET_BUTTON_ICON)
      edje_object_signal_emit(wd->o_button, "e,state,icon", "e");
    edje_object_message_signal_process(wd->o_button);
 }
+
