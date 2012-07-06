d5dbc783992fdc813a5a98a367d671d050c8f0dc
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2012-07-06 07:46:08 +0000

    formatting
    SVN revision: 73399

--- src/bin/e_widget_check.c
+++ src/bin/e_widget_check.c
@@ -5,7 +5,7 @@ struct _E_Widget_Data
 {
    Evas_Object *o_check;
    Evas_Object *o_icon;
-   int *valptr;
+   int         *valptr;
 };
 
 static void _e_wid_del_hook(Evas_Object *obj);
@@ -39,7 +39,7 @@ e_widget_check_add(Evas *evas, const char *label, int *val)
    o = edje_object_add(evas);
    wd->o_check = o;
    e_theme_edje_object_set(o, "base/theme/widgets",
-			   "e/widgets/check");
+                           "e/widgets/check");
    edje_object_signal_callback_add(o, "e,action,toggle", "*", _e_wid_signal_cb1, obj);
    edje_object_part_text_set(o, "e.text.label", label);
    evas_object_show(o);
@@ -47,7 +47,7 @@ e_widget_check_add(Evas *evas, const char *label, int *val)
    e_widget_size_min_set(obj, mw, mh);
    if (wd->valptr)
      {
-	if (*(wd->valptr)) edje_object_signal_emit(o, "e,state,checked", "e");
+        if (*(wd->valptr)) edje_object_signal_emit(o, "e,state,checked", "e");
      }
 
    e_widget_sub_object_add(obj, o);
@@ -106,14 +106,14 @@ e_widget_check_icon_add(Evas *evas, const char *label, const char *icon, int ico
    o = edje_object_add(evas);
    wd->o_check = o;
    e_theme_edje_object_set(o, "base/theme/widgets",
-			   "e/widgets/check_icon");
+                           "e/widgets/check_icon");
    edje_object_signal_callback_add(o, "e,action,toggle", "*", _e_wid_signal_cb1, obj);
    edje_object_part_text_set(o, "e.text.label", label);
    evas_object_show(o);
    if (label)
      {
-	edje_object_signal_emit(o, "e,state,labeled", "e");
-	edje_object_message_signal_process(o);
+        edje_object_signal_emit(o, "e,state,labeled", "e");
+        edje_object_message_signal_process(o);
      }
    if (icon)
      {
@@ -128,17 +128,17 @@ e_widget_check_icon_add(Evas *evas, const char *label, const char *icon, int ico
              e_util_edje_icon_set(o2, icon);
           }
         edje_extern_object_min_size_set(o2, icon_w, icon_h);
-	edje_object_part_swallow(wd->o_check, "e.swallow.icon", o2);
-	evas_object_show(o2);
-	e_widget_sub_object_add(obj, o2);
-	wd->o_icon = o2;
+        edje_object_part_swallow(wd->o_check, "e.swallow.icon", o2);
+        evas_object_show(o2);
+        e_widget_sub_object_add(obj, o2);
+        wd->o_icon = o2;
      }
 
    edje_object_size_min_calc(o, &mw, &mh);
    e_widget_size_min_set(obj, mw, mh);
    if (wd->valptr)
      {
-	if (*(wd->valptr)) edje_object_signal_emit(o, "e,state,checked", "e");
+        if (*(wd->valptr)) edje_object_signal_emit(o, "e,state,checked", "e");
      }
 
    e_widget_sub_object_add(obj, o);
@@ -165,13 +165,13 @@ _e_wid_focus_hook(Evas_Object *obj)
    wd = e_widget_data_get(obj);
    if (e_widget_focus_get(obj))
      {
-	edje_object_signal_emit(wd->o_check, "e,state,focused", "e");
-	evas_object_focus_set(wd->o_check, 1);
+        edje_object_signal_emit(wd->o_check, "e,state,focused", "e");
+        evas_object_focus_set(wd->o_check, 1);
      }
    else
      {
-	edje_object_signal_emit(wd->o_check, "e,state,unfocused", "e");
-	evas_object_focus_set(wd->o_check, 0);
+        edje_object_signal_emit(wd->o_check, "e,state,unfocused", "e");
+        evas_object_focus_set(wd->o_check, 0);
      }
 }
 
@@ -185,16 +185,16 @@ _e_wid_do(Evas_Object *obj)
    wd = e_widget_data_get(obj);
    if (wd->valptr)
      {
-	if (*(wd->valptr) == 0)
-	  {
-	     *(wd->valptr) = 1;
-	     edje_object_signal_emit(wd->o_check, "e,state,checked", "e");
-	  }
-	else
-	  {
-	     *(wd->valptr) = 0;
-	     edje_object_signal_emit(wd->o_check, "e,state,unchecked", "e");
-	  }
+        if (*(wd->valptr) == 0)
+          {
+             *(wd->valptr) = 1;
+             edje_object_signal_emit(wd->o_check, "e,state,checked", "e");
+          }
+        else
+          {
+             *(wd->valptr) = 0;
+             edje_object_signal_emit(wd->o_check, "e,state,unchecked", "e");
+          }
      }
    evas_object_smart_callback_call(obj, "changed", NULL);
    e_widget_change(obj);
@@ -229,3 +229,4 @@ _e_wid_focus_steal(void *data, Evas *e __UNUSED__, Evas_Object *obj __UNUSED__, 
 {
    e_widget_focus_steal(data);
 }
+
