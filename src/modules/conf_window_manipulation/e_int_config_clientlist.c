ebec5c34c64183970c62f9026ae00f448c227650
Chidambar Zinnoury <illogict@online.fr>
2012-07-15 11:01:30 +0000

    e window list: typo.
    SVN revision: 73871

--- src/modules/conf_window_manipulation/e_int_config_clientlist.c
+++ src/modules/conf_window_manipulation/e_int_config_clientlist.c
@@ -107,7 +107,7 @@ _basic_create(E_Config_Dialog *cfd __UNUSED__, Evas *evas, E_Config_Dialog_Data 
    e_widget_framelist_object_append(of, ow);
    e_widget_list_object_append(ol, of, 1, 1, 0.5);
 
-   of = e_widget_framelist_add(evas, _("Seperate Groups By"), 0);
+   of = e_widget_framelist_add(evas, _("Separate Groups By"), 0);
    rg = e_widget_radio_group_new(&(cfdata->separate_with));
    ow = e_widget_radio_add(evas, _("None"), E_CLIENTLIST_GROUP_SEP_NONE, rg);
    e_widget_framelist_object_append(of, ow);
