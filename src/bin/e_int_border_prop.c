a8a90dda494c88d4a37714693852a3923b6968a8
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2012-07-18 13:02:43 +0000

    formatting
    SVN revision: 74067

--- src/bin/e_int_border_prop.c
+++ src/bin/e_int_border_prop.c
@@ -6,19 +6,20 @@
 #define MODE_GEOMETRY_LOCKS 3
 #define MODE_ALL            4
 
-static void _bd_cb_dialog_del(void *obj);
-static void _bd_cb_dialog_close(void *data, E_Dialog *dia);
+static void         _bd_cb_dialog_del(void *obj);
+static void         _bd_cb_dialog_close(void *data, E_Dialog *dia);
 static Evas_Object *_bd_icccm_create(E_Dialog *dia, void *data);
 static Evas_Object *_bd_netwm_create(E_Dialog *dia, void *data);
-static void _bd_go(void *data, void *data2);
-static void _create_data(E_Dialog *cfd, E_Border *bd);
-static void _free_data(E_Dialog *cfd, E_Config_Dialog_Data *cfdata);
+static void         _bd_go(void *data, void *data2);
+static void         _create_data(E_Dialog *cfd, E_Border *bd);
+static void         _free_data(E_Dialog *cfd, E_Config_Dialog_Data *cfdata);
 
 struct _E_Config_Dialog_Data
 {
    E_Border *border;
    /*- BASIC -*/
-   struct {
+   struct
+   {
       char *title;
       char *name;
       char *class;
@@ -41,26 +42,26 @@ struct _E_Config_Dialog_Data
       char *gravity;
       char *command;
 
-      int take_focus;
-      int accepts_focus;
-      int urgent;
-      int delete_request;
-      int request_pos;
+      int   take_focus;
+      int   accepts_focus;
+      int   urgent;
+      int   delete_request;
+      int   request_pos;
    } icccm;
 
    struct
-     {
-	char *name;
-	char *icon_name;
-	int modal;
-	int sticky;
-	int shaded;
-	int skip_taskbar;
-	int skip_pager;
-	int hidden;
-	int fullscreen;
-	char *stacking;
-     } netwm;
+   {
+      char *name;
+      char *icon_name;
+      int   modal;
+      int   sticky;
+      int   shaded;
+      int   skip_taskbar;
+      int   skip_pager;
+      int   hidden;
+      int   fullscreen;
+      char *stacking;
+   } netwm;
 };
 
 EAPI void
@@ -95,8 +96,9 @@ _create_data(E_Dialog *cfd, E_Border *bd)
    cfdata->border = bd;
    bd->border_prop_dialog = cfd;
 
-#define IFDUP(prop, dest) \
-   if (cfdata->border->prop) cfdata->dest = strdup(cfdata->border->prop)
+#define IFDUP(prop, dest)   \
+  if (cfdata->border->prop) \
+    cfdata->dest = strdup(cfdata->border->prop)
 
    IFDUP(client.icccm.title, icccm.title);
    IFDUP(client.icccm.name, icccm.name);
@@ -107,157 +109,174 @@ _create_data(E_Dialog *cfd, E_Border *bd)
 
    if (cfdata->border->client.icccm.min_w >= 0)
      {
-	snprintf(buf, sizeof(buf), _("%i×%i"),
-		 cfdata->border->client.icccm.min_w,
-		 cfdata->border->client.icccm.min_h);
-	cfdata->icccm.min = strdup(buf);
+        snprintf(buf, sizeof(buf), _("%i×%i"),
+                 cfdata->border->client.icccm.min_w,
+                 cfdata->border->client.icccm.min_h);
+        cfdata->icccm.min = strdup(buf);
      }
    if (cfdata->border->client.icccm.max_w >= 0)
      {
-	snprintf(buf, sizeof(buf), _("%i×%i"),
-		 cfdata->border->client.icccm.max_w,
-		 cfdata->border->client.icccm.max_h);
-	cfdata->icccm.max = strdup(buf);
+        snprintf(buf, sizeof(buf), _("%i×%i"),
+                 cfdata->border->client.icccm.max_w,
+                 cfdata->border->client.icccm.max_h);
+        cfdata->icccm.max = strdup(buf);
      }
    if (cfdata->border->client.icccm.base_w >= 0)
      {
-	snprintf(buf, sizeof(buf), _("%i×%i"),
-		 cfdata->border->client.icccm.base_w,
-		 cfdata->border->client.icccm.base_h);
-	cfdata->icccm.base = strdup(buf);
+        snprintf(buf, sizeof(buf), _("%i×%i"),
+                 cfdata->border->client.icccm.base_w,
+                 cfdata->border->client.icccm.base_h);
+        cfdata->icccm.base = strdup(buf);
      }
    if (cfdata->border->client.icccm.step_w >= 0)
      {
-	snprintf(buf, sizeof(buf), _("%i,%i"),
-		 cfdata->border->client.icccm.step_w,
-		 cfdata->border->client.icccm.step_h);
-	cfdata->icccm.step = strdup(buf);
+        snprintf(buf, sizeof(buf), _("%i,%i"),
+                 cfdata->border->client.icccm.step_w,
+                 cfdata->border->client.icccm.step_h);
+        cfdata->icccm.step = strdup(buf);
      }
    if ((cfdata->border->client.icccm.min_aspect > 0.0) &&
        (cfdata->border->client.icccm.max_aspect > 0.0))
      {
-	if (cfdata->border->client.icccm.min_aspect == cfdata->border->client.icccm.max_aspect)
-	  snprintf(buf, sizeof(buf), _("%1.3f"),
-		   cfdata->border->client.icccm.min_aspect);
-	else
-	  snprintf(buf, sizeof(buf), _("%1.3f–%1.3f"),
-		   cfdata->border->client.icccm.min_aspect,
-		   cfdata->border->client.icccm.max_aspect);
-	cfdata->icccm.aspect = strdup(buf);
+        if (cfdata->border->client.icccm.min_aspect == cfdata->border->client.icccm.max_aspect)
+          snprintf(buf, sizeof(buf), _("%1.3f"),
+                   cfdata->border->client.icccm.min_aspect);
+        else
+          snprintf(buf, sizeof(buf), _("%1.3f–%1.3f"),
+                   cfdata->border->client.icccm.min_aspect,
+                   cfdata->border->client.icccm.max_aspect);
+        cfdata->icccm.aspect = strdup(buf);
      }
    if (cfdata->border->client.icccm.initial_state != ECORE_X_WINDOW_STATE_HINT_NONE)
      {
-	switch (cfdata->border->client.icccm.initial_state)
-	  {
-	   case ECORE_X_WINDOW_STATE_HINT_WITHDRAWN:
-	     snprintf(buf, sizeof(buf), _("Withdrawn"));
-	     break;
-	   case ECORE_X_WINDOW_STATE_HINT_NORMAL:
-	     snprintf(buf, sizeof(buf), _("Normal"));
-	     break;
-	   case ECORE_X_WINDOW_STATE_HINT_ICONIC:
-	     snprintf(buf, sizeof(buf), _("Iconic"));
-	     break;
-	   default:
-	     buf[0] = 0;
-	     break;
-	  }
-	cfdata->icccm.initial_state = strdup(buf);
+        switch (cfdata->border->client.icccm.initial_state)
+          {
+           case ECORE_X_WINDOW_STATE_HINT_WITHDRAWN:
+             snprintf(buf, sizeof(buf), _("Withdrawn"));
+             break;
+
+           case ECORE_X_WINDOW_STATE_HINT_NORMAL:
+             snprintf(buf, sizeof(buf), _("Normal"));
+             break;
+
+           case ECORE_X_WINDOW_STATE_HINT_ICONIC:
+             snprintf(buf, sizeof(buf), _("Iconic"));
+             break;
+
+           default:
+             buf[0] = 0;
+             break;
+          }
+        cfdata->icccm.initial_state = strdup(buf);
      }
    if (cfdata->border->client.icccm.state != ECORE_X_WINDOW_STATE_HINT_NONE)
      {
-	switch (cfdata->border->client.icccm.state)
-	  {
-	   case ECORE_X_WINDOW_STATE_HINT_WITHDRAWN:
-	     snprintf(buf, sizeof(buf), _("Withdrawn"));
-	     break;
-	   case ECORE_X_WINDOW_STATE_HINT_NORMAL:
-	     snprintf(buf, sizeof(buf), _("Normal"));
-	     break;
-	   case ECORE_X_WINDOW_STATE_HINT_ICONIC:
-	     snprintf(buf, sizeof(buf), _("Iconic"));
-	     break;
-	   default:
-	     buf[0] = 0;
-	     break;
-	  }
-	cfdata->icccm.state = strdup(buf);
+        switch (cfdata->border->client.icccm.state)
+          {
+           case ECORE_X_WINDOW_STATE_HINT_WITHDRAWN:
+             snprintf(buf, sizeof(buf), _("Withdrawn"));
+             break;
+
+           case ECORE_X_WINDOW_STATE_HINT_NORMAL:
+             snprintf(buf, sizeof(buf), _("Normal"));
+             break;
+
+           case ECORE_X_WINDOW_STATE_HINT_ICONIC:
+             snprintf(buf, sizeof(buf), _("Iconic"));
+             break;
+
+           default:
+             buf[0] = 0;
+             break;
+          }
+        cfdata->icccm.state = strdup(buf);
      }
    snprintf(buf, sizeof(buf), "0x%08x",
-	    cfdata->border->client.win);
+            cfdata->border->client.win);
    cfdata->icccm.window_id = strdup(buf);
    if (cfdata->border->client.icccm.window_group != 0)
      {
-	snprintf(buf, sizeof(buf), "0x%08x",
-		 cfdata->border->client.icccm.window_group);
-	cfdata->icccm.window_group = strdup(buf);
+        snprintf(buf, sizeof(buf), "0x%08x",
+                 cfdata->border->client.icccm.window_group);
+        cfdata->icccm.window_group = strdup(buf);
      }
    if (cfdata->border->client.icccm.transient_for != 0)
      {
-	snprintf(buf, sizeof(buf), "0x%08x",
-		 cfdata->border->client.icccm.transient_for);
-	cfdata->icccm.transient_for = strdup(buf);
+        snprintf(buf, sizeof(buf), "0x%08x",
+                 cfdata->border->client.icccm.transient_for);
+        cfdata->icccm.transient_for = strdup(buf);
      }
    if (cfdata->border->client.icccm.client_leader != 0)
      {
-	snprintf(buf, sizeof(buf), "0x%08x",
-		 cfdata->border->client.icccm.client_leader);
-	cfdata->icccm.client_leader = strdup(buf);
+        snprintf(buf, sizeof(buf), "0x%08x",
+                 cfdata->border->client.icccm.client_leader);
+        cfdata->icccm.client_leader = strdup(buf);
      }
    switch (cfdata->border->client.icccm.gravity)
      {
       case ECORE_X_GRAVITY_FORGET:
-	snprintf(buf, sizeof(buf), _("Forget/Unmap"));
-	break;
+        snprintf(buf, sizeof(buf), _("Forget/Unmap"));
+        break;
+
       case ECORE_X_GRAVITY_NW:
-	snprintf(buf, sizeof(buf), _("Northwest"));
-	break;
+        snprintf(buf, sizeof(buf), _("Northwest"));
+        break;
+
       case ECORE_X_GRAVITY_N:
-	snprintf(buf, sizeof(buf), _("North"));
-	break;
+        snprintf(buf, sizeof(buf), _("North"));
+        break;
+
       case ECORE_X_GRAVITY_NE:
-	snprintf(buf, sizeof(buf), _("Northeast"));
-	break;
+        snprintf(buf, sizeof(buf), _("Northeast"));
+        break;
+
       case ECORE_X_GRAVITY_W:
-	snprintf(buf, sizeof(buf), _("West"));
-	break;
+        snprintf(buf, sizeof(buf), _("West"));
+        break;
+
       case ECORE_X_GRAVITY_CENTER:
-	snprintf(buf, sizeof(buf), _("Center"));
-	break;
+        snprintf(buf, sizeof(buf), _("Center"));
+        break;
+
       case ECORE_X_GRAVITY_E:
-	snprintf(buf, sizeof(buf), _("East"));
-	break;
+        snprintf(buf, sizeof(buf), _("East"));
+        break;
+
       case ECORE_X_GRAVITY_SW:
-	snprintf(buf, sizeof(buf), _("Southwest"));
-	break;
+        snprintf(buf, sizeof(buf), _("Southwest"));
+        break;
+
       case ECORE_X_GRAVITY_S:
-	snprintf(buf, sizeof(buf), _("South"));
-	break;
+        snprintf(buf, sizeof(buf), _("South"));
+        break;
+
       case ECORE_X_GRAVITY_SE:
-	snprintf(buf, sizeof(buf), _("Southeast"));
-	break;
+        snprintf(buf, sizeof(buf), _("Southeast"));
+        break;
+
       case ECORE_X_GRAVITY_STATIC:
-	snprintf(buf, sizeof(buf), _("Static"));
-	break;
+        snprintf(buf, sizeof(buf), _("Static"));
+        break;
+
       default:
-	buf[0] = 0;
-	break;
+        buf[0] = 0;
+        break;
      }
    cfdata->icccm.gravity = strdup(buf);
    if (cfdata->border->client.icccm.command.argv)
      {
-	int i;
+        int i;
 
-	buf[0] = 0;
-	for (i = 0; i < cfdata->border->client.icccm.command.argc; i++)
-	  {
-	     if ((sizeof(buf) - strlen(buf)) <
-		 (strlen(cfdata->border->client.icccm.command.argv[i]) - 2))
-	       break;
-	     strcat(buf, cfdata->border->client.icccm.command.argv[i]);
-	     strcat(buf, " ");
-	  }
-	cfdata->icccm.command = strdup(buf);
+        buf[0] = 0;
+        for (i = 0; i < cfdata->border->client.icccm.command.argc; i++)
+          {
+             if ((sizeof(buf) - strlen(buf)) <
+                 (strlen(cfdata->border->client.icccm.command.argv[i]) - 2))
+               break;
+             strcat(buf, cfdata->border->client.icccm.command.argv[i]);
+             strcat(buf, " ");
+          }
+        cfdata->icccm.command = strdup(buf);
      }
 
    cfdata->icccm.take_focus = cfdata->border->client.icccm.take_focus;
@@ -278,14 +297,16 @@ _create_data(E_Dialog *cfd, E_Border *bd)
    switch (cfdata->border->client.netwm.state.stacking)
      {
       case 0:
-	cfdata->netwm.stacking = strdup(_("None"));
-	break;
+        cfdata->netwm.stacking = strdup(_("None"));
+        break;
+
       case 1:
-	cfdata->netwm.stacking = strdup(_("Above"));
-	break;
+        cfdata->netwm.stacking = strdup(_("Above"));
+        break;
+
       case 2:
-	cfdata->netwm.stacking = strdup(_("Below"));
-	break;
+        cfdata->netwm.stacking = strdup(_("Below"));
+        break;
      }
 
    cfd->data = cfdata;
@@ -362,17 +383,17 @@ _bd_go(void *data, void *data2)
 
    if (!data2)
      {
-	o = _bd_icccm_create(dia, NULL);
-	e_widget_list_object_append(c, o, 1, 1, 0.0);
-	ob = e_widget_button_add(e_win_evas_get(dia->win), _("NetWM"), "go-next",
-				 _bd_go, dia, (void *)1);
+        o = _bd_icccm_create(dia, NULL);
+        e_widget_list_object_append(c, o, 1, 1, 0.0);
+        ob = e_widget_button_add(e_win_evas_get(dia->win), _("NetWM"), "go-next",
+                                 _bd_go, dia, (void *)1);
      }
    else
      {
-	o = _bd_netwm_create(dia, NULL);
-	e_widget_list_object_append(c, o, 1, 1, 0.0);
-	ob = e_widget_button_add(e_win_evas_get(dia->win), _("ICCCM"), "go-next",
-				 _bd_go, dia, (void *)0);
+        o = _bd_netwm_create(dia, NULL);
+        e_widget_list_object_append(c, o, 1, 1, 0.0);
+        ob = e_widget_button_add(e_win_evas_get(dia->win), _("ICCCM"), "go-next",
+                                 _bd_go, dia, (void *)0);
      }
 
    e_widget_list_object_append(c, ob, 0, 0, 1.0);
@@ -382,27 +403,27 @@ _bd_go(void *data, void *data2)
    e_dialog_show(dia);
 }
 
-#define STR_ENTRY(label, x, y, val) \
-   { \
-      Evas_Coord mw, mh; \
-      ob = e_widget_label_add(evas, label); \
-      if (!cfdata->val) e_widget_disabled_set(ob, 1); \
-      e_widget_frametable_object_append(of, ob, x, y, 1, 1, 1, 1, 0, 1); \
-      ob = e_widget_entry_add(evas, &(cfdata->val), NULL, NULL, NULL); \
-      if (!cfdata->val) e_widget_disabled_set(ob, 1); \
-      e_widget_entry_readonly_set(ob, 1); \
-      e_widget_size_min_get(ob, &mw, &mh); \
-      e_widget_size_min_set(ob, 160, mh); \
-      e_widget_frametable_object_append(of, ob, x + 1, y, 1, 1, 1, 1, 1, 1); \
-   }
-#define CHK_ENTRY(label, x, y, val) \
-   { \
-      ob = e_widget_label_add(evas, label); \
-      e_widget_frametable_object_append(of, ob, x, y, 1, 1, 1, 1, 0, 1); \
-      ob = e_widget_check_add(evas, "", &(cfdata->val)); \
-      e_widget_disabled_set(ob, 1); \
-      e_widget_frametable_object_append(of, ob, x + 1, y, 1, 1, 1, 1, 1, 1); \
-   }
+#define STR_ENTRY(label, x, y, val)                                         \
+  {                                                                         \
+     Evas_Coord mw, mh;                                                     \
+     ob = e_widget_label_add(evas, label);                                  \
+     if (!cfdata->val) e_widget_disabled_set(ob, 1);                        \
+     e_widget_frametable_object_append(of, ob, x, y, 1, 1, 1, 1, 0, 1);     \
+     ob = e_widget_entry_add(evas, & (cfdata->val), NULL, NULL, NULL);      \
+     if (!cfdata->val) e_widget_disabled_set(ob, 1);                        \
+     e_widget_entry_readonly_set(ob, 1);                                    \
+     e_widget_size_min_get(ob, &mw, &mh);                                   \
+     e_widget_size_min_set(ob, 160, mh);                                    \
+     e_widget_frametable_object_append(of, ob, x + 1, y, 1, 1, 1, 1, 1, 1); \
+  }
+#define CHK_ENTRY(label, x, y, val)                                         \
+  {                                                                         \
+     ob = e_widget_label_add(evas, label);                                  \
+     e_widget_frametable_object_append(of, ob, x, y, 1, 1, 1, 1, 0, 1);     \
+     ob = e_widget_check_add(evas, "", & (cfdata->val));                    \
+     e_widget_disabled_set(ob, 1);                                          \
+     e_widget_frametable_object_append(of, ob, x + 1, y, 1, 1, 1, 1, 1, 1); \
+  }
 
 static Evas_Object *
 _bd_icccm_create(E_Dialog *dia, void *data __UNUSED__)
@@ -420,32 +441,32 @@ _bd_icccm_create(E_Dialog *dia, void *data __UNUSED__)
    evas = e_win_evas_get(dia->win);
    o = e_widget_list_add(evas, 0, 0);
    of = e_widget_frametable_add(evas, _("ICCCM Properties"), 0);
-   STR_ENTRY(_("Title"),          0, 0,    icccm.title);
-   STR_ENTRY(_("Name"),           0, 1,    icccm.name);
-   STR_ENTRY(_("Class"),          0, 2,    icccm.class);
-   STR_ENTRY(_("Icon Name"),      0, 3,    icccm.icon_name);
-   STR_ENTRY(_("Machine"),        0, 4,    icccm.machine);
-   STR_ENTRY(_("Role"),           0, 5,    icccm.role);
+   STR_ENTRY(_("Title"), 0, 0, icccm.title);
+   STR_ENTRY(_("Name"), 0, 1, icccm.name);
+   STR_ENTRY(_("Class"), 0, 2, icccm.class);
+   STR_ENTRY(_("Icon Name"), 0, 3, icccm.icon_name);
+   STR_ENTRY(_("Machine"), 0, 4, icccm.machine);
+   STR_ENTRY(_("Role"), 0, 5, icccm.role);
 
-   STR_ENTRY(_("Minimum Size"),   0, 6,    icccm.min);
-   STR_ENTRY(_("Maximum Size"),   0, 7,    icccm.max);
-   STR_ENTRY(_("Base Size"),      0, 8,    icccm.base);
-   STR_ENTRY(_("Resize Steps"),   0, 9,    icccm.step);
+   STR_ENTRY(_("Minimum Size"), 0, 6, icccm.min);
+   STR_ENTRY(_("Maximum Size"), 0, 7, icccm.max);
+   STR_ENTRY(_("Base Size"), 0, 8, icccm.base);
+   STR_ENTRY(_("Resize Steps"), 0, 9, icccm.step);
 
-   STR_ENTRY(_("Aspect Ratio"),   2, 0,   icccm.aspect);
-   STR_ENTRY(_("Initial State"),  2, 1,   icccm.initial_state);
-   STR_ENTRY(_("State"),          2, 2,   icccm.state);
-   STR_ENTRY(_("Window ID"),      2, 3,   icccm.window_id);
-   STR_ENTRY(_("Window Group"),   2, 4,   icccm.window_group);
-   STR_ENTRY(_("Transient For"),  2, 5,   icccm.transient_for);
-   STR_ENTRY(_("Client Leader"),  2, 6,   icccm.client_leader);
-   STR_ENTRY(_("Gravity"),        2, 7,   icccm.gravity);
-   STR_ENTRY(_("Command"),        2, 8,   icccm.command);
+   STR_ENTRY(_("Aspect Ratio"), 2, 0, icccm.aspect);
+   STR_ENTRY(_("Initial State"), 2, 1, icccm.initial_state);
+   STR_ENTRY(_("State"), 2, 2, icccm.state);
+   STR_ENTRY(_("Window ID"), 2, 3, icccm.window_id);
+   STR_ENTRY(_("Window Group"), 2, 4, icccm.window_group);
+   STR_ENTRY(_("Transient For"), 2, 5, icccm.transient_for);
+   STR_ENTRY(_("Client Leader"), 2, 6, icccm.client_leader);
+   STR_ENTRY(_("Gravity"), 2, 7, icccm.gravity);
+   STR_ENTRY(_("Command"), 2, 8, icccm.command);
 
-   CHK_ENTRY(_("Take Focus"),       0, 11, icccm.take_focus);
-   CHK_ENTRY(_("Accepts Focus"),    0, 12, icccm.accepts_focus);
-   CHK_ENTRY(_("Urgent"),           0, 13, icccm.urgent);
-   CHK_ENTRY(_("Request Delete"),   2, 11, icccm.delete_request);
+   CHK_ENTRY(_("Take Focus"), 0, 11, icccm.take_focus);
+   CHK_ENTRY(_("Accepts Focus"), 0, 12, icccm.accepts_focus);
+   CHK_ENTRY(_("Urgent"), 0, 13, icccm.urgent);
+   CHK_ENTRY(_("Request Delete"), 2, 11, icccm.delete_request);
    CHK_ENTRY(_("Request Position"), 2, 12, icccm.request_pos);
 
    e_widget_list_object_append(o, of, 1, 1, 0.0);
@@ -468,18 +489,19 @@ _bd_netwm_create(E_Dialog *dia, void *data __UNUSED__)
    evas = e_win_evas_get(dia->win);
    o = e_widget_list_add(evas, 0, 0);
    of = e_widget_frametable_add(evas, _("NetWM Properties"), 0);
-   STR_ENTRY(_("Name"),         0, 1,    netwm.name);
-   STR_ENTRY(_("Icon Name"),    0, 2,    netwm.icon_name);
-   STR_ENTRY(_("Stacking"),     0, 3,    netwm.stacking);
+   STR_ENTRY(_("Name"), 0, 1, netwm.name);
+   STR_ENTRY(_("Icon Name"), 0, 2, netwm.icon_name);
+   STR_ENTRY(_("Stacking"), 0, 3, netwm.stacking);
 
-   CHK_ENTRY(_("Modal"),        0, 4, netwm.modal);
-   CHK_ENTRY(_("Sticky"),       0, 5, netwm.sticky);
-   CHK_ENTRY(_("Shaded"),       0, 6, netwm.shaded);
+   CHK_ENTRY(_("Modal"), 0, 4, netwm.modal);
+   CHK_ENTRY(_("Sticky"), 0, 5, netwm.sticky);
+   CHK_ENTRY(_("Shaded"), 0, 6, netwm.shaded);
    CHK_ENTRY(_("Skip Taskbar"), 0, 7, netwm.skip_taskbar);
-   CHK_ENTRY(_("Skip Pager"),   0, 8, netwm.skip_pager);
-   CHK_ENTRY(_("Hidden"),       0, 9, netwm.hidden);
-   CHK_ENTRY(_("Fullscreen"),   0, 10, netwm.fullscreen);
+   CHK_ENTRY(_("Skip Pager"), 0, 8, netwm.skip_pager);
+   CHK_ENTRY(_("Hidden"), 0, 9, netwm.hidden);
+   CHK_ENTRY(_("Fullscreen"), 0, 10, netwm.fullscreen);
 
    e_widget_list_object_append(o, of, 1, 1, 0.0);
    return o;
 }
+
