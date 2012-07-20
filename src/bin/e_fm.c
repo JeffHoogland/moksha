5a4201210209f905d9d70f904efdf203548cf687
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2012-07-20 06:31:41 +0000

    more typebuf fixes for "favorites" in efm
    SVN revision: 74207

--- src/bin/e_fm.c
+++ src/bin/e_fm.c
@@ -5651,7 +5651,7 @@ _e_fm2_typebuf_char_append(Evas_Object *obj, const char *ch)
      {
         if (sd->typebuf.buf[0] != '~')
           {
-             if (e_util_strcmp(sd->dev, "desktop") && ecore_file_is_dir(sd->typebuf.buf))
+             if (e_util_strcmp(sd->dev, "desktop") && e_util_strcmp(sd->dev, "favorites") && ecore_file_is_dir(sd->typebuf.buf))
                {
                   sd->typebuf.setting = EINA_TRUE;
                   e_fm2_path_set(obj, "/", sd->typebuf.buf);
@@ -5663,7 +5663,7 @@ _e_fm2_typebuf_char_append(Evas_Object *obj, const char *ch)
              char buf[PATH_MAX];
 
              snprintf(buf, sizeof(buf), "%s/%s", getenv("HOME"), sd->typebuf.buf + 2);
-             if (e_util_strcmp(sd->dev, "desktop") && e_util_strcmp(sd->dev, "favorites")&& ecore_file_is_dir(buf))
+             if (e_util_strcmp(sd->dev, "desktop") && e_util_strcmp(sd->dev, "favorites") && ecore_file_is_dir(buf))
                {
                   sd->typebuf.setting = EINA_TRUE;
                   e_fm2_path_set(obj, "~/", sd->typebuf.buf + 1);
@@ -5701,7 +5701,7 @@ _e_fm2_typebuf_char_backspace(Evas_Object *obj)
      {
         if ((len > 1) || (sd->typebuf.buf[0] != '~'))
           {
-             if (e_util_strcmp(sd->dev, "desktop") && ecore_file_is_dir(sd->typebuf.buf))
+             if (e_util_strcmp(sd->dev, "desktop") && e_util_strcmp(sd->dev, "favorites") && ecore_file_is_dir(sd->typebuf.buf))
                {
                   sd->typebuf.setting = EINA_TRUE;
                   e_fm2_path_set(obj, "/", sd->typebuf.buf);
