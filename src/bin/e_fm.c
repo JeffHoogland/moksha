c98fe5be1444c9bd756cf4b2079cd11294f29acd
Carsten Haitzler <raster@rasterman.com>
2012-07-31 09:50:27 +0000

    please don't break Desktop-1/2 ec. for multi-screen. thanks. :)
    SVN revision: 74634

--- src/bin/e_fm.c
+++ src/bin/e_fm.c
@@ -3117,20 +3117,18 @@ _e_fm2_dev_path_map(const char *dev, const char *path)
           }
         else if (strcmp(dev, "desktop") == 0)
           {
-              size_t size;
              /* this is a virtual device - it's where your favorites list is
               * stored - a dir with
                 .desktop files or symlinks (in fact anything
               * you like
               */
-             size = eina_strlcpy(buf, efreet_desktop_dir_get(), sizeof(buf));
-             if (size >= sizeof(buf)) return NULL;
+             if (!path)
+               snprintf(buf, sizeof(buf), "%s",
+                        efreet_desktop_dir_get());
+             else
+               snprintf(buf, sizeof(buf), "%s-%s", 
+                        efreet_desktop_dir_get(), path);
              ecore_file_mkpath(buf);
-             if (path)
-               {
-                  if (eina_strlcat(buf + size, path, sizeof(buf) - size) >= sizeof(buf) - size)
-                    return NULL;
-               }
           }
         else if (strcmp(dev, "temp") == 0)
           PRT("/tmp");
