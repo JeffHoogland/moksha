506b9ad8e63395101479153a2135923befa1c24c
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2012-07-31 13:18:24 +0000

    whoops forgot this was a list of icons, not icon infos. unbreak!
    SVN revision: 74654

--- src/bin/e_fm.c
+++ src/bin/e_fm.c
@@ -1357,8 +1357,13 @@ e_fm2_config_get(Evas_Object *obj)
 EAPI Eina_List *
 e_fm2_selected_list_get(Evas_Object *obj)
 {
+   Eina_List *list = NULL, *l;
+   E_Fm2_Icon *ic;
+
    EFM_SMART_CHECK(NULL);
-   return eina_list_clone(sd->selected_icons);
+   EINA_LIST_FOREACH(sd->selected_icons, l, ic)
+     list = eina_list_append(list, &(ic->info));
+   return list;
 }
 
 EAPI Eina_List *
