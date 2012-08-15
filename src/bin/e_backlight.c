bd1e81be80ccb97999dd8e25def829181c15f86c
Carsten Haitzler <raster@rasterman.com>
2012-08-15 09:31:10 +0000

    umm lets use xrandr first/ it SHOULD be better than going to /sys as a primary backlight control.
    SVN revision: 75288

--- src/bin/e_backlight.c
+++ src/bin/e_backlight.c
@@ -298,15 +298,6 @@ _e_backlight_update(E_Zone *zone)
    int num = 0;
 
    root = zone->container->manager->root;
-#ifdef HAVE_EEZE
-   _bl_sys_find();
-   if (bl_sysval)
-     {
-        sysmode = MODE_SYS;
-        _bl_sys_level_get();
-        return;
-     }
-#endif
    // try randr
    out = ecore_x_randr_window_outputs_get(root, &num);
    if ((out) && (num > 0) && (ecore_x_randr_output_backlight_available()))
@@ -316,7 +307,17 @@ _e_backlight_update(E_Zone *zone)
      {
         bl_val = x_bl;
         sysmode = MODE_RANDR;
+        return;
      }
+#ifdef HAVE_EEZE
+   _bl_sys_find();
+   if (bl_sysval)
+     {
+        sysmode = MODE_SYS;
+        _bl_sys_level_get();
+        return;
+     }
+#endif
 }
 
 static void
