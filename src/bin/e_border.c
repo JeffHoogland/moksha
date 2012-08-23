177e3276edc1d809679a239e3955a43931c4cf8b
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2012-08-23 08:58:35 +0000

    sloppy focus does not require pointer warping ticket #daniel
    SVN revision: 75608

--- src/bin/e_border.c
+++ src/bin/e_border.c
@@ -2088,14 +2088,7 @@ e_border_focus_set_with_pointer(E_Border *bd)
    if (e_config->focus_policy == E_FOCUS_CLICK) return;
    if (!bd->visible) return;
 
-   if (e_config->focus_policy == E_FOCUS_SLOPPY)
-     {
-        if (!e_border_under_pointer_get(bd->desk, bd))
-          {
-             e_border_pointer_warp_to_center(bd);
-          }
-     }
-   else
+   if (e_config->focus_policy == E_FOCUS_MOUSE)
      {
         e_border_pointer_warp_to_center(bd);
      }
