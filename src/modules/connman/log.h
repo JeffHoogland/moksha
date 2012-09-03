69b4c1537c8d6b0206619e6f2e7dac2bce727350
Bruno Dilly <bdilly@profusion.mobi>
2012-09-03 22:03:13 +0000

    e/connman: add macro INF for eina logging
    SVN revision: 76061

--- src/modules/connman/log.h
+++ src/modules/connman/log.h
@@ -4,10 +4,12 @@
 extern int _e_connman_log_dom;
 
 #undef DBG
+#undef INF
 #undef WRN
 #undef ERR
 
 #define DBG(...) EINA_LOG_DOM_DBG(_e_connman_log_dom, __VA_ARGS__)
+#define INF(...) EINA_LOG_DOM_INFO(_e_connman_log_dom, __VA_ARGS__)
 #define WRN(...) EINA_LOG_DOM_WARN(_e_connman_log_dom, __VA_ARGS__)
 #define ERR(...) EINA_LOG_DOM_ERR(_e_connman_log_dom, __VA_ARGS__)
 
