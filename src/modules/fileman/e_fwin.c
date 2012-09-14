4c9de587c95650511c1b54ae0d39598c496090c8
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2012-09-14 10:22:40 +0000

    directories without parents enjoy having terminals opened in them as well
    SVN revision: 76656

--- src/modules/fileman/e_fwin.c
+++ src/modules/fileman/e_fwin.c
@@ -1950,11 +1950,10 @@ _e_fwin_menu_extend(void *data,
                     E_Fm2_Icon_Info *info __UNUSED__)
 {
    E_Menu_Item *mi;
+   Efreet_Desktop *tdesktop;
 
    if (e_fm2_has_parent_get(obj))
      {
-        Efreet_Desktop *tdesktop;
-        
         mi = e_menu_item_new(m);
         e_menu_item_separator_set(mi, 1);
 
@@ -1965,24 +1964,23 @@ _e_fwin_menu_extend(void *data,
                                                         "e/fileman/default/button/parent"),
                                   "e/fileman/default/button/parent");
         e_menu_item_callback_set(mi, _e_fwin_parent, obj);
-        
-        tdesktop = e_util_terminal_desktop_get();
-        if (tdesktop)
+     }
+   tdesktop = e_util_terminal_desktop_get();
+   if (tdesktop)
+     {
+        E_Fwin_Page *page = data;
+                  
+        mi = e_menu_item_new(m);
+        e_menu_item_label_set(mi, _("Open Terminal Here"));
+        e_menu_item_callback_set(mi, _e_fwin_terminal, page->fwin);
+        if (tdesktop->icon)
           {
-             E_Fwin_Page *page = data;
-                       
-             mi = e_menu_item_new(m);
-             e_menu_item_label_set(mi, _("Open Terminal Here"));
-             e_menu_item_callback_set(mi, _e_fwin_terminal, page->fwin);
-             if (tdesktop->icon)
-               {
-                  if (tdesktop->icon[0] == '/')
-                    e_menu_item_icon_file_set(mi, tdesktop->icon);
-                  else
-                    e_util_menu_item_theme_icon_set(mi, tdesktop->icon);
-               }
-             efreet_desktop_free(tdesktop);
+             if (tdesktop->icon[0] == '/')
+               e_menu_item_icon_file_set(mi, tdesktop->icon);
+             else
+               e_util_menu_item_theme_icon_set(mi, tdesktop->icon);
           }
+        efreet_desktop_free(tdesktop);
      }
    /* FIXME: if info != null then check mime type and offer options based
     * on that
