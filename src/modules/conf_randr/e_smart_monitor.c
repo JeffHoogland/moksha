fed73888f6a577a93983a1d884d8c9a1cbfbb0b8
Christopher Michael <cp.michael@samsung.com>
2012-10-10 10:10:25 +0000

    E (RandR): Make sure we have hTotal and vTotal from the mode before trying to calculate refresh rate.
    Signed-off-by: Christopher Michael <cp.michael@samsung.com>
    
    SVN revision: 77743

--- src/modules/conf_randr/e_smart_monitor.c
+++ src/modules/conf_randr/e_smart_monitor.c
@@ -203,8 +203,9 @@ e_smart_monitor_crtc_set(Evas_Object *obj, E_Randr_Crtc_Info *crtc)
    sd->rotation = _e_smart_monitor_rotation_get(sd->orientation);
    sd->mode = crtc->current_mode;
 
-   sd->rate = (int)((float)sd->mode->dotClock / 
-                    ((float)sd->mode->hTotal * (float)sd->mode->vTotal));
+   if ((sd->mode->hTotal) && (sd->mode->vTotal))
+     sd->rate = (int)((float)sd->mode->dotClock / 
+                      ((float)sd->mode->hTotal * (float)sd->mode->vTotal));
 
    EINA_LIST_FOREACH(crtc->outputs, l, output)
      {
