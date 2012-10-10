40e42c4d2809e1d0d80d1cf4504ed8b957e7d34e
Christopher Michael <cp.michael@samsung.com>
2012-10-10 06:20:39 +0000

    E (RandR): Add function prototypes for getting position, orientation, and mode from monitor.
    Signed-off-by: Christopher Michael <cp.michael@samsung.com>
    
    SVN revision: 77692

--- src/modules/conf_randr/e_smart_monitor.h
+++ src/modules/conf_randr/e_smart_monitor.h
@@ -9,6 +9,9 @@ E_Randr_Crtc_Info *e_smart_monitor_crtc_get(Evas_Object *obj);
 void e_smart_monitor_crtc_geometry_get(Evas_Object *obj, Evas_Coord *x, Evas_Coord *y, Evas_Coord *w, Evas_Coord *h);
 void e_smart_monitor_move_geometry_get(Evas_Object *obj, Evas_Coord *x, Evas_Coord *y, Evas_Coord *w, Evas_Coord *h);
 Eina_Bool e_smart_monitor_moving_get(Evas_Object *obj);
+void e_smart_monitor_position_get(Evas_Object *obj, Evas_Coord *x, Evas_Coord *y);
+Ecore_X_Randr_Orientation e_smart_monitor_orientation_get(Evas_Object *obj);
+Ecore_X_Randr_Mode_Info *e_smart_monitor_mode_get(Evas_Object *obj);
 
 #  define E_SMART_MONITOR_H
 # endif
