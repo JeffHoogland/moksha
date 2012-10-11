c063ed25ac1cab649df282a9050ba3329fc1315c
Christopher Michael <cp.michael@samsung.com>
2012-10-11 14:29:13 +0000

    E (RandR): Add function prototype to reset monitor changes.
    Signed-off-by: Christopher Michael <cp.michael@samsung.com>
    
    SVN revision: 77889

--- src/modules/conf_randr/e_smart_monitor.h
+++ src/modules/conf_randr/e_smart_monitor.h
@@ -24,7 +24,7 @@ void e_smart_monitor_crtc_geometry_get(Evas_Object *obj, Evas_Coord *x, Evas_Coo
 void e_smart_monitor_move_geometry_get(Evas_Object *obj, Evas_Coord *x, Evas_Coord *y, Evas_Coord *w, Evas_Coord *h);
 Eina_Bool e_smart_monitor_changed_get(Evas_Object *obj);
 E_Smart_Monitor_Changes e_smart_monitor_changes_get(Evas_Object *obj);
-
+void e_smart_monitor_changes_sent(Evas_Object *obj);
 
 
 Eina_Bool e_smart_monitor_moving_get(Evas_Object *obj);
