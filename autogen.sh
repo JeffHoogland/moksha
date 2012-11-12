12a2eacfb3736aac2304e44b8593c34efac06246
Carsten Haitzler <raster@rasterman.com>
2012-11-12 14:43:59 +0000

    lets remove the cache files when we autogen.
    SVN revision: 79165

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+rm -rf autom4te.cache
+rm -f aclocal.m4 ltmain.sh config.cache
+
 autoreconf --symlink --install
 
 if [ -z "$NOCONFIGURE" ]; then
-	exec ./configure -C "$@"
+  exec ./configure -C "$@"
 fi
