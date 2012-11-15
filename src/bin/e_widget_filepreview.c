97e91c2e9b15ca70ee66c999f91fa8931038d16b
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2012-11-15 08:20:48 +0000

    no more garbage in text file previews ticket #1601
    SVN revision: 79322

--- src/bin/e_widget_filepreview.c
+++ src/bin/e_widget_filepreview.c
@@ -807,14 +807,14 @@ static void
 _e_wid_fprev_preview_txt_read(void *data __UNUSED__, Ecore_Thread *eth)
 {
    char *text;
-   char buf[FILEPREVIEW_TEXT_PREVIEW_SIZE];
+   char buf[FILEPREVIEW_TEXT_PREVIEW_SIZE + 1] = {0};
    FILE *f;
 
    text = ecore_thread_global_data_find("fprev_file");
    if (!text) return;
    f = fopen(text, "r");
    if (!f) return;
-   fread(buf, sizeof(char), sizeof(buf), f);
+   fread(buf, sizeof(char), FILEPREVIEW_TEXT_PREVIEW_SIZE, f);
    ecore_thread_feedback(eth, evas_textblock_text_utf8_to_markup(NULL, buf));
    fclose(f);
 }
