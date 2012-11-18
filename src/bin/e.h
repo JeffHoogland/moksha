ba53c29261baa1f1c8a6f2cedbc6f755183d42d2
Vincent Torri <vincent.torri@gmail.com>
2012-11-18 13:42:48 +0000

    E17: update alloca detection
    SVN revision: 79427

--- src/bin/e.h
+++ src/bin/e.h
@@ -24,22 +24,31 @@
 #  define _FILE_OFFSET_BITS 64
 # endif
 
-# ifdef HAVE_ALLOCA_H
-#  include <alloca.h>
-# elif defined __GNUC__
+#ifdef STDC_HEADERS
+# include <stdlib.h>
+# include <stddef.h>
+#else
+# ifdef HAVE_STDLIB_H
+#  include <stdlib.h>
+# endif
+#endif
+#ifdef HAVE_ALLOCA_H
+# include <alloca.h>
+#elif !defined alloca
+# ifdef __GNUC__
 #  define alloca __builtin_alloca
 # elif defined _AIX
 #  define alloca __alloca
 # elif defined _MSC_VER
 #  include <malloc.h>
 #  define alloca _alloca
-# else
-#  include <stddef.h>
+# elif !defined HAVE_ALLOCA
 #  ifdef  __cplusplus
 extern "C"
 #  endif
-void *alloca(size_t);
+void *alloca (size_t);
 # endif
+#endif
 
 # ifdef __linux__
 #  include <features.h>
