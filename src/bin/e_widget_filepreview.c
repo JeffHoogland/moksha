7243e0c66517217363a6e705d47faccc09ce77a4
Carsten Haitzler <raster@rasterman.com>
2012-11-24 01:41:52 +0000

    and keep t in too. oops. last second edit.
    SVN revision: 79618

--- src/bin/e_widget_filepreview.c
+++ src/bin/e_widget_filepreview.c
@@ -190,7 +190,7 @@ _e_wid_fprev_preview_video_opened(E_Widget_Data *wd, Evas_Object *obj, void *eve
 static void
 _e_wid_fprev_preview_video_change(void *data, Evas_Object *obj)
 {
-   double pos, tot;
+   double pos, tot, t;
 
    tot = emotion_object_play_length_get(data);
    t = emotion_object_position_get(data);
