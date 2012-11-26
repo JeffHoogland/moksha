e2a529f0dcfd4d1ea60fbbd0deb0c950a8fcba67
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2012-11-26 13:37:19 +0000

    click focus in wizard now unsets winlist pointer warping
    SVN revision: 79718

--- src/modules/wizard/page_060.c
+++ src/modules/wizard/page_060.c
@@ -58,6 +58,8 @@ wizard_page_hide(E_Wizard_Page *pg __UNUSED__)
         e_config->always_click_to_focus = 0;
         e_config->focus_last_focused_per_desktop = 1;
         e_config->pointer_slide = 0;
+        e_config->winlist_warp_while_selecting = 0;
+        e_config->winlist_warp_at_end = 0;
      }
    else
      {
