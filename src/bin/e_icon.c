463617699c21228bba986f24be42bc4e8702b120
José Roberto de Souza <zehortigoza@profusion.mobi>
2012-12-10 06:58:58 +0000

    From: José Roberto de Souza <zehortigoza@profusion.mobi> Subject: [E-devel] [PATCH] e: Return EINA_FALSE if icon path was not found
    SVN revision: 80567

--- src/bin/e_icon.c
+++ src/bin/e_icon.c
@@ -444,7 +444,7 @@ e_icon_fdo_icon_set(Evas_Object *obj, const char *icon)
    if (!sd->fdo) return EINA_FALSE;
 
    path = efreet_icon_path_find(e_config->icon_theme, sd->fdo, sd->size);
-   if (!path) return EINA_TRUE;
+   if (!path) return EINA_FALSE;
 
    len = strlen(icon);
    if ((len > 4) && (!strcasecmp(icon + len - 4, ".edj")))
