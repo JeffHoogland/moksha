6c751918b843c53c0437ce3d08ff27ee3a039257
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2012-12-11 13:04:53 +0000

    warn--
    SVN revision: 80656

--- src/modules/comp/e_mod_config.c
+++ src/modules/comp/e_mod_config.c
@@ -1143,7 +1143,7 @@ _advanced_create_widgets(E_Config_Dialog *cfd,
                          Evas *evas,
                          E_Config_Dialog_Data *cfdata)
 {
-   Evas_Object *ob,*ol, *ol2, *of, *otb, *oi, *orec0, *tab;
+   Evas_Object *ob,*ol, *of, *otb, *oi, *orec0, *tab;
    E_Radio_Group *rg;
 
    orec0 = evas_object_rectangle_add(evas);
