6ae01a1b098705d616af01b8341369c4ff32b25e
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2012-12-13 19:12:37 +0000

    unbreak e_intl.h #ifdefs
    SVN revision: 80889

--- src/bin/e_intl.h
+++ src/bin/e_intl.h
@@ -1,27 +1,28 @@
-#ifdef E_INTERNAL
-#if E_INTERNAL
-
 #ifdef E_TYPEDEFS
-#ifdef HAVE_GETTEXT
+# ifdef E_INTERNAL
+#  if E_INTERNAL
+
+#   ifdef HAVE_GETTEXT
 #define _(str) gettext(str)
 #define d_(str, dom) dgettext(PACKAGE dom, str)
 #define P_(str, str_p, n) ngettext(str, str_p, n)
 #define dP_(str, str_p, n, dom) dngettext(PACKAGE dom, str, str_p, n)
-#else
+#   else
 #define _(str) (str)
 #define d_(str, dom) (str)
 #define P_(str, str_p, n) (str_p)
 #define dP_(str, str_p, n, dom) (str_p)
-#endif
+#   endif
 /* These macros are used to just mark strings for translation, this is useful
  * for string lists which are not dynamically allocated
  */
 #define N_(str) (str)
 #define NP_(str, str_p) str, str_p
-
+#  endif
+# endif
 typedef struct _E_Locale_Parts E_Locale_Parts;
 
-#else
+# else
 #ifndef E_INTL_H
 #define E_INTL_H
 
@@ -61,8 +62,5 @@ EAPI void		 e_intl_locale_parts_free(E_Locale_Parts *locale_parts);
 EAPI char               *e_intl_locale_parts_combine(E_Locale_Parts *locale_parts, int mask);
 EAPI char		*e_intl_locale_charset_canonic_get(const char *charset);
 
-#endif
-#endif
-
-#endif
+# endif
 #endif
