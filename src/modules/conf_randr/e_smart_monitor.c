710b996818b1b3e26b4415f258abd07b78760024
Christopher Michael <cp.michael@samsung.com>
2012-12-17 09:19:55 +0000

    Add code to limit resize not go below minimum resolution.
    Signed-off-by: Christopher Michael <cp.michael@samsung.com>
    
    SVN revision: 81090

--- src/modules/conf_randr/e_smart_monitor.c
+++ src/modules/conf_randr/e_smart_monitor.c
@@ -839,7 +839,10 @@ _e_smart_monitor_resize_event(E_Smart_Data *sd, Evas_Object *mon, void *event)
    e_layout_coord_canvas_to_virtual(sd->layout.obj, 
                                     (mw + dx), (mh + dy), &nw, &nh);
 
-   /* max sure we cannot resize beyond the max */
+   /* make sure we cannot resize below the min */
+   if ((nw < sd->min.w) || (nh < sd->min.h)) return;
+
+   /* make sure we cannot resize beyond the max */
    if ((nw > sd->max.w) || (nh > sd->max.h)) return;
 
    /* actually resize the monitor */
