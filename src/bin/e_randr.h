06a76bcb270173fa0810cdb78d6c1d15dd12cc4b
Christopher Michael <cp.michael@samsung.com>
2012-12-17 14:21:39 +0000

    Add e_randr_store_configuration function to exposed headers (NB: It was exposed in the serialization file, but never provided by the e_randr header).
    Signed-off-by: Christopher Michael <cp.michael@samsung.com>
    
    SVN revision: 81146

--- src/bin/e_randr.h
+++ src/bin/e_randr.h
@@ -181,8 +181,10 @@ struct _E_Randr_Serialized_Setup
    Eina_List *outputs_policies;
 };
 
-EINTERN Eina_Bool e_randr_init(void);
 EAPI Eina_Bool e_randr_screen_info_refresh(void);
+EAPI void e_randr_store_configuration(E_Randr_Configuration_Store_Modifier modifier);
+
+EINTERN Eina_Bool e_randr_init(void);
 EINTERN int e_randr_shutdown(void);
 EINTERN Eina_Bool e_randr_try_restore_configuration(void);
 EINTERN E_Randr_Serialized_Setup *e_randr_serialized_setup_new(void);
