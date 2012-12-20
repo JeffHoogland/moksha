7d1cc24f5cb9b37698e9ae2b4db081ff8075717b
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2012-12-20 13:52:49 +0000

    only do pointer warp for sloppy focus if the mouse is currently over another window, otherwise just do the other focus-in manually ticket #morlenxus
    SVN revision: 81455

--- src/bin/e_border.c
+++ src/bin/e_border.c
@@ -2153,10 +2153,10 @@ e_border_focus_set_with_pointer(E_Border *bd)
 
    if (e_config->focus_policy == E_FOCUS_SLOPPY)
      {
-        if (bd != e_border_under_pointer_get(bd->desk, bd))
-          {
-             e_border_pointer_warp_to_center(bd);
-          }
+        E_Border *pbd;
+        pbd = e_border_under_pointer_get(bd->desk, bd);
+        if (pbd && (pbd != bd)) e_border_pointer_warp_to_center(bd);
+        else e_border_focus_set(bd, 1, 0);
      }
    else
      {
