b7d8881e1e59cf175ce481bd2ed49c8d158777a6
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2013-01-23 10:00:09 +0000

    backport previous import config dialog leak fix
    SVN revision: 83138

--- src/bin/e_import_config_dialog.c
+++ src/bin/e_import_config_dialog.c
@@ -449,7 +449,11 @@ e_import_config_dialog_show(E_Container *con, const char *path, Ecore_End_Cb ok,
    if (!import) return NULL;
 
    dia = e_dialog_new(con, "E", "_import_config_dialog");
-   if (!dia) return NULL;
+   if (!dia)
+     {
+        e_object_del(E_OBJECT(import));
+        return NULL;
+     }
    e_dialog_resizable_set(dia, 1);
    
    e_dialog_title_set(dia, _("Import Settings..."));
