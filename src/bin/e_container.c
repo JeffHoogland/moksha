d8bfb3cdd39685ae3d9f4dc730f04bfe0404288a
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2013-02-05 07:59:51 +0000

    backport previous commit with shelf deletion during screen resolution change since this almost certainly fixes crashes
    SVN revision: 83615

--- src/bin/e_container.c
+++ src/bin/e_container.c
@@ -1190,20 +1190,9 @@ _e_container_resize_handle(E_Container *con)
 
              EINA_LIST_FREE(zones, zone)
                {
-                  Eina_List *shelves, *ll2, *del_shelves;
-                  E_Shelf *es;
                   E_Border_List *bl;
                   E_Border *bd;
 
-                  /* delete any shelves on this zone */
-                  shelves = e_shelf_list();
-                  del_shelves = NULL;
-                  EINA_LIST_FOREACH(shelves, ll2, es)
-                    {
-                       if (es->zone == zone)
-                         del_shelves = eina_list_append(del_shelves, es);
-                    }
-                  E_FREE_LIST(del_shelves, e_object_del);
                   bl = e_container_border_list_first(zone->container);
                   while ((bd = e_container_border_list_next(bl)))
                     {
