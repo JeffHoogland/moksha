e7c931500493c8c0c4d37e1d8520810511fd4c26
Christopher Michael <cp.michael@samsung.com>
2013-02-19 10:55:28 +0000

    Backport: 8a53055 :: Tell randr widget to create monitors.
    Signed-off-by: Christopher Michael <cp.michael@samsung.com>
    
    SVN revision: 84124
    Signed-off-by: Deon Thomas <PrinceAMD.Elive@gmail.com>

--- src/modules/conf_randr/e_int_config_randr.c
+++ src/modules/conf_randr/e_int_config_randr.c
@@ -85,6 +85,9 @@ _basic_create(E_Config_Dialog *cfd EINA_UNUSED, Evas *evas, E_Config_Dialog_Data
    /* try to create randr smart widget */
    if ((cfdata->o_randr = e_smart_randr_add(evas)))
      {
+        /* tell randr widget to create monitors */
+        e_smart_randr_monitors_create(cfdata->o_randr);
+
         /* append randr widget to list object */
         e_widget_list_object_append(o, cfdata->o_randr, 1, 1, 0.5);
      }
