d5c6502ed0ac80ecc6f7e2195889e009e9b75a52
Christopher Michael <cp.michael@samsung.com>
2013-02-19 10:59:33 +0000

    Backport: 54f611f :: Add function prototype for monitor clone set.
    Signed-off-by: Christopher Michael <cp.michael@samsung.com>
    
    SVN revision: 84208
    Signed-off-by: Deon Thomas <PrinceAMD.Elive@gmail.com>

--- src/modules/conf_randr/e_smart_monitor.h
+++ src/modules/conf_randr/e_smart_monitor.h
@@ -5,11 +5,13 @@
 
 Evas_Object *e_smart_monitor_add(Evas *evas);
 void e_smart_monitor_crtc_set(Evas_Object *obj, Ecore_X_Randr_Crtc crtc, Evas_Coord cx, Evas_Coord cy, Evas_Coord cw, Evas_Coord ch);
+Ecore_X_Randr_Crtc e_smart_monitor_crtc_get(Evas_Object *obj);
 void e_smart_monitor_output_set(Evas_Object *obj, Ecore_X_Randr_Output output);
 void e_smart_monitor_grid_set(Evas_Object *obj, Evas_Object *grid, Evas_Coord gx, Evas_Coord gy, Evas_Coord gw, Evas_Coord gh);
 void e_smart_monitor_grid_virtual_size_set(Evas_Object *obj, Evas_Coord vw, Evas_Coord vh);
 void e_smart_monitor_background_set(Evas_Object *obj, Evas_Coord dx, Evas_Coord dy);
 void e_smart_monitor_current_geometry_set(Evas_Object *obj, Evas_Coord x, Evas_Coord y, Evas_Coord w, Evas_Coord h);
+void e_smart_monitor_clone_set(Evas_Object *obj, Evas_Object *parent);
 
 # endif
 #endif
