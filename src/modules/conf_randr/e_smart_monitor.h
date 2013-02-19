f144a53b8782190002313072ed0e779300adcebe
Christopher Michael <cp.michael@samsung.com>
2013-02-19 10:56:40 +0000

    Backport: c476e30 :: Add function prototypes.
    Signed-off-by: Christopher Michael <cp.michael@samsung.com>
    
    SVN revision: 84148
    Signed-off-by: Deon Thomas <PrinceAMD.Elive@gmail.com>

--- src/modules/conf_randr/e_smart_monitor.h
+++ src/modules/conf_randr/e_smart_monitor.h
@@ -4,8 +4,10 @@
 #  define E_SMART_MONITOR_H
 
 Evas_Object *e_smart_monitor_add(Evas *evas);
-void e_smart_monitor_crtc_set(Evas_Object *obj, E_Randr_Crtc_Config *crtc);
-void e_smart_monitor_output_set(Evas_Object *obj, E_Randr_Output_Config *output);
+void e_smart_monitor_crtc_set(Evas_Object *obj, Ecore_X_Randr_Crtc crtc);
+void e_smart_monitor_output_set(Evas_Object *obj, Ecore_X_Randr_Output output);
+void e_smart_monitor_virtual_size_set(Evas_Object *obj, Evas_Coord vw, Evas_Coord vh);
+void e_smart_monitor_grid_set(Evas_Object *obj, Evas_Object *grid);
 
 # endif
 #endif
