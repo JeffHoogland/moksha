0137ff7e1f53de92483f8aed807aa85bff6ec2ff
Christopher Michael <cp.michael@samsung.com>
2013-02-19 10:56:51 +0000

    Backport: 6ca0528 :: Remove function call to monitor_virtual_size_set.
    Signed-off-by: Christopher Michael <cp.michael@samsung.com>
    
    SVN revision: 84152
    Signed-off-by: Deon Thomas <PrinceAMD.Elive@gmail.com>

--- src/modules/conf_randr/e_smart_randr.c
+++ src/modules/conf_randr/e_smart_randr.c
@@ -198,9 +198,6 @@ e_smart_randr_monitors_create(Evas_Object *obj)
                   /* tell monitor what the virtual grid is */
                   e_smart_monitor_grid_set(mon, sd->o_grid);
 
-                  /* tell monitor what the virtual grid size is */
-                  e_smart_monitor_virtual_size_set(mon, sd->vw, sd->vh);
-
                   /* tell monitor what crtc it uses */
                   e_smart_monitor_crtc_set(mon, crtcs[i]);
 
