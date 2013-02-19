4f8bb6f04cabe8c2421ca198aad06ce0508e4755
Christopher Michael <cp.michael@samsung.com>
2013-02-19 10:55:42 +0000

    Backport: 82a0808 :: Add function prototypes for setting monitor crtc and output config.
    Signed-off-by: Christopher Michael <cp.michael@samsung.com>
    
    SVN revision: 84129
    Signed-off-by: Deon Thomas <PrinceAMD.Elive@gmail.com>

--- src/modules/conf_randr/e_smart_monitor.h
+++ src/modules/conf_randr/e_smart_monitor.h
@@ -4,6 +4,8 @@
 #  define E_SMART_MONITOR_H
 
 Evas_Object *e_smart_monitor_add(Evas *evas);
+void e_smart_monitor_crtc_set(Evas_Object *obj, E_Randr_Crtc_Config *crtc);
+void e_smart_monitor_output_set(Evas_Object *obj, E_Randr_Output_Config *output);
 
 # endif
 #endif
