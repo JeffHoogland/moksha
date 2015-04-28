62d4c0c6956bf2a3f970402f24e1dee05841120a
Jeff Hoogland <jeffhoogland@linux.com>
2015-04-27 22:55:14 -0500

    Updated authors file, removed extra desktop files

--- debian/e17/usr/include/enlightenment/e_int_border_prop.h
+++
@@ -1,9 +0,0 @@
-#ifdef E_TYPEDEFS
-#else
-#ifndef E_INT_BORDER_PROP_H
-#define E_INT_BORDER_PROP_H
-
-EAPI void e_int_border_prop(E_Border *bd);
-
-#endif
-#endif
--- debian/e17/usr/include/enlightenment/e_widget_cslider.h
+++
@@ -1,9 +0,0 @@
-#ifndef E_WIDGET_CSLIDER_H
-#define E_WIDGET_CSLIDER_H
-
-Evas_Object * e_widget_cslider_add(Evas *e, E_Color_Component mode, E_Color *color, int vertical, int fixed);
-void e_widget_cslider_color_value_set(Evas_Object *obj, E_Color *ec);
-void e_widget_cslider_update(Evas_Object *obj);
-void e_widget_cslider_mode_set(Evas_Object *obj, E_Color_Component mode);
-
-#endif
