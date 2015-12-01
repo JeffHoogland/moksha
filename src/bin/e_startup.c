cb07a96668321e9ebb4e10bfe427606b7891db12
Jeff Hoogland <jeffhoogland@linux.com>
2015-12-01 12:17:32 -0600

    Trying different file permissions

--- src/bin/e_startup.c
+++ src/bin/e_startup.c
@@ -99,10 +99,7 @@ _e_startup_event_cb(void *data, int ev_type __UNUSED__, void *ev)
    e_user_dir_concat_static(shfile, "applications/startup/startupcommands");
    if (!ecore_file_exists(shfile)) 
    { 
-       FILE *f; 
-       f = fopen(shfile, "w"); 
-       fclose(f);
-       chmod(shfile, S_IXOTH);
+       create(shfile, S_IRWXU | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH);
    }
    e_exec(NULL, NULL, shfile, NULL, NULL);
    return ECORE_CALLBACK_PASS_ON;
