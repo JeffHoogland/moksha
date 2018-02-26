ee8da18882d17a46cdc6a171d5b75fcbf61414fb
Rbt Y-Lee <ylee@bodhilinux.com>
2018-02-26 16:22:42 -0500

    Build cleanup

--- debian/e17.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the e17 package
-#
-0 4	* * *	root	[ -x /usr/bin/e17_maintenance ] && /usr/bin/e17_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(e17):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="e17" command="/usr/bin/e17"
