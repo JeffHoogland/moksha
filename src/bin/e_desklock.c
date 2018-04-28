23ac197203f0009535ec65d22cb7fbae213c32b1
Rbt Y-Lee <ylee@bodhilinux.com>
2018-04-28 16:07:46 -0400

    disable bindings during desklock.

--- src/bin/e_desklock.c
+++ src/bin/e_desklock.c
@@ -231,6 +231,7 @@ e_desklock_show(Eina_Bool suspend)
         _e_custom_desklock_exe =
           ecore_exe_run(e_config->desklock_custom_desklock_cmd, NULL);
         _e_desklock_state = EINA_TRUE;
+        e_bindings_disabled_set(1);
         return 1;
      }
 
@@ -370,6 +371,7 @@ works:
 
    e_util_env_set("E_DESKLOCK_LOCKED", "locked");
    _e_desklock_state = EINA_TRUE;
+   e_bindings_disabled_set(1);
    return 1;
 }
 
@@ -391,6 +393,7 @@ e_desklock_hide(void)
      }
 
    _e_desklock_state = EINA_FALSE;
+   e_bindings_disabled_set(0);
    ev = E_NEW(E_Event_Desklock, 1);
    ev->on = 0;
    ev->suspend = 1;
