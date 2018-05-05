bda58466813afcd4ebfa48bad76ff73bfa409524
Rbt Y-Lee <ylee@bodhilinux.com>
2018-05-05 15:42:36 -0400

    Use direct path for setting icons when icon is a path

--- src/bin/e_utils.c
+++ src/bin/e_utils.c
@@ -426,6 +426,11 @@ _e_util_icon_fdo_set(Evas_Object *obj, const char *icon)
 EAPI int
 e_util_icon_theme_set(Evas_Object *obj, const char *icon)
 {
+   if (icon && (icon[0] == '/'))
+     {
+        e_icon_file_set(obj, icon);
+        return 1;
+     }
    if (e_config->icon_theme_overrides)
      {
         if (_e_util_icon_fdo_set(obj, icon))
