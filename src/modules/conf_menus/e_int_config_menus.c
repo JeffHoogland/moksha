f08f7294f6fa4b1f5edfe32201996d9164cb894f
Rbt Y-Lee <ylee@bodhilinux.com>
2018-06-01 16:35:20 -0400

    Nothing here: code cleanup.

--- src/modules/conf_menus/e_int_config_menus.c
+++ src/modules/conf_menus/e_int_config_menus.c
@@ -1,7 +1,7 @@
 #include "e.h"
 
 /* local structures */
-struct _E_Config_Dialog_Data 
+struct _E_Config_Dialog_Data
 {
    int show_favs, show_apps, scroll_toggle;
    int show_name, show_generic, show_comment;
@@ -21,7 +21,7 @@ static int _basic_apply(E_Config_Dialog *cfd, E_Config_Dialog_Data *cfdata);
 static int _basic_check_changed(E_Config_Dialog *cfd, E_Config_Dialog_Data *cfdata);
 
 E_Config_Dialog *
-e_int_config_menus(E_Container *con, const char *params __UNUSED__) 
+e_int_config_menus(E_Container *con, const char *params __UNUSED__)
 {
    E_Config_Dialog *cfd;
    E_Config_Dialog_View *v;
@@ -35,7 +35,7 @@ e_int_config_menus(E_Container *con, const char *params __UNUSED__)
    v->basic.apply_cfdata = _basic_apply;
    v->basic.check_changed = _basic_check_changed;
 
-   cfd = e_config_dialog_new(con, _("Menu Settings"), "E", "menus/menu_settings", 
+   cfd = e_config_dialog_new(con, _("Menu Settings"), "E", "menus/menu_settings",
                              "preferences-menus", 0, v, NULL);
    return cfd;
 }
@@ -55,7 +55,7 @@ static void
 _fill_data(E_Config_Dialog_Data *cfdata __UNUSED__)
 {
    if (e_config->default_system_menu)
-     cfdata->default_system_menu = 
+     cfdata->default_system_menu =
      eina_stringshare_add(e_config->default_system_menu);
    else
      cfdata->default_system_menu = NULL;
@@ -67,7 +67,7 @@ _fill_data(E_Config_Dialog_Data *cfdata __UNUSED__)
    cfdata->show_comment = e_config->menu_eap_comment_show;
    cfdata->menu_gadcon_client_toplevel = e_config->menu_gadcon_client_toplevel;
    cfdata->scroll_speed = e_config->menus_scroll_speed;
-   cfdata->fast_mouse_move_threshhold = 
+   cfdata->fast_mouse_move_threshhold =
      e_config->menus_fast_mouse_move_threshhold;
    cfdata->click_drag_timeout = e_config->menus_click_drag_timeout;
    cfdata->autoscroll_margin = e_config->menu_autoscroll_margin;
@@ -77,7 +77,7 @@ _fill_data(E_Config_Dialog_Data *cfdata __UNUSED__)
 static void
 _free_data(E_Config_Dialog *cfd __UNUSED__, E_Config_Dialog_Data *cfdata)
 {
-   if (cfdata->default_system_menu) 
+   if (cfdata->default_system_menu)
      eina_stringshare_del(cfdata->default_system_menu);
    E_FREE(cfdata);
 }
@@ -87,7 +87,7 @@ check_menu_dir(const char *dir, Eina_List **menus)
 {
    char buf[PATH_MAX], *file;
    Eina_List *files;
-   
+
    snprintf(buf, sizeof(buf), "%s/menus", dir);
    files = ecore_file_ls(buf);
    EINA_LIST_FREE(files, file)
@@ -100,7 +100,7 @@ check_menu_dir(const char *dir, Eina_List **menus)
         free(file);
      }
 }
-                            
+
 void
 get_menus(Eina_List **menus)
 {
@@ -120,12 +120,12 @@ get_menus(Eina_List **menus)
         NULL
      };
    int i, newdir;
-   
+
    e_user_homedir_concat(buf, sizeof(buf), ".config");
    check_menu_dir(buf, menus);
-   
+
    for (i = 0; dirs[i]; i++) check_menu_dir(dirs[i], menus);
-   
+
    newdir = 1;
    snprintf(buf, sizeof(buf), "%s/etc/xdg", e_prefix_get());
    for (i = 0; dirs[i]; i++)
@@ -146,24 +146,24 @@ _create_menus_list(Evas *evas, E_Config_Dialog_Data *cfdata)
    Evas_Object *ob;
    char *file;
    int sel = -1, i = 0;
-   
+
    get_menus(&menus);
-   ob = e_widget_ilist_add(evas, (32 * e_scale), (32 * e_scale), 
+   ob = e_widget_ilist_add(evas, (32 * e_scale), (32 * e_scale),
                            &(cfdata->default_system_menu));
    e_widget_size_min_set(ob, (100 * e_scale), (140 * e_scale));
    e_widget_ilist_freeze(ob);
-   
+
    EINA_LIST_FREE(menus, file)
      {
         char buf[PATH_MAX], buf2[PATH_MAX], *p, *p2, *tlabel, *tdesc;
         const char *label;
-        
+
         label = file;
         tlabel = NULL;
         tdesc = NULL;
-        e_user_homedir_concat(buf, sizeof(buf), 
+        e_user_homedir_concat(buf, sizeof(buf),
                               ".config/menus/applications.menu");
-        snprintf(buf2, sizeof(buf2), "%s/etc/xdg/menus/moksha-applications.menu", 
+        snprintf(buf2, sizeof(buf2), "%s/etc/xdg/menus/moksha-applications.menu",
                  e_prefix_get());
         if (!strcmp("/etc/xdg/menus/applications.menu", file))
           {
@@ -241,9 +241,9 @@ _create_menus_list(Evas *evas, E_Config_Dialog_Data *cfdata)
      }
    e_widget_ilist_go(ob);
    e_widget_ilist_thaw(ob);
-   
+
    if (sel >= 0) e_widget_ilist_selected_set(ob, sel);
-   
+
    return ob;
 }
 
@@ -255,47 +255,47 @@ _basic_create(E_Config_Dialog *cfd __UNUSED__, Evas *evas, E_Config_Dialog_Data 
    otb = e_widget_toolbook_add(evas, (24 * e_scale), (24 * e_scale));
 
    ol = e_widget_list_add(evas, 0, 0);
-   of = e_widget_framelist_add(evas, _("Main Menu"), 0); 
+   of = e_widget_framelist_add(evas, _("Main Menu"), 0);
    ow = e_widget_check_add(evas, _("Favorites"), &(cfdata->show_favs));
-   e_widget_framelist_object_append(of, ow); 
+   e_widget_framelist_object_append(of, ow);
    ow = e_widget_check_add(evas, _("Applications"), &(cfdata->show_apps));
    e_widget_framelist_object_append(of, ow);
    e_widget_list_object_append(ol, of, 1, 0, 0.5);
 
-   of = e_widget_framelist_add(evas, _("Applications Display"), 0); 
+   of = e_widget_framelist_add(evas, _("Applications Display"), 0);
    ow = e_widget_check_add(evas, _("Name"), &(cfdata->show_name));
-   e_widget_framelist_object_append(of, ow); 
+   e_widget_framelist_object_append(of, ow);
    ow = e_widget_check_add(evas, _("Generic"), &(cfdata->show_generic));
-   e_widget_framelist_object_append(of, ow); 
+   e_widget_framelist_object_append(of, ow);
    ow = e_widget_check_add(evas, _("Comments"), &(cfdata->show_comment));
-   e_widget_framelist_object_append(of, ow); 
+   e_widget_framelist_object_append(of, ow);
    e_widget_list_object_append(ol, of, 1, 0, 0.5);
 
-   of = e_widget_framelist_add(evas, _("Gadgets"), 0); 
+   of = e_widget_framelist_add(evas, _("Gadgets"), 0);
    ow = e_widget_check_add(evas, _("Show gadget settings in top-level"), &(cfdata->menu_gadcon_client_toplevel));
    e_widget_framelist_object_append(of, ow);
    e_widget_list_object_append(ol, of, 1, 1, 0.5);
-   e_widget_toolbook_page_append(otb, NULL, _("Menus"), ol, 1, 0, 1, 0, 
+   e_widget_toolbook_page_append(otb, NULL, _("Menus"), ol, 1, 0, 1, 0,
                                  0.5, 0.0);
 
    ol = e_widget_list_add(evas, 0, 0);
    ow = _create_menus_list(evas, cfdata);
-   e_widget_list_object_append(ol, ow, 1, 0, 0.5);   
-   e_widget_toolbook_page_append(otb, NULL, _("Applications"), ol, 1, 0, 1, 0, 
+   e_widget_list_object_append(ol, ow, 1, 0, 0.5);
+   e_widget_toolbook_page_append(otb, NULL, _("Applications"), ol, 1, 0, 1, 0,
                                  0.5, 0.0);
 
    ol = e_widget_list_add(evas, 0, 0);
    ow = e_widget_label_add(evas, _("Margin"));
    e_widget_list_object_append(ol, ow, 1, 0, 0.5);
-   ow = e_widget_slider_add(evas, 1, 0, _("%2.0f pixels"), 0, 50, 1, 0, NULL, 
+   ow = e_widget_slider_add(evas, 1, 0, _("%2.0f pixels"), 0, 50, 1, 0, NULL,
                             &(cfdata->autoscroll_margin), 100);
    e_widget_list_object_append(ol, ow, 1, 0, 0.5);
    ow = e_widget_label_add(evas, _("Cursor Margin"));
    e_widget_list_object_append(ol, ow, 1, 0, 0.5);
-   ow = e_widget_slider_add(evas, 1, 0, _("%2.0f pixels"), 0, 50, 1, 0, NULL, 
+   ow = e_widget_slider_add(evas, 1, 0, _("%2.0f pixels"), 0, 50, 1, 0, NULL,
                             &(cfdata->autoscroll_cursor_margin), 100);
    e_widget_list_object_append(ol, ow, 1, 0, 0.5);
-   e_widget_toolbook_page_append(otb, NULL, _("Autoscroll"), ol, 1, 0, 1, 0, 
+   e_widget_toolbook_page_append(otb, NULL, _("Autoscroll"), ol, 1, 0, 1, 0,
                                  0.5, 0.0);
 
    ol = e_widget_list_add(evas, 0, 0);
@@ -303,20 +303,20 @@ _basic_create(E_Config_Dialog *cfd __UNUSED__, Evas *evas, E_Config_Dialog_Data 
    e_widget_list_object_append(ol, ow, 1, 0, 0.5);
    ow = e_widget_label_add(evas, _("Menu Scroll Speed"));
    e_widget_list_object_append(ol, ow, 1, 0, 0.5);
-   ow = e_widget_slider_add(evas, 1, 0, _("%5.0f pixels/s"), 0, 20000, 100, 
+   ow = e_widget_slider_add(evas, 1, 0, _("%5.0f pixels/s"), 0, 20000, 100,
                             0, &(cfdata->scroll_speed), NULL, 100);
    e_widget_list_object_append(ol, ow, 1, 0, 0.5);
    ow = e_widget_label_add(evas, _("Fast Mouse Move Threshold"));
    e_widget_list_object_append(ol, ow, 1, 0, 0.5);
-   ow = e_widget_slider_add(evas, 1, 0, _("%4.0f pixels/s"), 0, 2000, 10, 
+   ow = e_widget_slider_add(evas, 1, 0, _("%4.0f pixels/s"), 0, 2000, 10,
                             0, &(cfdata->fast_mouse_move_threshhold), NULL, 100);
    e_widget_list_object_append(ol, ow, 1, 0, 0.5);
    ow = e_widget_label_add(evas, _("Click Drag Timeout"));
    e_widget_list_object_append(ol, ow, 1, 0, 0.5);
-   ow = e_widget_slider_add(evas, 1, 0, _("%2.2f s"), 0, 10, 0.25, 
+   ow = e_widget_slider_add(evas, 1, 0, _("%2.2f s"), 0, 10, 0.25,
                             0, &(cfdata->click_drag_timeout), NULL, 100);
    e_widget_list_object_append(ol, ow, 1, 0, 0.5);
-   e_widget_toolbook_page_append(otb, NULL, _("Miscellaneous"), ol, 1, 0, 1, 0, 
+   e_widget_toolbook_page_append(otb, NULL, _("Miscellaneous"), ol, 1, 0, 1, 0,
                                  0.5, 0.0);
 
    e_widget_toolbook_page_show(otb, 0);
@@ -339,9 +339,9 @@ _basic_apply(E_Config_Dialog *cfd __UNUSED__, E_Config_Dialog_Data *cfdata)
 
    if (cfdata->fast_mouse_move_threshhold == 0)
      e_config->menus_fast_mouse_move_threshhold = 1.0;
-   else 
+   else
      {
-        e_config->menus_fast_mouse_move_threshhold = 
+        e_config->menus_fast_mouse_move_threshhold =
           cfdata->fast_mouse_move_threshhold;
      }
    e_config->menus_click_drag_timeout = cfdata->click_drag_timeout;
@@ -351,7 +351,7 @@ _basic_apply(E_Config_Dialog *cfd __UNUSED__, E_Config_Dialog_Data *cfdata)
      {
         if (e_config->default_system_menu)
           eina_stringshare_del(e_config->default_system_menu);
-        e_config->default_system_menu = 
+        e_config->default_system_menu =
           eina_stringshare_add(cfdata->default_system_menu);
      }
    else
@@ -380,17 +380,17 @@ _basic_check_changed(E_Config_Dialog *cfd __UNUSED__, E_Config_Dialog_Data *cfda
      move_threshold = cfdata->fast_mouse_move_threshhold;
 
    return ((e_config->menu_favorites_show != cfdata->show_favs) ||
-	   (e_config->menu_apps_show != cfdata->show_apps) ||
-	   (e_config->menu_scroll_toggle != cfdata->scroll_toggle) ||
-	   (e_config->menu_eap_name_show != cfdata->show_name) ||
-	   (e_config->menu_eap_generic_show != cfdata->show_generic) ||
-	   (e_config->menu_eap_comment_show != cfdata->show_comment) ||
-	   (e_config->menus_click_drag_timeout != cfdata->click_drag_timeout) ||
-	   (e_config->menu_autoscroll_margin != cfdata->autoscroll_margin) ||
-	   (e_config->menu_autoscroll_cursor_margin != cfdata->autoscroll_cursor_margin) ||
-	   (e_config->menus_scroll_speed != scroll_speed) ||
-	   (e_config->menus_fast_mouse_move_threshhold != move_threshold) ||
-	   (e_config->menu_gadcon_client_toplevel != cfdata->menu_gadcon_client_toplevel) ||
+       (e_config->menu_apps_show != cfdata->show_apps) ||
+       (e_config->menu_scroll_toggle != cfdata->scroll_toggle) ||
+       (e_config->menu_eap_name_show != cfdata->show_name) ||
+       (e_config->menu_eap_generic_show != cfdata->show_generic) ||
+       (e_config->menu_eap_comment_show != cfdata->show_comment) ||
+       (e_config->menus_click_drag_timeout != cfdata->click_drag_timeout) ||
+       (e_config->menu_autoscroll_margin != cfdata->autoscroll_margin) ||
+       (e_config->menu_autoscroll_cursor_margin != cfdata->autoscroll_cursor_margin) ||
+       (e_config->menus_scroll_speed != scroll_speed) ||
+       (e_config->menus_fast_mouse_move_threshhold != move_threshold) ||
+       (e_config->menu_gadcon_client_toplevel != cfdata->menu_gadcon_client_toplevel) ||
            (!((cfdata->default_system_menu) &&
               (e_config->default_system_menu) &&
               (!strcmp(cfdata->default_system_menu,
