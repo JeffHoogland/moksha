971e1c412fc96046bbbfadcf2bec3c25ee890d11
Robert Wiley <ylee@bodhilinux.com>
2019-06-19 12:24:20 -0400

    About Bodhi: Update to 5.1

--- src/bin/e_bodhi_about.c
+++ src/bin/e_bodhi_about.c
@@ -1,6 +1,6 @@
 #include "e.h"
 
-#define BODHI_VERSION "5.0.0"
+#define BODHI_VERSION "5.1.0"
 #define BODHI_ABOUT_TITLE "About Bodhi"
 #define BODHI_ABOUT_TEXT "Bodhi Linux - Enlightened Linux Desktop"
 #define BODHI_ABOUT_AUTHORS \
