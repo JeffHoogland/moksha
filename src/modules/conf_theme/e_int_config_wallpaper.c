ff980af7c93cc3490ea3c7d9701992a5cc5f285c
Robert Wiley <ylee@bodhilinux.com>
2020-05-11 06:52:04 -0400

    warn - make buffer bigger to avoid trunc warn

--- src/modules/conf_theme/e_int_config_wallpaper.c
+++ src/modules/conf_theme/e_int_config_wallpaper.c
@@ -833,7 +833,7 @@ _edj_gen(Import *import)
    int fd, num = 1;
    int w = 0, h = 0;
    const char *file, *locale;
-   char buf[PATH_MAX], cmd[PATH_MAX], tmpn[PATH_MAX], ipart[PATH_MAX] = "", enc[128];
+   char buf[PATH_MAX], cmd[PATH_MAX + PATH_MAX + 40], tmpn[PATH_MAX], ipart[PATH_MAX] = "", enc[128];
    char *imgdir = NULL, *fstrip;
    int cr, cg, cb, ca;
    FILE *f;
