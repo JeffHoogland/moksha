1a8f403316d1fbc4c9df303b8dc27969f92512db
Robert Wiley <ylee@bodhilinux.com>
2020-06-15 08:47:56 -0400

    e fileman config - fix definite alignment bug with cast char to int ptr

--- src/modules/fileman/e_mod_config.c
+++ src/modules/fileman/e_mod_config.c
@@ -6,7 +6,7 @@ struct _E_Config_Dialog_Data
    /* general view mode */
     struct
     {
-       E_Fm2_View_Mode mode;
+       int mode;
        int open_dirs_in_place;
        int selector;
        int single_click;
@@ -28,7 +28,7 @@ struct _E_Config_Dialog_Data
    {
       double delay;
       double size;
-      Eina_Bool enable;
+      int enable;
       Evas_Object *delay_slider_text;
       Evas_Object *delay_slider;
       Evas_Object *size_slider_text;
@@ -239,7 +239,7 @@ _basic_check_changed(E_Config_Dialog *cfd  __UNUSED__,
                      E_Config_Dialog_Data *cfdata)
 {
    return
-     (fileman_config->view.mode != cfdata->view.mode) ||
+     ((int)fileman_config->view.mode != cfdata->view.mode) ||
      (fileman_config->view.open_dirs_in_place != cfdata->view.open_dirs_in_place) ||
      (fileman_config->view.single_click != cfdata->view.single_click) ||
      (e_config->filemanager_secure_rm != cfdata->secure_rm) ||
@@ -312,7 +312,7 @@ _basic_create(E_Config_Dialog *cfd  __UNUSED__,
 
    o = e_widget_list_add(evas, 0, 0);
    of = e_widget_framelist_add(evas, _("View Mode"), 0);
-   rg = e_widget_radio_group_new((int*)&(cfdata->view.mode));
+   rg = e_widget_radio_group_new(&cfdata->view.mode);
 /*    ob = e_widget_radio_add(evas, _("Icons"), 0, rg); */
 /*    e_widget_disabled_set(ob, 1); */
 /*    e_widget_framelist_object_append(of, ob); */
@@ -469,7 +469,7 @@ _basic_create(E_Config_Dialog *cfd  __UNUSED__,
    /////////////////////////////////////////////////////////////
    o = e_widget_list_add(evas, 1, 0);
    ob = e_widget_check_add(evas, _("Show tooltip"),
-                           (int*)&(cfdata->tooltip.enable));
+                           &(cfdata->tooltip.enable));
    e_widget_on_change_hook_set(ob, _tooltip_changed, cfdata);
    e_widget_list_object_append(o, ob, 1, 1, 0.5);
    cfdata->tooltip.delay_slider_text = ob = e_widget_label_add(evas, _("Tooltip delay"));
