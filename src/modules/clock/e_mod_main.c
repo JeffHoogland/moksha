e8270d594b62e2424575901f60a9ceef826ccefe
thewaiter <s.uram@centrum.sk>
2021-01-03 00:01:48 +0100

    Clock Date update hack (date does not update after PC wake up

--- src/modules/clock/e_mod_main.c
+++ src/modules/clock/e_mod_main.c
@@ -566,7 +566,7 @@ e_int_clock_instances_redo(Eina_Bool all)
 static Eina_Bool
 _update_today_timer(void *data __UNUSED__)
 {
-   time_t t, t_tomorrow;
+   time_t t; //, t_tomorrow;
    const struct tm *now;
    struct tm today;
 
@@ -584,9 +584,13 @@ _update_today_timer(void *data __UNUSED__)
    today.tm_min = 0;
    today.tm_hour = 0;
 
-   t_tomorrow = mktime(&today) + 24 * 60 * 60;
-   if (update_today) ecore_timer_interval_set(update_today, t_tomorrow - t);
-   else update_today = ecore_timer_add(t_tomorrow - t, _update_today_timer, NULL);
+   //~ t_tomorrow = mktime(&today) + 24 * 60 * 60;
+   //~ if (update_today) ecore_timer_interval_set(update_today, t_tomorrow - t);
+   //~ else update_today = ecore_timer_add(t_tomorrow - t, _update_today_timer, NULL);
+   
+   if (update_today) ecore_timer_interval_set(update_today, 5.0);
+   else update_today = ecore_timer_add(5.0, _update_today_timer, NULL);
+   
    return EINA_TRUE;
 }
 
