b92d5549f9a59d1b1562d299ea82689fb84d35ce
thewaiter <s.uram@centrum.sk>
2021-01-12 22:00:59 +0100

    Config items change for better understanding

--- src/modules/tasks/e_mod_config.c
+++ src/modules/tasks/e_mod_config.c
@@ -32,7 +32,7 @@ _config_tasks_module(Config_Item *ci)
    v->basic.create_widgets = _basic_create_widgets;
 
    con = e_container_current_get(e_manager_current_get());
-   cfd = e_config_dialog_new(con, _("Tasks Configuration"), "Tasks",
+   cfd = e_config_dialog_new(con, _("Tasks Settings"), "Tasks",
                              "_e_modules_tasks_config_dialog", NULL, 0, v, ci);
    if (tasks_config->config_dialog)
      e_object_del(E_OBJECT(tasks_config->config_dialog));
@@ -96,15 +96,15 @@ _basic_create_widgets(E_Config_Dialog *cfd __UNUSED__, Evas *evas, E_Config_Dial
    cfdata->text = ob = e_widget_check_add(evas, _("Show text only"),
                            &(cfdata->text_only));
    e_widget_framelist_object_append(of, ob);
-   ob = e_widget_check_add(evas, _("Item autoresize"),
-                           &(cfdata->autoresize));
-   e_widget_framelist_object_append(of, ob);
    e_widget_on_change_hook_set(ob, _toggle_check, cfdata);
    ow = e_widget_label_add(evas, _("Item width"));
    e_widget_framelist_object_append(of, ow);
    ow = e_widget_slider_add(evas, 1, 0, _("%1.0f px"), 20, 420, 1, 0,
                             NULL, &(cfdata->minw), 100);
    e_widget_framelist_object_append(of, ow);
+   ob = e_widget_check_add(evas, _("Shrink to fit"),
+                           &(cfdata->autoresize));
+   e_widget_framelist_object_append(of, ob);
    ow = e_widget_label_add(evas, _("Item height"));
    e_widget_framelist_object_append(of, ow);
    ow = e_widget_slider_add(evas, 1, 0, _("%1.0f px"), 20, 420, 1, 0,
