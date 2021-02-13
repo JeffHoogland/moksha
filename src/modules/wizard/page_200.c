793b0a3cdd93f4ed3e90b4c25734c2dbbc82d7b2
Robert Wiley <ylee@bodhilinux.com>
2021-02-13 13:42:57 -0500

    Disable firstboot landing

--- src/modules/wizard/page_200.c
+++ src/modules/wizard/page_200.c
@@ -42,7 +42,7 @@ wizard_page_apply(E_Wizard_Page *pg __UNUSED__)
    // restart e
    e_sys_action_do(E_SYS_RESTART, NULL);
 
-   e_util_open_bodhi_landing();
+   // e_util_open_bodhi_landing();
    return 1;
 }
 
