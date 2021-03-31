e4bd202b42dd421b5046d2add82b9bd0e416dcc6
Robert Wiley <ylee@bodhilinux.com>
2021-03-31 09:14:19 -0400

    e_wizard: Tweak focus settings to restore traditional Bodhi settings

--- src/modules/wizard/page_060.c
+++ src/modules/wizard/page_060.c
@@ -1,7 +1,7 @@
 /* Ask about focus mode */
 #include "e_wizard.h"
 
-static int focus_mode = 1;
+static int focus_mode = 0;
 /*
 EAPI int
 wizard_page_init(E_Wizard_Page *pg __UNUSED__, Eina_Bool *need_xdg_desktops __UNUSED__, Eina_Bool *need_xdg_icons __UNUSED__)
@@ -33,12 +33,14 @@ wizard_page_show(E_Wizard_Page *pg)
    else
      {
         e_config->focus_policy = E_FOCUS_SLOPPY;
-        e_config->focus_setting = E_FOCUS_NEW_DIALOG_IF_OWNER_FOCUSED;
+        //e_config->focus_setting = E_FOCUS_NEW_DIALOG_IF_OWNER_FOCUSED;
+        e_config->focus_setting = E_FOCUS_NEW_WINDOW;
         e_config->pass_click_on = 1;
         e_config->always_click_to_raise = 0;
         e_config->always_click_to_focus = 0;
         e_config->focus_last_focused_per_desktop = 1;
-        e_config->pointer_slide = 1;
+        //e_config->pointer_slide = 1;
+        e_config->pointer_slide = 0;
      }
    return 0; /* 1 == show ui, and wait for user, 0 == just continue */
 }
