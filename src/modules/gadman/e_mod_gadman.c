dbd7e6fc93f3fc6907b973d1f4c76599693e744b
thewaiter <s.uram@centrum.sk>
2021-06-26 21:03:02 +0200

    Gadman middle click optimized

--- src/modules/gadman/e_mod_gadman.c
+++ src/modules/gadman/e_mod_gadman.c
@@ -854,7 +854,6 @@ _create_mover(E_Gadcon *gc)
                                    gadman_gadget_edit_end, NULL);
    edje_object_signal_callback_add(mover, "mouse,down,2", "*",
                                    gadman_gadget_edit_end, NULL);
-
    edje_object_signal_callback_add(mover, "e,action,resize,left,start", "",
                                    on_left, (void *)DRAG_START);
    edje_object_signal_callback_add(mover, "e,action,resize,left,stop", "",
@@ -1200,22 +1199,12 @@ on_frame_click(void *data, Evas *e __UNUSED__, Evas_Object *obj __UNUSED__, void
 {
    Evas_Event_Mouse_Down *ev;
    E_Gadcon_Client *gcc;
-   E_Gadcon *gc;
 
    ev = event_info;
-
    gcc = data;
 
    if (ev->button == 2)
      {
-        int cx, cy, cw, ch;
-
-        e_gadcon_canvas_zone_geometry_get(gcc->gadcon, &cx, &cy, &cw, &ch);
-
-        evas_event_feed_mouse_up(gcc->gadcon->evas, ev->button,
-                                 EVAS_BUTTON_NONE, ev->timestamp, NULL);
-        gc = gcc->gadcon;
-        if (!gc->editing)
         gadman_gadget_edit_start(gcc);
      }
 
