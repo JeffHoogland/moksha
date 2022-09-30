93eaef4589a9238e974ddd03c5c38616894a869d
Robert Wiley <ylee@bodhilinux.com>
2022-09-30 19:13:57 -0400

    Clean up mixer/lib/common.h

--- src/modules/mixer/lib/common.h
+++ src/modules/mixer/lib/common.h
@@ -16,17 +16,17 @@
 # include <Elementary.h>
 #endif
 
-#   ifdef HAVE_GETTEXT
-#define _(str) gettext(str)
-#define d_(str, dom) dgettext(PACKAGE dom, str)
-#define P_(str, str_p, n) ngettext(str, str_p, n)
-#define dP_(str, str_p, n, dom) dngettext(PACKAGE dom, str, str_p, n)
-#   else
-#define _(str) (str)
-#define d_(str, dom) (str)
-#define P_(str, str_p, n) (str_p)
-#define dP_(str, str_p, n, dom) (str_p)
-#   endif
+#ifdef HAVE_GETTEXT
+# define _(str) gettext(str)
+# define d_(str, dom) dgettext(PACKAGE dom, str)
+# define P_(str, str_p, n) ngettext(str, str_p, n)
+# define dP_(str, str_p, n, dom) dngettext(PACKAGE dom, str, str_p, n)
+#else
+# define _(str) (str)
+# define d_(str, dom) (str)
+# define P_(str, str_p, n) (str_p)
+# define dP_(str, str_p, n, dom) (str_p)
+#endif
 
 /* These macros are used to just mark strings for translation, this is useful
  * for string lists which are not dynamically allocated
