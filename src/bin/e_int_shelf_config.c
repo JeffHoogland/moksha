369d8234092f1e97a278bf6dec12c6d4669fe632
thewaiter <s.uram@centrum.sk>
2023-02-18 21:01:46 +0100

    Shelf recreate after style change

--- src/bin/e_int_shelf_config.c
+++ src/bin/e_int_shelf_config.c
@@ -412,6 +412,7 @@ _basic_apply(E_Config_Dialog *cfd, E_Config_Dialog_Data *cfdata)
      {
         eina_stringshare_replace(&cfdata->escfg->style, cfdata->style);
         e_shelf_style_set(cfdata->es, cfdata->style);
+        recreate = 1;
      }
    if (recreate)
      {
