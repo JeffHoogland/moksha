e409fd16d0aee5cb62866018cb89fcc04e9f0508
thewaiter <s.uram@centrum.sk>
2023-07-09 22:13:10 +0200

    Taksks: loop indent correction

--- src/modules/tasks/e_mod_main.c
+++ src/modules/tasks/e_mod_main.c
@@ -447,16 +447,17 @@ _tasks_autoresize(void *data __UNUSED__)
    Tasks *tasks;
    int gw, gh;
 
-     EINA_LIST_FOREACH(tasks_config->tasks, l, tasks)
-       {
-          if (tasks->config->autoresize)
-          {
-            evas_object_geometry_get(tasks->gcc->o_frame, NULL, NULL, &gw, &gh);
-            e_gadcon_client_min_size_set(tasks->gcc, gw - 10, gh);
-            evas_object_show(tasks->gcc->o_base);
-          }
-       }
-    return ECORE_CALLBACK_CANCEL;
+   EINA_LIST_FOREACH(tasks_config->tasks, l, tasks)
+     {
+       if (tasks->config->autoresize)
+         {
+           evas_object_geometry_get(tasks->gcc->o_frame, NULL, NULL, &gw, &gh);
+           e_gadcon_client_min_size_set(tasks->gcc, gw - 10, gh);
+           evas_object_show(tasks->gcc->o_base);
+         }
+     }
+
+   return ECORE_CALLBACK_CANCEL;
 }
 
 static void
