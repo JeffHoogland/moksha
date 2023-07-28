39160c076ce81cc787d56506da6cb23511a6d389
thewaiter <s.uram@centrum.sk>
2023-07-28 13:13:02 +0200

    Prevent EFM from opening symlinks

--- src/modules/fileman/e_fwin.c
+++ src/modules/fileman/e_fwin.c
@@ -2390,9 +2390,11 @@ _e_fwin_open(E_Fwin_Page *page, E_Fm2_Icon_Info *ici, Eina_Bool force, int *need
              if (new_fwin)
                {
                   if (page->fwin->win)
-                    fwin = _e_fwin_new(page->fwin->win->container, NULL, ici->real_link);
+                    //~ fwin = _e_fwin_new(page->fwin->win->container, NULL, ici->real_link);
+                     e_custom_fm_open(e_util_zone_current_get (e_manager_current_get ()), ici->real_link, "");
                   else if (page->fwin->zone)
-                    fwin = _e_fwin_new(page->fwin->zone->container, NULL, ici->real_link);
+                    //~ fwin = _e_fwin_new(page->fwin->zone->container, NULL, ici->real_link);
+                    e_custom_fm_open(e_util_zone_current_get (e_manager_current_get ()), ici->real_link, "");
                }
              else
                {
