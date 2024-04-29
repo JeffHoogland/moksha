9a7d9ead1b231469250a10b4fa8b684e316a5388
thewaiter <s.uram@centrum.sk>
2024-04-29 19:07:44 +0200

    e sys main - fix minor leaking iterator (E port)

--- src/bin/e_sys_main.c
+++ src/bin/e_sys_main.c
@@ -345,6 +345,7 @@ main(int argc,
                   rm = EINA_FALSE;
                   eina_stringshare_del(s);
                }
+             eina_iterator_free(it);
              if (rm)
                {
                   if (rmdir(path))
