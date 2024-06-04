2175ff632b75e9b8e262264f368bb71e72cfef0c
thewaiter <s.uram@centrum.sk>
2024-06-04 23:16:30 +0200

    Menus: separator after menu title causes freez? (hmmm)

--- src/bin/e_int_menus.c
+++ src/bin/e_int_menus.c
@@ -337,8 +337,6 @@ e_int_menus_apps_empty()
 
    e_menu_title_set(m, _("Favorite Apps"));
    mi = e_menu_item_new(m);
-   e_menu_item_separator_set(mi, 1);
-   mi = e_menu_item_new(m);
 
    e_menu_item_label_set(mi, _("No applications"));
    e_util_menu_item_theme_icon_set(mi, "user-bookmarks");
