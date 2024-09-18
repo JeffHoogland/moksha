81ba66aac5222b0e77d9bee1ac7714008327cdef
thewaiter <s.uram@centrum.sk>
2024-09-18 20:29:27 +0200

    Gadcon: remove call to useless gadcon function

--- src/bin/e_gadcon.c
+++ src/bin/e_gadcon.c
@@ -2517,7 +2517,7 @@ _e_gadcon_cb_client_resize(void *data, Evas *evas __UNUSED__, Evas_Object *obj, 
 static void
 _e_gadcon_client_move_start(E_Gadcon_Client *gcc)
 {
-   int x, y, gcx, gcy, gy, gx;
+   int x, y, gy, gx;
 
    evas_object_raise(gcc->o_event);
    evas_object_stack_below(gcc->o_control, gcc->o_event);
@@ -2527,10 +2527,9 @@ _e_gadcon_client_move_start(E_Gadcon_Client *gcc)
    else
      {
         ecore_x_pointer_xy_get(gcc->gadcon->zone->container->win, &gcc->dx, &gcc->dy);
-        e_gadcon_canvas_zone_geometry_get(gcc->gadcon, &gcx, &gcy, NULL, NULL);
         evas_object_geometry_get(gcc->gadcon->o_container, &gx, &gy, NULL, NULL);
-        gcc->dx -= (gcx + gx);
-        gcc->dy -= (gcy + gy);
+        gcc->dx -= gx;
+        gcc->dy -= gy;
      }
 
    if (gcc->o_frame)
