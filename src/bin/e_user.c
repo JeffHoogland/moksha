6edf79a1e1de6cfffd973ccef800acd9485fd010
Robert Wiley <ylee@bodhilinux.com>
2025-01-10 06:42:55 -0500

    allow xdg dir usage if you add -DDOXDG to CFLAGS

--- src/bin/e_user.c
+++ src/bin/e_user.c
@@ -1,5 +1,12 @@
-#include "e.h"
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+#include <stdio.h>
+#include <string.h>
+#include <limits.h>
+#include <Efreet.h>
 
+#include "e_macros.h"
 
 static const char *_e_user_homedir = NULL;
 static size_t _e_user_homedir_len = 0;
@@ -16,17 +23,17 @@ e_user_homedir_get(void)
    _e_user_homedir = d = getenv("HOME");
    if (!_e_user_homedir)
      {
-	_e_user_homedir = "/tmp";
-	_e_user_homedir_len = sizeof("/tmp") - 1;
-	return _e_user_homedir;
+        _e_user_homedir = "/tmp";
+        _e_user_homedir_len = sizeof("/tmp") - 1;
+        return _e_user_homedir;
      }
 
    _e_user_homedir_len = strlen(_e_user_homedir);
    while ((_e_user_homedir_len > 1) &&
-	  (d[_e_user_homedir_len - 1] == '/'))
+          (d[_e_user_homedir_len - 1] == '/'))
      {
-	_e_user_homedir_len--;
-	d[_e_user_homedir_len] = '\0';
+        _e_user_homedir_len--;
+        d[_e_user_homedir_len] = '\0';
      }
    return _e_user_homedir;
 }
@@ -55,7 +62,7 @@ e_user_homedir_concat(char *dst, size_t size, const char *path)
 /**
  * same as snprintf("~/"fmt, ...).
  */
-EAPI size_t
+EAPI size_t EINA_PRINTF(3, 4)
 e_user_homedir_snprintf(char *dst, size_t size, const char *fmt, ...)
 {
    size_t off, ret;
@@ -71,14 +78,14 @@ e_user_homedir_snprintf(char *dst, size_t size, const char *fmt, ...)
    off = _e_user_homedir_len + 1;
    if (size < _e_user_homedir_len + 2)
      {
-	if (size > 1)
-	  {
-	     memcpy(dst, _e_user_homedir, size - 1);
-	     dst[size - 1] = '\0';
-	  }
-	ret = off + vsnprintf(dst + off, size - off, fmt, ap);
-	va_end(ap);
-	return ret;
+        if (size > 1)
+          {
+             memcpy(dst, _e_user_homedir, size - 1);
+             dst[size - 1] = '\0';
+          }
+        ret = off + vsnprintf(dst + off, size - off, fmt, ap);
+        va_end(ap);
+        return ret;
      }
 
    memcpy(dst, _e_user_homedir, _e_user_homedir_len);
@@ -124,6 +131,8 @@ e_user_icon_dir_get(void)
 static const char *_e_user_dir = NULL;
 static size_t _e_user_dir_len = 0;
 
+EAPI void         e_util_env_set(const char *var, const char *val);
+
 /**
  * Return ~/.e/e
  */
@@ -131,24 +140,35 @@ EAPI const char *
 e_user_dir_get(void)
 {
    static char dir[PATH_MAX] = "";
-   static char buf[PATH_MAX] = "";
 
    if (!dir[0])
      {
-	char *e_home = getenv("E_HOME");
-	if (e_home)
-	  {
-	     snprintf(buf, sizeof(buf), "%s/e", e_home);
-	  }
-	else
-	  {
-	     snprintf(buf, sizeof(buf), ".e/e");
-	  }
-	_e_user_dir_len = e_user_homedir_concat(dir, sizeof(dir), buf);
-	_e_user_dir = dir;
-     }
+        char *d = getenv("E_HOME");
 
-   return dir;
+        if (d)
+          snprintf(dir, sizeof(dir), "E_HOME_DIR=%s/e", d);
+        else
+          {
+#ifdef DOXDG
+             if ((d = getenv("XDG_CONFIG_HOME")))
+               snprintf(dir, sizeof(dir), "E_HOME_DIR=%s/e", d);
+             else
+#endif
+               {
+                  char buf[PATH_MAX - 128];
+#ifdef DOXDG
+                  e_user_homedir_concat(buf, sizeof(buf), ".config/e");
+#else
+                  e_user_homedir_concat(buf, sizeof(buf), ".e/e");
+#endif
+                  snprintf(dir, sizeof(dir), "E_HOME_DIR=%s", buf);
+               }
+          }
+        _e_user_dir = dir + strlen("E_HOME_DIR=");
+        _e_user_dir_len = strlen(_e_user_dir);
+        putenv(dir);
+     }
+   return _e_user_dir;
 }
 
 /**
@@ -175,7 +195,7 @@ e_user_dir_concat(char *dst, size_t size, const char *path)
 /**
  * same as snprintf("~/.e/e/"fmt, ...).
  */
-EAPI size_t
+EAPI size_t EINA_PRINTF(3, 4)
 e_user_dir_snprintf(char *dst, size_t size, const char *fmt, ...)
 {
    size_t off, ret;
@@ -191,14 +211,14 @@ e_user_dir_snprintf(char *dst, size_t size, const char *fmt, ...)
    off = _e_user_dir_len + 1;
    if (size < _e_user_dir_len + 2)
      {
-	if (size > 1)
-	  {
-	     memcpy(dst, _e_user_dir, size - 1);
-	     dst[size - 1] = '\0';
-	  }
-	ret = off + vsnprintf(dst + off, size - off, fmt, ap);
-	va_end(ap);
-	return ret;
+        if (size > 1)
+          {
+             memcpy(dst, _e_user_dir, size - 1);
+             dst[size - 1] = '\0';
+          }
+        ret = off + vsnprintf(dst + off, size - off, fmt, ap);
+        va_end(ap);
+        return ret;
      }
 
    memcpy(dst, _e_user_dir, _e_user_dir_len);
