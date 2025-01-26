91cd788f0f29b111ac41bf862636cb28ca31db65
thewaiter <s.uram@centrum.sk>
2025-01-26 08:47:38 +0100

    Edit for personal launcher dialog added

--- src/modules/conf_applications/e_int_config_apps_personal.c
+++ src/modules/conf_applications/e_int_config_apps_personal.c
@@ -7,7 +7,7 @@ struct _E_Config_Dialog_Data
 
    struct
    {
-      Evas_Object *list, *add, *del;
+      Evas_Object *list, *add, *del, *edit;
    } obj;
 };
 
@@ -20,6 +20,7 @@ static int          _cb_desks_sort(const void *data1, const void *data2);
 static void         _fill_apps_list(E_Config_Dialog_Data *cfdata, Evas_Object *il);
 static void         _btn_cb_add(void *data, void *data2);
 static void         _btn_cb_del(void *data, void *data2);
+static void         _btn_cb_edit(void *data, void *data2);
 static void         _widget_list_selection_changed(void *data, Evas_Object *obj __UNUSED__);
 
 E_Config_Dialog *
@@ -85,7 +86,7 @@ _basic_create(E_Config_Dialog *cfd, Evas *evas, E_Config_Dialog_Data *cfdata)
    if (mh < (160 * e_scale)) mh = 160 * e_scale;
    e_widget_size_min_set(li, mw, mh);
    e_widget_on_change_hook_set(li, _widget_list_selection_changed, cfdata);
-   e_widget_table_object_append(of, li, 0, 1, 2, 1, 1, 1, 1, 1);
+   e_widget_table_object_append(of, li, 0, 1, 3, 1, 1, 1, 1, 1);
 
    _fill_apps_list(cfdata, cfdata->obj.list);
    e_widget_ilist_go(li);
@@ -98,6 +99,11 @@ _basic_create(E_Config_Dialog *cfd, Evas *evas, E_Config_Dialog_Data *cfdata)
    cfdata->obj.del = ob;
    e_widget_disabled_set(ob, 1);
    e_widget_table_object_append(of, ob, 1, 2, 1, 1, 1, 1, 1, 0);
+
+   ob = e_widget_button_add(evas, _("Edit"), "edit-rename", _btn_cb_edit, cfdata, cfd);
+   cfdata->obj.edit = ob;
+   e_widget_disabled_set(ob, 1);
+   e_widget_table_object_append(of, ob, 2, 2, 1, 1, 1, 1, 1, 0);
 
    e_win_centered_set(cfd->dia->win, 1);
    return of;
@@ -216,10 +222,34 @@ _btn_cb_del(void *data, void *data2 __UNUSED__)
 }
 
 static void
+_btn_cb_edit(void *data, void *data2)
+{
+   E_Config_Dialog_Data *cfdata = data;
+   E_Config_Dialog *cfd = data2;
+   const Eina_List *l;
+   const E_Ilist_Item *it;
+
+   EINA_LIST_FOREACH(e_widget_ilist_items_get(cfdata->obj.list), l, it)
+     {
+        const char *file;
+        Efreet_Desktop *desktop;
+
+        if (!it->selected) continue;
+        file = e_widget_ilist_item_data_get(it);
+        if (!file) break;
+        desktop = efreet_desktop_new(file);
+        if (desktop)
+          e_desktop_edit(cfd->con, desktop);
+        break;
+     }
+}
+
+static void
 _widget_list_selection_changed(void *data, Evas_Object *obj __UNUSED__)
 {
    E_Config_Dialog_Data *cfdata = data;
 
    e_widget_disabled_set(cfdata->obj.del, !e_widget_ilist_selected_count_get(cfdata->obj.list));
+   e_widget_disabled_set(cfdata->obj.edit, !e_widget_ilist_selected_count_get(cfdata->obj.list));
 }
 
