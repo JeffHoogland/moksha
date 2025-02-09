82a1575db7b9f38a47098935977abc135e3460a6
thewaiter <s.uram@centrum.sk>
2025-02-09 21:53:25 +0100

    Wallpaper2 out of the Moksha core (standalone mod now in extras)

--- src/bin/e_module.c
+++ src/bin/e_module.c
@@ -28,7 +28,6 @@
         "conf_randr", \
         "conf_shelves", \
         "conf_theme", \
-        "conf_wallpaper2", \
         "conf_window_manipulation", \
         "conf_window_remembers", \
         "connman", \
